49e7e6730fb3603c15095cb615b845b0382f2492
daadaadaah <daadaadaah@gmail.com>
2020-06-03 03:31:42 +0900

    Remove console.log

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 /* @jsx createElement */
 
-
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
@@ -21,298 +20,181 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-let operand1 = "";
-let operator = "";
-let operand2 = "";
+let operand1 = '';
+let operator = '';
+let operand2 = '';
 
 function handleClickNumber(number) {
-  console.log(
-    `--------------------------------------------------------------------`
-  );
-  if (operand2 === "") {
+  if (operand2 === '') {
     operand2 = String(number);
   } else {
     operand2 += String(number);
   }
 
-  console.log(
-    `1-1 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-
   render(Number(operand2));
 }
 
 function handleClickPlusNumber() {
-  console.log(
-    `--------------------------------(${number}+)------------------------------------`
-  );
-  console.log(
-    `1 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-
-  if (operator === "") {
-    if (operand1 == "") {
+  if (operator === '') {
+    if (operand1 === '') {
       operand1 = operand2;
-      operator = "+";
-      console.log(
-        `2-1 (1) operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-      );
+      operator = '+';
     } else {
-      
-      console.log(
-        `2-1 (2-1) operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-      );
       operand1 = Number(operand1) + Number(operand2);
       render(Number(operand1));
-      // operator = "";
-      console.log(
-        `2-1 (2-2) operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-      );
     }
-
-    
   } else {
     switch (operator) {
-      case "+":
-        operand1 = Number(operand1) + Number(operand2);
-        break;
-      case "-":
-        operand1 = Number(operand1) - Number(operand2);
-        break;
-      case "*":
-        operand1 = Number(operand1) * Number(operand2);
-        break;
-      case "/":
-        operand1 = Number(operand1) / Number(operand2);
-        break;
-      default:
-        break;
-    }
-    console.log(
-      `2-2 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-    );
-
-    render(Number(operand1));
-    operator = "+";
-  }
-  console.log(
-    `3 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-  operand2 = "";
-  console.log(
-    `4 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-}
-
-function handleClickMinusNumber() {
-  console.log(
-    `-----------------------------(${number}-)---------------------------------------`
-  );
-  console.log(
-    `1 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-
-  if (operator === "") {
-    if (operand1 == "") {
-      operand1 = operand2;
-      operator = "-";
-    } else {
-
-
-      
-      operand1 = Number(operand1) - Number(operand2);
-      render(Number(operand1));
-      // operator = "";
-    }
-
-    console.log(
-      `2-1 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-    );
-  } else {
-    switch (operator) {
-      case "+":
-        operand1 = Number(operand1) + Number(operand2);
-        break;
-      case "-":
-        operand1 = Number(operand1) - Number(operand2);
-        break;
-      case "*":
-        operand1 = Number(operand1) * Number(operand2);
-        break;
-      case "/":
-        operand1 = Number(operand1) / Number(operand2);
-        break;
-      default:
-        break;
-    }
-    console.log(
-      `2-2 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-    );
-
-    render(Number(operand1));
-    operator = "-";
-  }
-  console.log(
-    `3 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-  operand2 = "";
-  console.log(
-    `4 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-}
-
-function handleClickMultiplyNumber() {
-  console.log(
-    `--------------------------------(${number}+)------------------------------------`
-  );
-  console.log(
-    `1 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-
-  if (operator === "") {
-    if (operand1 == "") {
-      operand1 = operand2;
-      operator = "+";
-      console.log(
-        `2-1 (1) operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-      );
-    } else {
-      
-      console.log(
-        `2-1 (2-1) operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-      );
-      operand1 = Number(operand1) + Number(operand2);
-      render(Number(operand1));
-      // operator = "";
-      console.log(
-        `2-1 (2-2) operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-      );
-    }
-
-    
-  } else {
-    switch (operator) {
-      case "+":
-        operand1 = Number(operand1) + Number(operand2);
-        break;
-      case "-":
-        operand1 = Number(operand1) - Number(operand2);
-        break;
-      case "*":
-        operand1 = Number(operand1) * Number(operand2);
-        break;
-      case "/":
-        operand1 = Number(operand1) / Number(operand2);
-        break;
-      default:
-        break;
-    }
-    console.log(
-      `2-2 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-    );
-
-    render(Number(operand1));
-    operator = "+";
-  }
-  console.log(
-    `3 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-  operand2 = "";
-  console.log(
-    `4 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-}
-
-function handleClickDivideAddNumber() {
-  console.log(
-    `-----------------------------(${number}-)---------------------------------------`
-  );
-  console.log(
-    `1 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-
-  if (operator === "") {
-    if (operand1 == "") {
-      operand1 = operand2;
-      operator = "-";
-    } else {
-
-
-      
-      operand1 = Number(operand1) - Number(operand2);
-      render(Number(operand1));
-      // operator = "";
-    }
-
-    console.log(
-      `2-1 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-    );
-  } else {
-    switch (operator) {
-      case "+":
-        operand1 = Number(operand1) + Number(operand2);
-        break;
-      case "-":
-        operand1 = Number(operand1) - Number(operand2);
-        break;
-      case "*":
-        operand1 = Number(operand1) * Number(operand2);
-        break;
-      case "/":
-        operand1 = Number(operand1) / Number(operand2);
-        break;
-      default:
-        break;
-    }
-    console.log(
-      `2-2 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-    );
-
-    render(Number(operand1));
-    operator = "-";
-  }
-  console.log(
-    `3 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-  operand2 = "";
-  console.log(
-    `4 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
-}
-
-function handleClickShowTotal() {
-  
-  switch (operator) {
-    case "+":
+    case '+':
       operand1 = Number(operand1) + Number(operand2);
       break;
-    case "-":
+    case '-':
       operand1 = Number(operand1) - Number(operand2);
       break;
-    case "*":
+    case '*':
       operand1 = Number(operand1) * Number(operand2);
       break;
-    case "/":
+    case '/':
       operand1 = Number(operand1) / Number(operand2);
       break;
     default:
       break;
+    }
+
+    render(Number(operand1));
+    operator = '+';
   }
-  console.log(
-    `2-2 operand1(${operand1}) operator(${operator}) + operane2(${operand2})`
-  );
 
-  render(Number(operand1));
-  operand1 = "";
-  operator = "";
-  operand2 = "";
-
-
+  operand2 = '';
 }
 
+function handleClickMinusNumber() {
+  if (operator === '') {
+    if (operand1 === '') {
+      operand1 = operand2;
+      operator = '-';
+    } else {
+      operand1 = Number(operand1) - Number(operand2);
+      render(Number(operand1));
+    }
+  } else {
+    switch (operator) {
+    case '+':
+      operand1 = Number(operand1) + Number(operand2);
+      break;
+    case '-':
+      operand1 = Number(operand1) - Number(operand2);
+      break;
+    case '*':
+      operand1 = Number(operand1) * Number(operand2);
+      break;
+    case '/':
+      operand1 = Number(operand1) / Number(operand2);
+      break;
+    default:
+      break;
+    }
+
+    render(Number(operand1));
+    operator = '-';
+  }
+
+  operand2 = '';
+}
+
+function handleClickMultiplyNumber() {
+  if (operator === '') {
+    if (operand1 === '') {
+      operand1 = operand2;
+      operator = '+';
+    } else {
+      operand1 = Number(operand1) + Number(operand2);
+      render(Number(operand1));
+    }
+  } else {
+    switch (operator) {
+    case '+':
+      operand1 = Number(operand1) + Number(operand2);
+      break;
+    case '-':
+      operand1 = Number(operand1) - Number(operand2);
+      break;
+    case '*':
+      operand1 = Number(operand1) * Number(operand2);
+      break;
+    case '/':
+      operand1 = Number(operand1) / Number(operand2);
+      break;
+    default:
+      break;
+    }
+
+    render(Number(operand1));
+    operator = '+';
+  }
+
+  operand2 = '';
+}
+
+function handleClickDivideAddNumber() {
+  if (operator === '') {
+    if (operand1 === '') {
+      operand1 = operand2;
+      operator = '-';
+    } else {
+      operand1 = Number(operand1) - Number(operand2);
+      render(Number(operand1));
+    }
+  } else {
+    switch (operator) {
+    case '+':
+      operand1 = Number(operand1) + Number(operand2);
+      break;
+    case '-':
+      operand1 = Number(operand1) - Number(operand2);
+      break;
+    case '*':
+      operand1 = Number(operand1) * Number(operand2);
+      break;
+    case '/':
+      operand1 = Number(operand1) / Number(operand2);
+      break;
+    default:
+      break;
+    }
+
+    render(Number(operand1));
+    operator = '-';
+  }
+
+  operand2 = '';
+}
+
+function handleClickShowTotal() {
+  switch (operator) {
+  case '+':
+    operand1 = Number(operand1) + Number(operand2);
+    break;
+  case '-':
+    operand1 = Number(operand1) - Number(operand2);
+    break;
+  case '*':
+    operand1 = Number(operand1) * Number(operand2);
+    break;
+  case '/':
+    operand1 = Number(operand1) / Number(operand2);
+    break;
+  default:
+    break;
+  }
+
+  render(Number(operand1));
+  operand1 = '';
+  operator = '';
+  operand2 = '';
+}
 
 function render(number = 0) {
   const element = (
@@ -336,10 +218,7 @@ function render(number = 0) {
         <button type="button" onClick={() => handleClickMultiplyNumber()}>
           *
         </button>
-        <button
-          type="button"
-          onClick={() => handleClickDivideAddNumber()}
-        >
+        <button type="button" onClick={() => handleClickDivideAddNumber()}>
           /
         </button>
         <button type="button" onClick={() => handleClickShowTotal()}>
@@ -350,8 +229,8 @@ function render(number = 0) {
     </div>
   );
 
-  document.getElementById("app").textContent = "";
-  document.getElementById("app").appendChild(element);
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
 render();
