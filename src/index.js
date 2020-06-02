965508b2f550b24c8b1a2e860a0f5d4782066d6f
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-06-02 23:56:52 +0900

    feat: initial buggy implementation

--- src/index.js
+++ src/index.js
@@ -20,10 +20,60 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render({
+  result = 0, left = '', right = '', op = '', displayValue = '0',
+}) {
+  const handleNumClick = (numStr) => {
+    if (['+', '-', '*', '/'].includes(op)) {
+      right += numStr;
+      render({
+        result, left, right, op, displayValue,
+      });
+      return;
+    }
+    left += numStr;
+    render({
+      result, left, right, op, displayValue,
+    });
+  };
+
+  const handleOpClick = (opStr) => {
+    op = opStr;
+    render({
+      result, left, right, op, displayValue,
+    });
+  };
+
+  const handleResultClick = () => {
+    displayValue = eval(left + op + right);
+    left = result;
+    render({
+      result, left, right, op, displayValue,
+    });
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      l:
+      {left}
+      <br />
+      r:
+      {right}
+      <br />
+      op:
+      {op}
+      <br />
+      dis:
+      {displayValue}
+      <br />
+      <div>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (<button type="button" onClick={() => handleNumClick(num)}>{num}</button>))}
+      </div>
+      <div>
+        {['+', '-', '*', '/'].map((op) => (<button type="button" onClick={() => handleOpClick(op)}>{op}</button>))}
+        <button type="button" onClick={handleResultClick}>=</button>
+      </div>
     </div>
   );
 
@@ -31,4 +81,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render({});
