7d23c4a6b524ae1f19659ef46e5812aec9b753b2
Kwakcena <rhkrgudwh@gmail.com>
2020-06-03 18:36:07 +0900

    UI: 버튼 추가
    숫자와 연산자 버튼을 화면에 추가한다.

--- src/index.js
+++ src/index.js
@@ -20,10 +20,24 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operators = ['+', '-', '*', '/', '='];
+
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <div id="result">답</div>
+      <p id="button-list">
+        {numbers.map((number) => (
+          <button type="button">{number}</button>
+        ))}
+      </p>
+      <p id="operator-list">
+        {operators.map((operator) => (
+          <button type="button">{operator}</button>
+        ))}
+      </p>
     </div>
   );
 
