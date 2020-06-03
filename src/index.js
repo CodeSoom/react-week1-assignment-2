76de8ab3d323034c2c7edcb0558dc52a3b4a8903
eonsang <2eonhworking@gmail.com>
2020-06-03 11:48:31 +0900

    기능1 구현완료 했다.
    구현기능1: 숫자를 누르면 누른 숫자가 출력되어야 합니다

--- src/index.js
+++ src/index.js
@@ -20,10 +20,55 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+let number = 0;
 function render() {
+  const showCurrentNumber = () => {
+    return number;
+  };
+  const setCurrentNumber = (value) => {
+    if (number !== 0) {
+      number += String(value);
+    } else {
+      number = value;
+    }
+    render();
+  };
   const element = (
     <div>
       <p>간단 계산기</p>
+      <h1 id="result">{number}</h1>
+      <div>
+        <button type="button" onClick={() => setCurrentNumber(1)}>
+          1
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(2)}>
+          2
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(3)}>
+          3
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(4)}>
+          4
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(5)}>
+          5
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(6)}>
+          6
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(7)}>
+          7
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(8)}>
+          8
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(9)}>
+          9
+        </button>
+        <button type="button" onClick={() => setCurrentNumber(0)}>
+          0
+        </button>
+      </div>
     </div>
   );
 
