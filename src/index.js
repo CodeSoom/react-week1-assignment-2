640ec2db6467df264304b06ddb234b4a41832c87
eonsang <2eonhworking@gmail.com>
2020-06-03 11:53:11 +0900

    기능2 구현완료 했다.
    구현기능2: 숫자를 연속해서 누르면 숫자가 더해져서 출력되어야 합니다.

--- src/index.js
+++ src/index.js
@@ -21,51 +21,110 @@ function createElement(tagName, props, ...children) {
 }
 
 let number = 0;
+const showCurrentNumber = () => {
+  return number;
+};
+const setCurrentNumber = (value) => {
+  if (number !== 0) {
+    number += String(value);
+  } else {
+    number = value;
+  }
+};
 function render() {
-  const showCurrentNumber = () => {
-    return number;
-  };
-  const setCurrentNumber = (value) => {
-    if (number !== 0) {
-      number += String(value);
-    } else {
-      number = value;
-    }
-    render();
-  };
   const element = (
     <div>
       <p>간단 계산기</p>
       <h1 id="result">{number}</h1>
       <div>
-        <button type="button" onClick={() => setCurrentNumber(1)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(1);
+            render();
+          }}
+        >
           1
         </button>
-        <button type="button" onClick={() => setCurrentNumber(2)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(2);
+            render();
+          }}
+        >
           2
         </button>
-        <button type="button" onClick={() => setCurrentNumber(3)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(3);
+            render();
+          }}
+        >
           3
         </button>
-        <button type="button" onClick={() => setCurrentNumber(4)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(4);
+            render();
+          }}
+        >
           4
         </button>
-        <button type="button" onClick={() => setCurrentNumber(5)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(5);
+            render();
+          }}
+        >
           5
         </button>
-        <button type="button" onClick={() => setCurrentNumber(6)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(6);
+            render();
+          }}
+        >
           6
         </button>
-        <button type="button" onClick={() => setCurrentNumber(7)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(7);
+            render();
+          }}
+        >
           7
         </button>
-        <button type="button" onClick={() => setCurrentNumber(8)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(8);
+            render();
+          }}
+        >
           8
         </button>
-        <button type="button" onClick={() => setCurrentNumber(9)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(9);
+            render();
+          }}
+        >
           9
         </button>
-        <button type="button" onClick={() => setCurrentNumber(0)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCurrentNumber(0);
+            render();
+          }}
+        >
           0
         </button>
       </div>
