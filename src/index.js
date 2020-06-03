87a3340548a0673de5aecac4f711113566972f26
morykwon <kwonmory@gmail.com>
2020-06-03 16:11:40 +0900

    Implement Difficult Simple Caculator

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 
 /* @jsx createElement */
 
+/*
+  [나의 목표]
+  함수는 단 한가지 일만 하도록 만들어보자.
+*/
+
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
@@ -20,10 +25,77 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+const numbers = [...Array.from({ length: 9 }, (v, i) => i + 1), 0]; // 1 ~ 9 ~ 0
+const operator = ['+', '-', '*', '/', '='];
+
+function calc([firstNumber, operation, secondNumber]) {
+  let acc = Number(firstNumber);
+  if (operation === '+') {
+    acc += Number(secondNumber);
+  } else if (operation === '-') {
+    acc -= Number(secondNumber);
+  } else if (operation === '*') {
+    acc *= Number(secondNumber);
+  } else if (operation === '/') {
+    acc /= Number(secondNumber);
+  }
+
+  return acc;
+}
+
+function render({ number, operationAndNumberArr, isNewNumber }) {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{number}</p>
+      <p>
+        {numbers.map((value) => (
+          <button
+            type="button"
+            onClick={() => {
+              let newNumber = number;
+              let newIsNewNumber = isNewNumber;
+
+              if (newIsNewNumber) {
+                newNumber = '';
+                newIsNewNumber = false;
+              }
+
+              newNumber = Number(String(newNumber) + value);
+              render({
+                number: newNumber,
+                operationAndNumberArr,
+                isNewNumber: newIsNewNumber,
+              });
+            }}
+          >
+            {value}
+          </button>
+        ))}
+      </p>
+      <p>
+        {operator.map((value) => (
+          <button
+            type="button"
+            onClick={() => {
+              operationAndNumberArr.push(number);
+              operationAndNumberArr.push(value);
+
+              if (operationAndNumberArr.length > 2) {
+                operationAndNumberArr.unshift(calc(operationAndNumberArr.splice(0, 3)));
+              }
+
+              render({
+                number: operationAndNumberArr[0],
+                operationAndNumberArr,
+                isNewNumber: true,
+              });
+            }}
+          >
+            {value}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
@@ -31,4 +103,8 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render({
+  number: 0,
+  operationAndNumberArr: [],
+  isNewNumber: false,
+});
