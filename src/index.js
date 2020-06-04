09fb0979224984b0cefb3673b56d94cebb0090da
daadaadaah <daadaadaah@gmail.com>
2020-06-04 17:17:32 +0900

    Fix no-use-before-define lint error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-use-before-define */
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
 
 /* @jsx createElement */
@@ -42,57 +41,57 @@ const calculations = [
   },
 ];
 
-function calculate(calculationName) {
-  const operates = {
-    plus(operand1, operand2) {
-      return operand1 + operand2;
-    },
-    minus(operand1, operand2) {
-      return operand1 - operand2;
-    },
-    multiply(operand1, operand2) {
-      return operand1 * operand2;
-    },
-    divide(operand1, operand2) {
-      return operand1 / operand2;
-    },
-  };
-
-  return operates[calculationName];
-}
-
-function getOperand2(operand2, number) {
-  return operand2 === 0 ? number : operand2 * 10 + number;
-}
-
-function handleClickNumber(operand1, calculationName, operand2, number) {
-  render({ operand1, calculationName, operand2: getOperand2(operand2, number) }, operand2);
-}
-
-function calculateNumber(calculationName, operand1, operand2) {
-  const result = calculate(calculationName)(operand1, operand2);
-  return result;
-}
-
-function getOperand1(operand1, calculationName, operand2, inputCalculation) {
-  if (calculationName !== '') return calculateNumber(calculationName, operand1, operand2);
-  return operand1 === 0
-    ? operand2
-    : calculateNumber(inputCalculation, operand1, operand2);
-}
-
-function handleClickCalculation(operand1, prevCalculation, operand2, inputCalculation) {
-  const intermediateResults = getOperand1(operand1, prevCalculation, operand2, inputCalculation);
-  render({ operand1: intermediateResults, calculationName: inputCalculation, operand2: 0 },
-    intermediateResults);
-}
-
-function handleClickShowTotal(operand1, calculationName, operand2) {
-  const total = calculateNumber(calculationName, operand1, operand2);
-  render({ operand1: 0, calculationName: '', operand2: 0 }, total);
-}
 
 function render(formula, showNumber = 0) {
+  const calculate = (calculationName) => {
+    const operates = {
+      plus(operand1, operand2) {
+        return operand1 + operand2;
+      },
+      minus(operand1, operand2) {
+        return operand1 - operand2;
+      },
+      multiply(operand1, operand2) {
+        return operand1 * operand2;
+      },
+      divide(operand1, operand2) {
+        return operand1 / operand2;
+      },
+    };
+
+    return operates[calculationName];
+  };
+
+  const calculateNumber = (calculationName, operand1, operand2) => {
+    const result = calculate(calculationName)(operand1, operand2);
+    return result;
+  };
+
+  const getOperand2 = (operand2, number) => (operand2 === 0 ? number : operand2 * 10 + number);
+
+  const handleClickNumber = (operand1, calculationName, operand2, number) => {
+    const nowNumber = getOperand2(operand2, number);
+    render({ operand1, calculationName, operand2: nowNumber }, nowNumber);
+  };
+
+  const getOperand1 = (operand1, calculationName, operand2, inputCalculation) => {
+    if (calculationName !== '') return calculateNumber(calculationName, operand1, operand2);
+    return operand1 === 0
+      ? operand2
+      : calculateNumber(inputCalculation, operand1, operand2);
+  };
+
+  const handleClickCalculation = (operand1, prevCalculation, operand2, inputCalculation) => {
+    const middleResults = getOperand1(operand1, prevCalculation, operand2, inputCalculation);
+    render({ operand1: middleResults, calculationName: inputCalculation, operand2: 0 },
+      middleResults);
+  };
+
+  const handleClickShowTotal = (operand1, calculationName, operand2) => {
+    const total = calculateNumber(calculationName, operand1, operand2);
+    render({ operand1: 0, calculationName: '', operand2: 0 }, total);
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
