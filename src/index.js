1365c66f67b92442d629677cc1002c0cf6f4e103
Joylish <yeonzzu2@gmail.com>
2020-06-05 05:21:58 +0900

    initial trial to make calculator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
 
 /* @jsx createElement */
@@ -19,11 +20,97 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
+let flagOperation = false;
+let flagEqual = false;
+let operatorNum = 0;
+let operator = '';
+let value = '0';
+let result = 0;
+
+const calculate = (a, op, b) => {
+  if (op === '+') {
+    // console.log(`a::${a} b::${b}`);
+    // console.log(Number(a) + Number(b));
+    return Number(a) + Number(b);
+  }
+  if (op === '-') {
+    return Number(a) - Number(b);
+  }
+  if (op === '*') {
+    return Number(a) * Number(b);
+  }
+  if (op === '/') {
+    return Number(a) / Number(b);
+  }
+  return false;
+};
+
+function buttonclick(i) {
+  console.log(flagOperation);
+  if (flagEqual) {
+    value = i;
+    flagOperation = false;
+    flagEqual = false;
+    render();
+    return;
+  }
+  if (flagOperation) {
+    if (operatorNum > 1) {
+      result = calculate(result, operator, i);
+      // console.log(`value::${value} i::${i} result:${result}`);
+      value = result;
+    } else {
+      result = calculate(value, operator, i);
+      // console.log(`value::${value} i::${i} result:${result}`);
+      value = i;
+    }
+    flagOperation = false;
+  } else if (value === '0') {
+    value = i;
+  } else {
+    value += i;
+  }
+  render();
+}
+
+function checkOperator(nowOperator) {
+  operator = nowOperator;
+  operatorNum += 1;
+  flagOperation = true;
+  if (nowOperator === '=') {
+    value = result;
+    operatorNum = 0;
+    result = 0;
+    flagEqual = true;
+  }
+  render();
+}
 
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>
+        {value}
+      </p>
+      <p>
+        {['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].map((i) => (
+          <button type="button" onClick={() => buttonclick(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {' '}
+        {['+', '-', '*', '/', '='].map((nowOperator) => (
+          <button
+            type="button"
+            onClick={() => checkOperator(nowOperator)}
+          >
+            {nowOperator}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
--- test/calculator_test.js
+++ test/calculator_test.js
@@ -1,3 +1,4 @@
+/* eslint-disable implicit-arrow-linebreak */
 Feature('Calculator');
 
 Scenario('계산기이 입력한 순서대로 계산하여 값을 출력합니다.', (I) => {
