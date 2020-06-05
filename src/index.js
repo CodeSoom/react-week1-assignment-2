55d56f99c4857210d96ab18194c3c7ac92016ea2
JangGeonil <>
2020-06-05 22:23:16 +0900

    간단한 계산기 만들기 업로드

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
 
 /* @jsx createElement */
-
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
-
   Object.entries(props || {}).forEach(([key, value]) => {
     element[key.toLowerCase()] = value;
   });
-
   children.flat().forEach((child) => {
     if (child instanceof Node) {
       element.appendChild(child);
@@ -16,7 +13,6 @@ function createElement(tagName, props, ...children) {
     }
     element.appendChild(document.createTextNode(child));
   });
-
   return element;
 }
 
