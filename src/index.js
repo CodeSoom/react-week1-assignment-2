9c5012f3dc483e278dc82c7d70b30df8a77063a6
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-05 16:53:02 +0900

    Submit assignment1-2 as the first try

--- src/index.js
+++ src/index.js
@@ -20,15 +20,114 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
-  const element = (
+function setElement(result, repository, handleClickNumber, handleClickOperator, handleClickEqual) {
+  // console.log(result);
+  // console.log(repository);
+
+  return (
     <div>
       <p>간단 계산기</p>
+      <p>{ result }</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (<button type="button" onClick={() => handleClickNumber(i, repository)}>{i}</button>))}
+      </p>
+      <p>
+        {['+', '-', '*', '/'].map((i) => (<button type="button" onClick={() => handleClickOperator(i, repository)}>{i}</button>))}
+        <button type="button" onClick={() => handleClickEqual(repository)}>=</button>
+      </p>
+
     </div>
   );
+}
+
+function render(result, repository) {
+  const isOperand = (value) => typeof value === 'number';
+
+  const isOperator = (value) => ['+', '-', '*', '/'].includes(value);
+
+  const isEqualSign = (value) => value === '=';
+
+  const readyToCalculate = (values) => values.length === 3;
+
+  const readyToSetOperator = (values) => values.length === 1;
+
+  const getResultForDisplay = (values) => values.filter(isOperand).slice(-1)[0];
+
+  // const hasOperator = (values) => values.filter(isOperator).length > 0;
+
+  // const isEqualitySign = (value) => value === '=';
+
+  const calculate = (firstOperand, operator, secondOperand) => {
+    console.log(operator);
+    switch (operator) {
+    case '+':
+      return firstOperand + secondOperand;
+    case '-':
+      return firstOperand - secondOperand;
+    case '*':
+      return firstOperand * secondOperand;
+    case '/':
+      return firstOperand / secondOperand;
+    default:
+      return;
+    }
+  };
+
+  const handleClickNumber = (number, currentRepository) => {
+    const lastValue = currentRepository.pop();
+    if (isOperand(lastValue)) {
+      const newValue = lastValue * 10 + number;
+      currentRepository.push(newValue);
+      console.log(currentRepository);
+    } else if (isOperator(lastValue)) {
+      currentRepository.push(lastValue, number);
+      console.log(currentRepository);
+    } else if (isEqualSign(lastValue)) {
+      currentRepository.splice(0, currentRepository.length);
+      currentRepository.push(number);
+    }
+
+    const newResult = getResultForDisplay(currentRepository);
+
+    render(newResult, currentRepository);
+  };
+
+  const handleClickOperator = (operator, currentRepository) => {
+
+    if (readyToCalculate(currentRepository)) {
+      const newValue = calculate(...currentRepository);
+      currentRepository.splice(0, currentRepository.length);
+      currentRepository.push(newValue, operator);
+      console.log(currentRepository);
+    } else if (readyToSetOperator(currentRepository)) {
+      currentRepository.push(operator);
+      console.log(currentRepository);
+    }
+
+    const newResult = getResultForDisplay(currentRepository);
+
+    render(newResult, currentRepository);
+  };
+
+  const handleClickEqual = (currentRepository) => {
+    if (readyToCalculate(currentRepository)) {
+      const newValue = calculate(...currentRepository);
+      currentRepository.splice(0, currentRepository.length);
+      currentRepository.push(newValue, '=');
+      console.log(currentRepository);
+    }
+
+    const newResult = getResultForDisplay(currentRepository);
+    render(newResult, currentRepository);
+  };
+
+  const element = setElement(result, repository, handleClickNumber, handleClickOperator, handleClickEqual);
 
   document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(element);
 }
 
-render();
+const initialResult = 0;
+const initialRepository = [0];
+
+render(initialResult, initialRepository);
