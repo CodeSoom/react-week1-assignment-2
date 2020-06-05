1e31959ad71729fd6c43ab8cc12422cfcc554e57
Kwakcena <rhkrgudwh@gmail.com>
2020-06-05 13:41:27 +0900

    Feature) 계산기 완성
    수정사항
    - button의 숫자에 0을 추가함.
    - state 객체가 갖는 변수는 아래와 같음
    result: 계산된 결과를 저장.
    showNumber: 버튼을 누르면 화면에 보여지는 숫자.
    expression: 누른 숫자나 연산자가 쌓이는 배열.
    
    - 계산은 어떻게 하는가?
    연산자 버튼을 누르면 계산을 수행한다. calculator 함수는 인자로
    expression 배열에 현재 화면에 표시된 숫자를 포함하여 받는다.
    여기서 숫자와 연산자를 따로 분리하여 배열에 담고, reduce를 통해 계산이
    수행된다. 수행된 결과가 result에 담기게 된다.
    
    - showNumber를 보여줄 때와 result를 보여줄 때를 나누는 방법?
    숫자 버튼을 누르면 누른 숫자가 화면에 표시되어야 하고, 연산자나 = 버튼을
    누르면 계산된 결과가 화면에 표시되어야 한다. 이를 위해서 버튼을 누를때는
    result 값이 undefined 인 점을 이용해 result || showNumber 연산을
    활용했다. result에 값이 없으면 showNumber의 값을 보여주고, 연산자를 눌러
    계산 결과가 존재하면 result의 값을 보여주도록 한다.
    
    - '='을 눌렀을 땐 기존의 계산식 초기화?
    만약 연산자로 눌린 것이 = 이라면 expression은 빈 배열로 초기화 한다.
    그래야 다음 계산을 수행하는데 버그가 발생하지 않는다.
    
    고찰
    - calculator의 reduce가 너무 지저분하다... 뭔가 중복되어 보이는데 이걸
      해결할 수 있는 방법은 뭘까?
    - handleClickNumber나 handleClickOperator의 매개변수로 보내는 부분 또한
      중복되어 보이는데 이대로 괜찮은가?
    - expression이란 표현은 적절한가?

--- src/index.js
+++ src/index.js
@@ -20,29 +20,68 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const operators = ['+', '-', '*', '/', '='];
+const buttonNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const buttonOperators = ['+', '-', '*', '/', '='];
 const state = {
   result: 0,
+  showNumber: 0,
+  expression: [],
 };
 
-const handleClickNumber = (currentNumber, number) => render(
-  { result: currentNumber * 10 + number },
-);
+const handleClickNumber = (showNumber, expression, number) => render({
+  expression,
+  showNumber: showNumber * 10 + number,
+});
 
-function render({ result }) {
+const handleClickOperator = (showNumber, expression, operator) => render({
+  result: calculator([...expression, showNumber]),
+  showNumber: 0,
+  expression: operator === '=' ? [] : [...expression, showNumber, `${operator}`],
+});
+
+const calculator = (expression) => {
+  const operators = expression.filter((value) => buttonOperators.includes(value));
+  const numbers = expression.filter((value) => !buttonOperators.includes(value));
+  return numbers.reduce((acc, value, index) => {
+    if (operators[index - 1] === '+') {
+      return parseFloat(acc) + Number(value);
+    }
+    if (operators[index - 1] === '-') {
+      return parseFloat(acc) - Number(value);
+    }
+    if (operators[index - 1] === '*') {
+      return parseFloat(acc) * Number(value);
+    }
+    if (operators[index - 1] === '/') {
+      return parseFloat(acc) / Number(value);
+    }
+    return acc;
+  });
+};
+
+function render({ result, showNumber, expression }) {
   const element = (
     <div>
       <p>간단 계산기</p>
-      <div id="result">{result}</div>
+      <div id="result">{result || showNumber}</div>
       <p id="button-list">
-        {numbers.map((number) => (
-          <button type="button" onClick={() => handleClickNumber(result, number)}>{number}</button>
+        {buttonNumbers.map((number) => (
+          <button
+            type="button"
+            onClick={() => handleClickNumber(showNumber, expression, number)}
+          >
+            {number}
+          </button>
         ))}
       </p>
       <p id="operator-list">
-        {operators.map((operator) => (
-          <button type="button">{operator}</button>
+        {buttonOperators.map((operator) => (
+          <button
+            type="button"
+            onClick={() => handleClickOperator(showNumber, expression, operator)}
+          >
+            {operator}
+          </button>
         ))}
       </p>
     </div>
