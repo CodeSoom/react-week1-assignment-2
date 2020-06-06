c087a56ae4d0d5e66495ae77158165e5c6101890
SeaFalcon <seafalcon323@gmail.com>
2020-06-07 01:03:18 +0900

    Simple Calculator
    나의 고민사항
    계산기 만들어본적이 없음
    
    * 첫번째 전개방식
    
      번호를 눌렀을때 숫자가 저장될 공간 -> 좌변수라고 치자
      추가로 번호를 눌렀을때는 10의자리가 입력되어야 함
      연산자가 눌리기 전 숫자버튼은 무조건 좌변수에 입력됨
    
      연산자를 눌렀을 경우에는 좌변수에 대한 입력대기가 끝나고
      연산자 변수에 연산자가 입력 되어야 할듯
    
      연산자 입력 후에 숫자를 누른다면
      우변수에 숫자가 입력되기 시작
      마찬가지로 연산자가 입력되기 전까지는 숫자버튼은 눌렀을 때 무조건 우변수에 입력
    
      = 연산자가 나올경우 끝이나므로
      좌변수, 연산자, 우변수 모두 초기화
    
      그 외의 다른 연산자가 나올경우
      ex) 좌 = 10, 연산자 = ‘+’, 우 = 20
      두 값의 계산 결과를 좌변수에 저장하고
      연산자와 우변수 초기화
    
      연산자 변수에 값이 있냐없냐로 좌/우 변수에 넣을 값 판단
      조건 산출 고고
    
      숫자입력시
      만약 (연산자 변수가 비었음)
        좌변수에 삽입
      만약 (연산자 변수에 값이 존재)
        우변수에 삽입
    
      연산자입력시
      만약 (연산자 변수가 비었음)
        연산자 변수에 삽입
      만약 (연산자 변수에 값이 존재)
        만약 (우변수가 비었음)
          연산자 변수 값 교체
        만약 (우변수에 값이 존재)
          좌변수에 계산된 값 대입
          연산자 변수, 우변수 초기화
          연산자 변수 값 교체
    
      변수를 좌변, 연산자, 우변 세 개 사용하면서
      무식하게 스스로 테스트해보며 만들었지만 실패
      인수테스트를 실행 후
      테스트 조건으로 부터 역으로 실행하며 디버깅해보려 했지만 도저히 안돼서 실패
    
    * 두번째 전개 방식
    	연산자와 누계 변수 만을 사용해서
    	인수테스트 조건을 맞춰보려 했으나 역시 실패
    
    * 세번째 전개 방식
    	현재 숫자, 이전 숫자, 연산자, 누계 변수를 사용하여
    	인수테스트 조건을 일단 모두 만족하는 결과 값을 도출해 냄.
    
    계산기 짜는 로직에 대한 부분이 많이 부족하다는 생각.
    첫번째 과제에서 외부 변수를 사용하지 않도록 유도 하였으나
    도저히 계산기 문제에서 변수를 사용하지 않고는 만들 방법을 생각해내지 못함.
    
    억지로 결과를 충족시켰지만
    43 + 21 같은 부분에서 두번째 21을 입력받을 때 두자리 수를 입력받도록 하는 부분이 어려웠습니다.

--- src/index.js
+++ src/index.js
@@ -20,10 +20,75 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+let pre = 0;
+let cur = 0;
+let op = '';
+let acc = null;
+
+function render(count) {
+  function handleNumberClick(val) {
+    if (op === '') {
+      cur = Number(cur + String(val));
+      return render(cur);
+    }
+    cur = Number(cur + String(val));
+    return render(cur);
+  }
+  function handleOperatorClick(oper) {
+    if (oper === '=') {
+      if (op === '+') {
+        acc = (acc === null) ? (pre + cur) : (acc + cur);
+      } else if (op === '-') {
+        acc = (acc === null) ? (pre - cur) : (acc - cur);
+      } else if (op === '*') {
+        acc = (acc === null) ? (pre * cur) : (acc * cur);
+      } else if (op === '/') {
+        acc = (acc === null) ? (pre / cur) : (acc / cur);
+      }
+      op = null;
+      pre = 0;
+      cur = 0;
+      return render(acc);
+    }
+    if (op !== '') {
+      if (op === '+') {
+        acc = (acc === null) ? (pre + cur) : (acc + cur);
+      } else if (op === '-') {
+        acc = (acc === null) ? (pre - cur) : (acc - cur);
+      } else if (op === '*') {
+        acc = (acc === null) ? (pre * cur) : (acc * cur);
+      } else if (op === '/') {
+        acc = (acc === null) ? (pre / cur) : (acc / cur);
+      }
+      op = oper;
+      cur = 0;
+      return render(acc);
+    }
+    op = oper;
+    pre = cur;
+    cur = 0;
+    return render(count);
+  }
   const element = (
     <div>
       <p>간단 계산기</p>
+
+      <p>{count}</p>
+      <p>
+        {[...Array(10)].map((_, idx) => {
+          if (idx === 9) {
+            return (
+              <button type="button" onClick={() => handleNumberClick(0)}>{0}</button>
+            );
+          }
+          return (
+            <button type="button" onClick={() => handleNumberClick((idx + 1))}>{idx + 1}</button>
+          );
+        })}
+      </p>
+      {['+', '-', '*', '/', '='].map((val) => (
+        <button type="button" onClick={() => handleOperatorClick(val)}>{val}</button>
+      ))}
     </div>
   );
 
@@ -31,4 +96,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render(0);
