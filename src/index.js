717a2834d7b7a8e482dbb422dbe3a93e7ab055fa
Paul An <anpaul0615@gmail.com>
2020-06-06 11:06:37 +0900

    "과제 2 - 간단한 사칙 연산 계산기 구현" 최초 작성

--- src/index.js
+++ src/index.js
@@ -20,10 +20,59 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+let leftNumber = '';
+let operator = '';
+let rightNumber = '';
+
+const calculate = (lft, opr, rgt) => {
+  if (opr === '+') return lft + rgt;
+  if (opr === '-') return lft - rgt;
+  if (opr === '*') return lft * rgt;
+  if (opr === '/') return lft / rgt;
+  throw Error('dd');
+};
+
+function render(result = '') {
+  const handleClickNumber = (num) => {
+    if (typeof num !== 'number') return;
+    if (operator === '') leftNumber += String(num);
+    else rightNumber += String(num);
+    render(rightNumber !== '' ? rightNumber : leftNumber);
+  };
+
+  const handleClickOperator = (opr) => {
+    if (typeof opr !== 'string') return;
+    if (leftNumber === '') return;
+    if (rightNumber === '') operator = opr;
+    else {
+      leftNumber = calculate(Number(leftNumber), operator, Number(rightNumber));
+      rightNumber = '';
+      operator = opr;
+    }
+    render(rightNumber !== '' ? rightNumber : leftNumber);
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{result}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+          <button type="button" onClick={() => handleClickNumber(num)}>
+            {num}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/'].map((opr) => (
+          <button type="button" onClick={() => handleClickOperator(opr)}>
+            {opr}
+          </button>
+        ))}
+        <button type="button" onClick={() => render(calculate(Number(leftNumber), operator, Number(rightNumber)))}>
+          =
+        </button>
+      </p>
     </div>
   );
 
