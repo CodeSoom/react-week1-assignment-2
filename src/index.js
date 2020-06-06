4ca40494a0e983f7a6b7ef4b8e96702c9f4b8c13
Joylish <yeonzzu2@gmail.com>
2020-06-07 07:32:52 +0900

    updating code.. transfer from let variables to parameter of render() change calculate using new Function()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-use-before-define */
+/* eslint-disable no-new-func */
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -20,102 +19,159 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
-let flagOperation = false;
-let flagEqual = false;
-let operatorNum = 0;
-let operator = '';
-let value = '0';
-let result = 0;
+function render(operandA = '0', operandB = '-1', operator = '', operatedNumber = 0, afterResult = false) {
+  console.log(
+    `operandA::${operandA} operandB::${operandB} typeOperandB::${typeof (operandB)} operator:: ${operator} operatedNumber:: ${operatedNumber} resultNumber: ${resultNumber}`,
+  );
 
-const calculate = (a, op, b) => {
-  if (op === '+') {
-    // console.log(`a::${a} b::${b}`);
-    // console.log(Number(a) + Number(b));
-    return Number(a) + Number(b);
-  }
-  if (op === '-') {
-    return Number(a) - Number(b);
-  }
-  if (op === '*') {
-    return Number(a) * Number(b);
-  }
-  if (op === '/') {
-    return Number(a) / Number(b);
-  }
-  return false;
-};
+  const handleClickedNumber = (userNumber) => {
+    // if ((operatedNumber >= 2) || operandA === '0') render(userNumber, operandB, operator, operatedNumber, afterResult);
+    // else if (operator !== '') {
+    //   if (afterResult) render(userNumber, false)
+    //   render(userNumber, operandA, operator, operatedNumber, afterResult);
+    // }else render(operandA.concat(userNumber), operandB, operator, operatedNumber, afterResult);
+  };
 
-function buttonclick(i) {
-  console.log(flagOperation);
-  if (flagEqual) {
-    value = i;
-    flagOperation = false;
-    flagEqual = false;
-    render();
-    return;
-  }
-  if (flagOperation) {
-    if (operatorNum > 1) {
-      result = calculate(result, operator, i);
-      // console.log(`value::${value} i::${i} result:${result}`);
-      value = result;
+  const calculate = (expression) => new Function(`return String(${expression})`)();
+
+  const handleClickedOpertor = (userOperator) => {
+    const result = String(operandB.concat(operator, operandA));
+    if (operandB === '-1') {
+      render(operandA, operandB, userOperator, operatedNumber + 1, afterResult);
+    } else if (userOperator === '=') {
+      render(calculate(result), '-1', '', 0, true);
     } else {
-      result = calculate(value, operator, i);
-      // console.log(`value::${value} i::${i} result:${result}`);
-      value = i;
+      render(
+        operandA,
+        calculate(result),
+        userOperator,
+        operatedNumber + 1,
+        afterResult,
+      );
     }
-    flagOperation = false;
-  } else if (value === '0') {
-    value = i;
-  } else {
-    value += i;
-  }
-  render();
-}
-
-function checkOperator(nowOperator) {
-  operator = nowOperator;
-  operatorNum += 1;
-  flagOperation = true;
-  if (nowOperator === '=') {
-    value = result;
-    operatorNum = 0;
-    result = 0;
-    flagEqual = true;
-  }
-  render();
-}
-
-function render() {
+  };
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{operandA}</p>
       <p>
-        {value}
+        {['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].map(
+          (numberPlate) => (
+            <button
+              type="button"
+              onClick={() => {
+                handleClickedNumber(numberPlate);
+              }}
+            >
+              {numberPlate}
+            </button>
+          ),
+        )}
       </p>
       <p>
-        {['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].map((i) => (
-          <button type="button" onClick={() => buttonclick(i)}>
-            {i}
-          </button>
-        ))}
-      </p>
-      <p>
-        {' '}
-        {['+', '-', '*', '/', '='].map((nowOperator) => (
+        {['+', '-', '*', '/', '='].map((operatorPanel) => (
           <button
             type="button"
-            onClick={() => checkOperator(nowOperator)}
+            onClick={() => handleClickedOpertor(operatorPanel)}
           >
-            {nowOperator}
+            {operatorPanel}
           </button>
         ))}
       </p>
     </div>
   );
-
   document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(element);
 }
-
 render();
+// let flagOperation = false;
+// let flagEqual = false;
+// let operatorNum = 0;
+// let operator = '';
+// let value = '0';
+// let result = 0;
+
+// const calculate = (a, op, b) => {
+//   if (op === '+') {
+//     // console.log(`a::${a} b::${b}`);
+//     // console.log(Number(a) + Number(b));
+//     return Number(a) + Number(b);
+//   }
+//   if (op === '-') {
+//     return Number(a) - Number(b);
+//   }
+//   if (op === '*') {
+//     return Number(a) * Number(b);
+//   }
+//   if (op === '/') {
+//     return Number(a) / Number(b);
+//   }
+//   return false;
+// };
+
+// function buttonclick(i) {
+//   if (flagEqual) {
+//     value = i;
+//     flagOperation = false;
+//     flagEqual = false;
+//     render();
+//     return;
+//   }
+//   if (flagOperation) {
+//     result = result + operator + i;
+//     console.log(result);
+//     if (operatorNum > 1) value = result;
+//     else value = i;
+//     flagOperation = false;
+//   }
+//   else if (value === '0') value = i;
+//   else value += i;
+//   render();
+// }
+
+// function checkOperator(nowOperator) {
+//   operator = nowOperator;
+//   operatorNum += 1;
+//   flagOperation = true;
+//   if (nowOperator === '=') {
+//     value = result;
+//     operatorNum = 0;
+//     result = 0;
+//     flagEqual = true;
+//   }
+//   render();
+// }
+
+// function render() {
+//   const element = (
+//     <div>
+//       <p>간단 계산기</p>
+//       <p>
+//         {value}
+//       </p>
+//       <p>
+//         {['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].map((i) => (
+//           <button type="button" onClick={() => buttonclick(i)}>
+//             {i}
+//           </button>
+//         ))}
+//       </p>
+//       <p>
+//         {' '}
+//         {['+', '-', '*', '/', '='].map((nowOperator) => (
+//           <button
+//             type="button"
+//             onClick={() => checkOperator(nowOperator)}
+//           >
+//             {nowOperator}
+//           </button>
+//         ))}
+//       </p>
+//     </div>
+//   );
+
+//   document.getElementById('app').textContent = '';
+//   document.getElementById('app').appendChild(element);
+// }
+
+// render();
