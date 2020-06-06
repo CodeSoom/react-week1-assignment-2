5125aae4c1126b606f7f4ca4950e390afc5de93d
KiChang Shin <kc7851@gmail.com>
2020-06-06 10:13:44 +0900

    Set button UI
    계산기 화면 UI구성.
    기능 미구현.

--- src/index.js
+++ src/index.js
@@ -23,7 +23,34 @@ function createElement(tagName, props, ...children) {
 function render() {
   const element = (
     <div>
-      <p>간단 계산기</p>
+      <p>
+        간단 계산기
+      </p>
+      <p>
+        {input}
+      </p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button
+            type="button"
+            onClick={() => {
+            }}
+          >
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => (
+          <button
+            type="button"
+            onClick={() => {
+            }}
+          >
+            {i}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
