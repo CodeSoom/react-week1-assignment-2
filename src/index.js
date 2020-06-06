7c809565c51de07ef9719020ad2918567b3080dc
yeonhwa <yeonhwa6717@gmail.com>
2020-06-07 07:54:40 +0900

    모든 인수테스트 통과

--- src/index.js
+++ src/index.js
@@ -23,27 +23,16 @@ function createElement(tagName, props, ...children) {
 
 
 
+function calculate(operator, calculation, current) {
+  const operators = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+  }
+  return operator === '=' ? current*1 : operators[operator](calculation*1, current*1)
 
-function add(a, b) {
-  return a + b
 }
-
-function minus(a, b) {
-  console.log(a-b)
-  return a - b
-}
-
-function multiply(a, b) {
-  return a * b
-}
-
-function divide(a, b) {
-  console.log(a)
-  console.log(b)
-  console.log(a/b)
-  return a / b
-}
-
 
 function render(current = 0, reset = false, operator='=', calculation = '') {
   const element = (
@@ -59,7 +48,7 @@ function render(current = 0, reset = false, operator='=', calculation = '') {
               reset ? index : [current * 1 === 0 ? '' : current, index].join(''),
               false,
               operator,
-              calculation,
+              calculate(operator, calculation, index),
             )}              
           >
             {index}
@@ -67,11 +56,11 @@ function render(current = 0, reset = false, operator='=', calculation = '') {
         ))}
       </p>
       <p>
-        <button type="button" onClick={() => {operator === '=' ? render(current*1, true, '+', current*1) : render(add(calculation*1, current*1), true, '+', add(calculation*1, current*1))}}>+</button>
-        <button type="button" onClick={() => {operator === '=' ? render(current*1, true, '-', current*1) : render(minus(calculation*1, current*1), true, '-', minus(calculation*1, current*1))}}>-</button>
-        <button type="button" onClick={() => {operator === '=' ? render(current*1, true, '*', current*1) : render(multiply(calculation*1, current*1), true, '*', multiply(calculation*1, current*1))}}>*</button>
-        <button type="button" onClick={() => {operator === '=' ? render(current*1, true, '/', current*1) : render(divide(calculation*1, current*1), true, '/', divide(calculation*1, current*1))}}>/</button>
-        <button type="button" onClick={() => {render(multiply(calculation*1, current*1), true, '=', 0)}}>=</button>
+        <button type="button" onClick={() => {operator !== '+' ? render(operator === '-' ? calculation*1 : current*1, true, '+', calculation*1) : render(calculation*1, true, '+', calculation*1)}}>+</button>
+        <button type="button" onClick={() => {operator !== '-' ? render(current*1, true, '-', calculation*1) : render(calculation*1, true, '-', calculation*1)}}>-</button>
+        <button type="button" onClick={() => {operator !== '*' ? render(current*1, true, '*', calculation*1) : render(calculation*1, true, '*', calculation*1)}}>*</button>
+        <button type="button" onClick={() => {operator !== '/' ? render(current*1, true, '/', calculation*1) : render(calculation*1, true, '/', calculation*1)}}>/</button>
+        <button type="button" onClick={() => {render(calculation*1, true, '=', 0)}}>=</button>
       </p>
     </div>
   );
