4637df19de00ee499d90b7390f7634b19d58124e
newoo <45201399+newoo@users.noreply.github.com>
2020-06-07 17:20:05 +0900

    Update codes about no-use-before-define lint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-/* eslint no-use-before-define: ["error", { "functions": false }] */
 
 /* @jsx createElement */
 
 const rhsDefaultValue = -1;
+const calculation = {
+  '+': (lhs, rhs) => lhs + rhs,
+  '-': (lhs, rhs) => lhs - rhs,
+  '*': (lhs, rhs) => lhs * rhs,
+  '/': (lhs, rhs) => lhs / rhs,
+};
 
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
@@ -23,62 +28,49 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function handleClickNumber(lhs, rhs, mark, value) {
-  switch (mark) {
-  case '+':
-  case '-':
-  case '*':
-  case '/': {
-    const newRhs = checkRhsIfAddTail() ? value : (rhs * 10) + value;
-    render(lhs, newRhs, mark);
-    break;
-  }
-  default: {
-    const newLhs = lhs === 0 || mark === '=' ? value : (lhs * 10) + value;
-    if (mark === '=') {
-      render(newLhs, rhs, '');
-    } else {
-      render(newLhs, rhs, mark);
-    }
-    break;
-  }
-  }
-
-  function checkRhsIfAddTail() {
-    return rhs === 0 || rhs === rhsDefaultValue;
-  }
-}
-
-function handleClickCalculationMark(lhs, rhs, mark, value) {
-  if (value === '=' || rhs > -1) {
-    if (mark === '+') render(lhs + rhs, rhsDefaultValue, value);
-    if (mark === '-') render(lhs - rhs, rhsDefaultValue, value);
-    if (mark === '*') render(lhs * rhs, rhsDefaultValue, value);
-    if (mark === '/') render(lhs / rhs, rhsDefaultValue, value);
-  } else {
-    render(lhs, rhs, value);
-  }
-}
-
 function printNumber(lhs, rhs) {
   return rhs > -1 ? rhs : lhs;
 }
 
 function render(lhs, rhs, mark) {
+  function handleClickCalculationMark(value) {
+    if (value === '=' || rhs > rhsDefaultValue) {
+      const newLhs = calculation[mark](lhs, rhs);
+      render(newLhs, rhsDefaultValue, value);
+    } else {
+      render(lhs, rhs, value);
+    }
+  }
+
+  function handleClickNumber(value) {
+    function checkRhsIfAddTail() {
+      return rhs === 0 || rhs === rhsDefaultValue;
+    }
+
+    if (Object.prototype.hasOwnProperty.call(calculation, mark)) {
+      const newRhs = checkRhsIfAddTail() ? value : (rhs * 10) + value;
+      render(lhs, newRhs, mark);
+    } else {
+      const newLhs = lhs === 0 || mark === '=' ? value : (lhs * 10) + value;
+      const newMark = mark === '=' ? '' : mark;
+      render(newLhs, rhs, newMark);
+    }
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
       <p>{printNumber(lhs, rhs)}</p>
       <p>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
-          <button type="button" onClick={() => handleClickNumber(lhs, rhs, mark, i)}>
+          <button type="button" onClick={() => handleClickNumber(i)}>
             {i}
           </button>
         ))}
       </p>
       <p>
         {['+', '-', '*', '/', '='].map((m) => (
-          <button type="button" onClick={() => handleClickCalculationMark(lhs, rhs, mark, m)}>
+          <button type="button" onClick={() => handleClickCalculationMark(m)}>
             {m}
           </button>
         ))}
