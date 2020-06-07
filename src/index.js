1ba5cf519630377579133b65a7c9a46931954885
newoo <45201399+newoo@users.noreply.github.com>
2020-06-07 19:04:36 +0900

    Update name of parameters

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 
 /* @jsx createElement */
 
-const rhsDefaultValue = -1;
+const addingNumberDefaultValue = -1;
 const calculation = {
-  '+': (lhs, rhs) => lhs + rhs,
-  '-': (lhs, rhs) => lhs - rhs,
-  '*': (lhs, rhs) => lhs * rhs,
-  '/': (lhs, rhs) => lhs / rhs,
+  '+': (addedNumber, addingNumber) => addedNumber + addingNumber,
+  '-': (addedNumber, addingNumber) => addedNumber - addingNumber,
+  '*': (addedNumber, addingNumber) => addedNumber * addingNumber,
+  '/': (addedNumber, addingNumber) => addedNumber / addingNumber,
 };
 
 function createElement(tagName, props, ...children) {
@@ -28,46 +28,46 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function printNumber(lhs, rhs) {
-  return rhs > -1 ? rhs : lhs;
+function printNumber(addedNumber, addingNumber) {
+  return addingNumber > -1 ? addingNumber : addedNumber;
 }
 
 function render({
-  lhs = 0,
-  rhs = rhsDefaultValue,
+  addedNumber = 0,
+  addingNumber = addingNumberDefaultValue,
   mark = '',
 } = {}) {
   function handleClickCalculationMark(value) {
-    if (value === '=' || rhs > rhsDefaultValue) {
+    if (value === '=' || addingNumber > addingNumberDefaultValue) {
       render({
-        lhs: calculation[mark](lhs, rhs),
-        rhs: rhsDefaultValue,
+        addedNumber: calculation[mark](addedNumber, addingNumber),
+        addingNumber: addingNumberDefaultValue,
         mark: value,
       });
     } else {
       render({
-        lhs,
-        rhs,
+        addedNumber,
+        addingNumber,
         mark: value,
       });
     }
   }
 
   function handleClickNumber(value) {
-    function checkRhsIfAddTail() {
-      return rhs === 0 || rhs === rhsDefaultValue;
+    function checkaddingNumberIfAddTail() {
+      return addingNumber === 0 || addingNumber === addingNumberDefaultValue;
     }
 
     if (Object.prototype.hasOwnProperty.call(calculation, mark)) {
       render({
-        lhs,
-        rhs: checkRhsIfAddTail() ? value : (rhs * 10) + value,
+        addedNumber,
+        addingNumber: checkaddingNumberIfAddTail() ? value : (addingNumber * 10) + value,
         mark,
       });
     } else {
       render({
-        lhs: lhs === 0 || mark === '=' ? value : (lhs * 10) + value,
-        rhs,
+        addedNumber: addedNumber === 0 || mark === '=' ? value : (addedNumber * 10) + value,
+        addingNumber,
         mark: mark === '=' ? '' : mark,
       });
     }
@@ -76,7 +76,7 @@ function render({
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{printNumber(lhs, rhs)}</p>
+      <p>{printNumber(addedNumber, addingNumber)}</p>
       <p>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
           <button type="button" onClick={() => handleClickNumber(i)}>
