c32e7d25bedace8e0e9d3dcb5575ebf70055e7cc
Hwang <iamhwang9@gmail.com>
2020-06-07 18:44:22 +0900

    Simple Calculator

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const { setHeadlessWhen } = require('@codeceptjs/configure');
 
 // turn on headless mode when running with HEADLESS=true environment variable
@@ -11,18 +12,18 @@ exports.config = {
     Puppeteer: {
       url: 'http://localhost:8080',
       show: true,
-      windowSize: '1200x900'
-    }
+      windowSize: '1200x900',
+    },
   },
   bootstrap: null,
   mocha: {},
   name: 'assignment-1-2',
   plugins: {
     retryFailedStep: {
-      enabled: true
+      enabled: true,
     },
     screenshotOnFail: {
-      enabled: true
-    }
-  }
-}
+      enabled: true,
+    },
+  },
+};
--- src/index.js
+++ src/index.js
@@ -20,10 +20,76 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+
+// x + y
+// x : left number, 결과값
+// resultNum
+let resultNum = 0;
+
+// y : right number, 입력 받을 숫자
+let inputNum = 0;
+// 연산자 변수, 첫 연산자 초기화
+let nowOp = '+';
+
+// 계산기 숫자 패드
+const calNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// 계산기 연산자
+const calOp = ['+', '-', '*', '/', '='];
+
+// 계산기 숫자 입력
+function calInput(i) {
+  if (inputNum === 0) {
+    inputNum = i;
+  } else inputNum = inputNum * 10 + i;
+
+  return inputNum;
+}
+
+function calEqual(left, right, op) {
+  inputNum = 0;
+  switch (op) {
+  case '+':
+    resultNum = left + right;
+    break;
+  case '-':
+    resultNum = left - right;
+    break;
+  case '*':
+    resultNum = left * right;
+    break;
+  case '/':
+    resultNum = left / right;
+    break;
+  case '=':
+    break;
+  default:
+  }
+}
+
+// 연산자 클릭시 동작
+function calFormula(op) {
+  calEqual(resultNum, inputNum, nowOp);
+
+  nowOp = op;
+
+  return resultNum;
+}
+
+function render(calValue) {
   const element = (
-    <div>
+    <div id="calculator">
       <p>간단 계산기</p>
+      <p>{calValue}</p>
+      <p>
+        {calNum.map((i) => (
+          <button type="button" onClick={() => render(calInput(i))}>{i}</button>
+        ))}
+      </p>
+      <p id="operator">
+        {calOp.map((i) => (
+          <button type="button" onClick={() => render(calFormula(i))}>{i}</button>
+        ))}
+      </p>
     </div>
   );
 
@@ -31,4 +97,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render(0);
