8b8119d40b1fdd26636219568610c8504202a07f
Joylish <yeonzzu2@gmail.com>
2020-06-07 22:04:03 +0900

    complete refactoring

--- src/index.js
+++ src/index.js
@@ -24,31 +24,26 @@ function createElement(tagName, props, ...children) {
 const calculate = (expression) => new Function(`return (${expression})`)();
 
 const handleClickedNumber = (data, number) => {
-  console.log(data);
-
-  const { operandA, expression } = data;
-
-  const showNumber = (operatorList.includes(expression.slice(-1))) ? number : operandA * 10 + number;
-  const expressions = (operatorList.includes(expression.slice(-1))) ? expression.concat(showNumber) : expression.concat(number);
+  const displayNumber = (operatorList.includes(data.expression.slice(-1))) ? number : data.displayNumber * 10 + number;
+  const expression = data.expression.concat((operatorList.includes(data.expression.slice(-1))) ? displayNumber : number);
 
   render({
-    operandA: showNumber,
-    operandB: undefined,
-    expression: expressions,
+    displayNumber,
+    expression,
   });
 };
 
 const handleClickedOpertor = (data, operator) => {
-  const { operandA, operandB } = data;
+  const { displayNumber } = data;
   const numbers = data.expression.replace(/[^0-9]/g, ' ').split(' ');
   const expression = data.expression.concat(operator);
-  if (Object.keys(numbers).length < 2) render({ operandA, operandB: operandA, expression });
+
+  if (Object.keys(numbers).length < 2) render({ displayNumber, expression });
   else {
     const result = calculate(expression.slice(0, -1));
     render({
-      operandA: result,
-      operandB: 0,
-      expression: (operator === '=') ? '' : String(result),
+      displayNumber: result,
+      expression: operator === '=' ? '' : `${String(result) + operator}`,
     });
   }
 };
@@ -56,17 +51,16 @@ const handleClickedOpertor = (data, operator) => {
 const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const operatorList = ['+', '-', '*', '/', '='];
 const initialData = {
-  operandA: 0,
-  operandB: 0,
+  displayNumber: 0,
   expression: '',
 };
 
 function render(data = initialData) {
-  const { operandA, operandB } = data;
+  const { displayNumber } = data;
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p id="result">{operandA || operandB}</p>
+      <p id="result">{displayNumber}</p>
       <p id="btn-Number">
         {numberList.map(
           (number) => (
