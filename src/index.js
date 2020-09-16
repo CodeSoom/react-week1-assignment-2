79b6335c0faf4025c83d95664a9b6ff50c13e6d0
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-09-16 21:57:16 +0900

    define calculate function and define handleClickOperator function and edit handleClickFunction

--- src/index.js
+++ src/index.js
@@ -20,33 +20,99 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function handleClickNumber(previousNumber, afterNumber) {
-  if (previousNumber === 0) {
+function calculate(previousNumber, operator, afterNumber) {
+  if (operator === '+') {
+    return previousNumber + afterNumber;
+  }
+
+  if (operator === '-') {
+    return previousNumber - afterNumber;
+  }
+
+  if (operator === '*') {
+    return previousNumber * afterNumber;
+  }
+
+  return previousNumber / afterNumber;
+}
+
+function handleClickNumber(
+  previousNumber,
+  operator,
+  afterNumber,
+  temporaryResult,
+) {
+  // 연산이 끝난 뒤 숫자를 눌렀을때
+  if (operator === '=') {
     render(afterNumber);
     return;
   }
 
-  // 두개의 숫자를 합치기
-  render(previousNumber + String(afterNumber));
+  if (operator === null) {
+    // 눌린 연산자가 없고 화면에 그려진 숫자가 0일때
+    if (previousNumber === 0) {
+      render(afterNumber);
+      return;
+    }
+    // 눌린 연산자가 없고 화면에 그려진 숫자가 0이 아닐때 숫자 합치기
+    render(previousNumber + String(afterNumber), operator, temporaryResult);
+    return;
+  }
+
+  // 중간 계산결과가 없을떄 이전 숫자와 연산
+  if (temporaryResult === 0) {
+    render(
+      afterNumber,
+      operator,
+      calculate(previousNumber, operator, afterNumber),
+    );
+    return;
+  }
+
+  // 이전 숫자가 아닌 중간 계산결과와 연산
+  render(
+    afterNumber,
+    operator,
+    calculate(temporaryResult, operator, afterNumber),
+  );
 }
 
-function handleClickOperator() {}
+function handleClickOperator(number, operator, temporaryResult) {
+  if (operator === '=') {
+    render(temporaryResult, operator);
+    return;
+  }
 
-function render(number = 0) {
+  // 중간 계산결과가 있다면 화면에 그 결과를 보여준다.
+  if (temporaryResult !== 0) {
+    render(temporaryResult, operator, temporaryResult);
+    return;
+  }
+
+  render(number, operator, temporaryResult);
+}
+
+function render(number = 0, operator = null, temporaryResult = 0) {
   const element = (
     <div>
       <p>간단 계산기</p>
       {number}
       <p>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
-          <button type="button" onClick={() => handleClickNumber(number, i)}>
+          <button
+            type="button"
+            onClick={() => handleClickNumber(number, operator, i, temporaryResult)}
+          >
             {i}
           </button>
         ))}
       </p>
       <p>
         {['+', '-', '*', '/', '='].map((o) => (
-          <button type="button" onClick={() => handleClickOperator(o)}>
+          <button
+            type="button"
+            onClick={() => handleClickOperator(number, o, temporaryResult)}
+          >
             {o}
           </button>
         ))}
