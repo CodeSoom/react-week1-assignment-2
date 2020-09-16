b7fcedb1dfc45cd2ee34992ebd8a27db49b045f1
Noah <mike1124@naver.com>
2020-09-16 20:26:19 +0900

    🎨 Add first rendering calculator structure

--- src/index.js
+++ src/index.js
@@ -21,14 +21,37 @@ function createElement(tagName, props, ...children) {
 }
 
 function render() {
+  const handleClickNumber = (e) => {
+    const number = e.target.textContent;
+    console.log(typeof e.target.textContent);
+  };
+  const handleClickOperator = (e) => {
+    const operator = e.target.textContent;
+    console.log(e.target.textContent);
+  };
   const element = (
     <div>
       <p>간단 계산기</p>
+      <span>0</span>
+      <div className="button_number_collection">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+          <button type="button" onClick={(e) => handleClickNumber(e)}>
+            {number}
+          </button>
+        ))}
+      </div>
+      <div className="button_operator_collection">
+        {["+", "-", "*", "/", "="].map((operator) => (
+          <button type="button" onClick={(e) => handleClickOperator(e)}>
+            {operator}
+          </button>
+        ))}
+      </div>
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  document.getElementById("app").textContent = "";
+  document.getElementById("app").appendChild(element);
 }
 
 render();
