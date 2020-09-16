22cce469057226e0e7c40a11220657f0df9c2881
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-09-16 19:35:36 +0900

    define handleClickNumber function

--- src/index.js
+++ src/index.js
@@ -20,10 +20,37 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function handleClickNumber(previousNumber, afterNumber) {
+  if (previousNumber === 0) {
+    render(afterNumber);
+    return;
+  }
+
+  // 두개의 숫자를 합치기
+  render(previousNumber + String(afterNumber));
+}
+
+function handleClickOperator() {}
+
+function render(number = 0) {
   const element = (
     <div>
       <p>간단 계산기</p>
+      {number}
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(number, i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((o) => (
+          <button type="button" onClick={() => handleClickOperator(o)}>
+            {o}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
