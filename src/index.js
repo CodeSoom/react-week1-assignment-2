1b988c7040643cc589bb5ee7dedd43b79e357c5c
wjy5446 <wjy5446@gmail.com>
2020-09-18 00:44:27 +0900

    feat: 계산기 코드 작성
    - generateButtons : 버튼에 표시할 텍스트 배열과 event 함수를 입력으로
      받고 여러개의 버튼 태그를 생성하는 함수
    
    - calcuate : 두 숫자와 연산자를 입력으로 받아서 연산된 값을 반환하는
      함수
    
    - handleClickNumberBtn : 숫자 버튼을 클릭했을 때 event 함수
    - handleClickOpBtn : 연산자 버튼을 클릭했을 때 envent 함수
    
    - showValue : 화면에 출력할 값
    - resultValue : 계산된 결과 값
    - prevOp : 이전에 클릭한 연산자 버튼 값
    - reset : 숫자 버튼 클릭시 누른 숫자가 출력 여부

--- src/index.js
+++ src/index.js
@@ -20,10 +20,53 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render(showValue = 0, resultValue = 0, prevOp = '+', reset = true) {
+  function generateButtons(btnsArr, onClickHandler) {
+    return btnsArr.map((i) => (
+      <button type="button" onClick={() => onClickHandler(i)}>
+        {i}
+      </button>
+    ));
+  }
+
+  function calculate(num1, num2, op) {
+    switch (op) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    case '/': return num1 / num2;
+    default: throw new Error('ValueError');
+    }
+  }
+
+  function handleClickNumberBnt(number) {
+    if (reset) {
+      render(number, resultValue, prevOp, false);
+    } else {
+      render(showValue * 10 + number, resultValue, prevOp, false);
+    }
+  }
+
+  function handleClickOpBtn(op) {
+    const result = calculate(resultValue, showValue, prevOp);
+
+    if (op !== '=') {
+      render(result, result, op, true);
+    } else {
+      render(result, 0, '+', true);
+    }
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{showValue}</p>
+      <p>
+        {generateButtons([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], handleClickNumberBnt)}
+      </p>
+      <p>
+        {generateButtons(['+', '-', '*', '/', '='], handleClickOpBtn)}
+      </p>
     </div>
   );
 
