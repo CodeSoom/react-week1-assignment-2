395efd1004e7d1cae60af1eb09d4717e6421dd11
Seungmin Lee <seungmin@uxplus.dev>
2020-09-17 20:19:04 +0900

    Update calculator
    Update calculator with buttons and basic calculation functionality using `eval`
    
    - [ ] Figure out how to remove `eval`
    - [ ] Accumulate calculated value

--- src/index.js
+++ src/index.js
@@ -20,10 +20,63 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function updateValue(current, previous) {
+  if (typeof current === 'number' && typeof previous === 'number') {
+    return previous * 10 + current;
+  }
+  return current;
+}
+
+function evaluateExpression(expression) {
+  return eval(expression.join(''));
+}
+
+function updateExpression(expression, value) {
+  if (typeof expression[expression.length - 1] === 'number' && typeof value === 'number') {
+    expression.pop();
+  }
+  expression.push(value);
+  return expression;
+}
+
+function updateDisplay(value, display, expression) {
+  if (expression && expression.length > 0) {
+    return expression[expression.length - 1];
+  }
+  return display;
+}
+
+function updateState(state) {
+  let { value, expression, display } = state;
+  if (value === '=') {
+    return { value: 0, expression: [], display: evaluateExpression(expression) };
+  }
+  value = updateValue(value, expression[expression.length - 1]);
+  expression = updateExpression(expression, value);
+  display = updateDisplay(value, display, expression);
+  return {
+    ...state,
+    expression,
+    display,
+  };
+}
+
+function render(state = { value: 0, expression: [], display: 0 }) {
+  const { display } = state;
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{display}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((value) => (
+          <button type="button" onClick={() => render(updateState({ ...state, value }))}>{value}</button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((value) => (
+          <button type="button" onClick={() => render(updateState({ ...state, value }))}>{value}</button>
+        ))}
+      </p>
     </div>
   );
 
