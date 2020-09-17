c632cce04eacc7d0c9034ddfff24fb006d5e912d
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-17 23:27:07 +0900

    Display calculate contents in DOM
    display Buttons in DOM

--- src/index.js
+++ src/index.js
@@ -24,6 +24,35 @@ function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>0</p>
+      <div>
+        {
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+            <button
+              type="button"
+              onClick={() => { }}
+            >
+              {i}
+            </button>
+          ))
+        }
+      </div>
+      <div>
+        {['+', '-', '*', '/'].map((i) => (
+          <button
+            type="button"
+            onClick={() => { }}
+          >
+            {i}
+          </button>
+        ))}
+        <button
+          type="button"
+          onClick={() => { }}
+        >
+          =
+        </button>
+      </div>
     </div>
   );
 
