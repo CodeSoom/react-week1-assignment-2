e9f4f6ba9066e8a9a1041b6f8f20d909c81c0497
ParkDongJo <pdj10211@gmail.com>
2020-09-18 01:47:16 +0900

    [week1-3] 실제 결과갓 연산되게 구현하기
    * reRender() 함수 코드개선
    * caculator() 함수 코드개선
    
    불필요하고, 지저분해 보이는 코드를 제거했습니다.
    코드 가독성을 높이고자 했습니다.
    아직까지 화면 중간 출력값이 미흡합니다.
    
    console.log 제거

--- src/index.js
+++ src/index.js
@@ -24,8 +24,9 @@ const reRender = (stack) => {
   const lastElemnt = stack.slice(-1)[0]
 
   if (lastElemnt === "=" && stack.length > 3) {
-    const result = calculator(remove(stack, stack.length -1))
-    render([result])
+    render([
+      calculator(remove(stack, stack.length -1))
+    ])
   } else {
     render(stack)
   }
@@ -37,35 +38,20 @@ const remove = (array, index) => {
 }
 
 const calculator = (stack) => {
-  return [999]
-}
+  let memo = ""
+  stack.map((e) => memo += e)
 
-const plus = () => {
-
-}
-
-const minus = () => {
-
-}
-
-const divide = () => {
-
-}
-
-const multiply = () => {
-
+  return new Function(`return (${memo})`)()
 }
 
 function render(stack = []) {
-  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  const operators = ['+', '-', '*', '/', '='];
   const result = stack.length == 0 ? 0 : getLastNum(stack)
 
-  function getLastNum(_stack) {
-    const arr = _stack
-    const lastElemnt = arr.slice(-1)[0]
-    if (/[+|-|*|/]/g.test(lastElemnt)) {
-      return arr.slice(-2)[0]
+  function getLastNum(array) {
+    const lastElemnt = array.slice(-1)[0]
+    
+    if (/\+|\-|\*|\//g.test(lastElemnt)) {
+      return array.slice(-2)[0]
     } else {
       return lastElemnt
     }
@@ -77,7 +63,7 @@ function render(stack = []) {
       <div>{result}</div>
       <p>
         {
-          nums.map((num) => (
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
             <button type="button" onClick={() => ( reRender([...stack, num]) )}>
               {num}
             </button>
@@ -86,7 +72,7 @@ function render(stack = []) {
       </p>
       <p>
         {
-          operators.map((operator) => (
+          ['+', '-', '*', '/', '='].map((operator) => (
             <button type="button" onClick={() => ( reRender([...stack, operator]) )}>
               {operator}
             </button>
