eefe50344bf020419e1f6cd2d7b4135905da51fc
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-17 23:32:02 +0900

    Make funtions to calculate number

--- src/index.js
+++ src/index.js
@@ -20,17 +20,44 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render(number = 0, numbers = [], calculateContent = []) {
+  function combineNumber(num) {
+    if (!numbers[0]) {
+      numbers.push(num);
+    } else {
+      numbers[0] += num.toString();
+      numbers[0] = Number(numbers[0]);
+    }
+  }
+
+  function calculateNumbers() {
+    switch (calculateContent[1]) {
+      case '+':
+        return calculateContent[0] + calculateContent[2];
+      case '-':
+        return calculateContent[0] - calculateContent[2];
+      case '*':
+        return calculateContent[0] * calculateContent[2];
+      case '/':
+        return calculateContent[0] / calculateContent[2];
+      default:
+        return 0;
+    }
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>0</p>
+      <p>{number}</p>
       <div>
         {
           [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
             <button
               type="button"
-              onClick={() => { }}
+              onClick={() => {
+                combineNumber(i);
+                render(numbers[0], numbers, calculateContent);
+              }}
             >
               {i}
             </button>
@@ -41,14 +68,25 @@ function render() {
         {['+', '-', '*', '/'].map((i) => (
           <button
             type="button"
-            onClick={() => { }}
+            onClick={() => {
+              calculateContent.push(numbers.pop());
+              if (calculateContent.length === 1) {
+                calculateContent.push(i);
+                render(calculateContent[0], numbers, calculateContent);
+              } else {
+                render(calculateNumbers(), numbers, [calculateNumbers(), i]);
+              }
+            }}
           >
             {i}
           </button>
         ))}
         <button
           type="button"
-          onClick={() => { }}
+          onClick={() => {
+            calculateContent.push(numbers.pop());
+            render(calculateNumbers());
+          }}
         >
           =
         </button>
