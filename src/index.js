3979c944386237539d911a19583aa60bf7305eb2
saseungmin <dbd02169@naver.com>
2020-09-18 21:00:02 +0900

    Update Simple Calculator
    테스트 통과
    하지만 리팩토링 필요

--- src/index.js
+++ src/index.js
@@ -22,23 +22,83 @@ function createElement(tagName, props, ...children) {
 
 const initialState = {
   sum: 0,
+  func: null,
+  frontNum: 0,
+  backNum: 0,
+  display: 0,
+  operators: {
+    '+': (frontOperand, backOperand) => frontOperand + backOperand,
+    '-': (frontOperand, backOperand) => frontOperand - backOperand,
+    '*': (frontOperand, backOperand) => frontOperand * backOperand,
+    '/': (frontOperand, backOperand) => frontOperand / backOperand,
+  },
 };
 
-function render({ sum }) {
+function render(props) {
+  const {
+    func, sum, frontNum, operators, backNum, display,
+  } = props;
+  const numberOnClick = (i) => {
+    if (func) {
+      render({
+        ...props,
+        backNum: `${backNum + i}`,
+        display: `${backNum + i}`,
+      });
+    } else {
+      render({
+        ...props,
+        sum: parseInt(`${frontNum + i}`, 10),
+        frontNum: `${frontNum + i}`,
+        display: `${frontNum + i}`,
+      });
+    }
+  };
+
+  const calculator = (operate) => {
+    const cal = operators[operate];
+    if (operate === '=') {
+      const result = func(sum, parseInt(backNum, 10));
+      render({
+        ...props,
+        frontNum: '',
+        backNum: '',
+        func: null,
+        sum: result,
+        display: result,
+      });
+    } else if (func) {
+      const result = func(sum, parseInt(backNum, 10));
+      render({
+        ...props,
+        backNum: '',
+        frontNum: '',
+        sum: result,
+        display: result,
+        func: cal,
+      });
+    } else {
+      render({
+        ...props,
+        func: cal,
+      });
+    }
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{sum}</p>
+      <p>{display}</p>
       <p>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
-          <button type="button">
+          <button type="button" onClick={() => numberOnClick(i)}>
             {i}
           </button>
         ))}
       </p>
       <p>
         {['+', '-', '*', '/', '='].map((operator) => (
-          <button type="button">
+          <button type="button" onClick={() => calculator(operator)}>
             {operator}
           </button>
         ))}
