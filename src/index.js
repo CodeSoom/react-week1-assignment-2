224fa008423b3e42446551a0dabdc252104200dc
hyeonhyejin <dev.hyejin04@gmail.com>
2020-09-19 00:12:29 +0900

    Implement function of simple calculator
    과제의 요구사항에 따라 간단 계산기 프로그램의 기능을 구현했습니다.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -20,38 +19,76 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-const symbolOfOperations = ['+', '-', '*', '/', '='];
+const makeNumberUsingClickedNumber = (clickedNumber, beforeNumber) => Number(beforeNumber === 0 ? clickedNumber : `${beforeNumber}${clickedNumber}`);
 
-const numberButtonClick = (clickedNumber, beforeNumber) => (
-  beforeNumber === 0 ? clickedNumber : `${beforeNumber}${clickedNumber}`
-);
+const calculating = (num1) => (operation) => (num2) => operation(num1, num2);
 
+const plus = (num1, num2) => num1 + num2;
 
-function render(num = 0) {
+const minus = (num1, num2) => num1 - num2;
+
+const multiplication = (num1, num2) => num1 * num2;
+
+const division = (num1, num2) => num1 / num2;
+
+const result = (num1, operation, num2) => (operation !== undefined && num2 > 0
+  ? operation(num2) : num1);
+
+const symbolOfOperations = {
+  '+': plus,
+  '-': minus,
+  '*': multiplication,
+  '/': division,
+};
+
+function render(num1 = 0, operate, num2) {
   const element = (
     <div>
       <p>간단 계산기</p>
       <p>
-        result :
         {
-          num
+          operate !== undefined && num2 > 0 ? num2 : num1
         }
       </p>
 
       <div>
         {
           Array.from({ length: 9 }).map((_, i) => (
-            <button type="button" onClick={() => render(numberButtonClick(i + 1, num))}>{ i + 1}</button>
+            <button
+              type="button"
+              onClick={() => (operate === undefined
+                ? render(makeNumberUsingClickedNumber(i + 1, num1))
+                : render(num1, operate, makeNumberUsingClickedNumber(i + 1, num2)))}
+            >
+              { i + 1}
+            </button>
           ))
         }
       </div>
 
       <div>
         {
-          symbolOfOperations.map((v) => (
-            <button type="button">{v}</button>
+          Object.entries(symbolOfOperations).map(([key, value]) => (
+            <button
+              type="button"
+              onClick={() => {
+                render(
+                  result(num1, operate, num2),
+                  calculating(result(num1, operate, num2))(value), 0,
+                );
+              }}
+            >
+              {key}
+            </button>
           ))
+
         }
+        <button
+          type="button"
+          onClick={() => render(result(num1, operate, num2))}
+        >
+          =
+        </button>
       </div>
     </div>
   );
