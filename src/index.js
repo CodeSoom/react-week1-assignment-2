9417550930c143e67ee95df11d528dbc6644ce6f
comejtome508 <youngnoh.goh@gmail.com>
2020-09-18 23:00:12 +0900

    Delete functions and compose 'if' in render
    1. 기존의 분기를 나누던 복잡한 방법로직 삭제
    2. 연산자가 나오기 전까지는 첫번째 숫자만 수정,
    3. 연산자가 나오면 연산자 할당 및 숫자는 두번째 값에 할당
    4. '='가 나오면 연산함수로 전달
    위의 로직으로 처리하였음

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-const tempValue = {a: 'z', b: 'z', c:'z',d:'z'};
+const tempValue = {firstValue: 0, secondValue: 0, thirdValue: 0};
 
 // const makeNumber = (value) => {
 //   console.log("숫자 만드는 곳");
@@ -32,7 +32,7 @@ const tempValue = {a: 'z', b: 'z', c:'z',d:'z'};
   
 // }
 
-const calculWays = (value1,value2,str) => {
+const calculResult = (value1,value2,str) => {
   console.log("연산해야할곳");
   switch (str) 
   {
@@ -40,70 +40,85 @@ const calculWays = (value1,value2,str) => {
     //switch로 연산자를 입력하고 그 다음에 숫자를 다시 입력 받아야 연산을 할 수 있음
     //연산자를 스트링으로 받으면 할당받고 그다음에 연산진행함수 안에서 계산을 해야 할듯하다.
     case '+':
-      preValue.d  = sum(value1, value2);
-      break;
+      return(value1 + value2)
     case '-':
-      preValue.d = substract(value1, value2);
-      break;
+      return(value1 - value2)
     case '*':
-      preValue.d = multiply(value1, value2);
-      break;
+      return(value1 * value2)
     case '/':
-      preValue.d = divide(value1, value2);
-      break;
-    case '=':
-      return preValue.d;
+      return(value1 / value2)
   }
 } 
 
-const isNumber = (preValue1, preValue2) => {
-  if(isNaN(preValue2) != true){
-    //숫자
-    tempValue.a = (preValue1*10)+preValue2;
+// const isNumber = (preValue1, preValue2) => {
+//   if(isNaN(preValue2) != true){
+//     //숫자
+//     tempValue.a = (preValue1*10)+preValue2;
     
-  }else {
-    //연산자
-    tempValue.c = preValue2;
-    
-  };
-}
+//   }else {
+//     //연산자
+//     tempValue.c = preValue2;
+//     console.log(`c: ${tempValue.c}`);
+//   };
+// }
 
 //사칙연산 함수 
-const sum = (a, b) => {
-  return a + b;
-}
+// const sum = (a, b) => {
+//   return a + b;
+// }
 
-const substract = (a, b) => {
-  return a - b;
-}
+// const substract = (a, b) => {
+//   return a - b;
+// }
 
-const multiply = (a, b) => {
-  return a * b;
-}
+// const multiply = (a, b) => {
+//   return a * b;
+// }
 
-const divide = (a, b) => {
-  return a / b;
-}
-//숫자 다음 숫자를 누른 경우
-const multiplyTen = (a, b) => {
-  return (a * 10) + b;
-}
+// const divide = (a, b) => {
+//   return a / b;
+// }
+// //숫자 다음 숫자를 누른 경우
+// const multiplyTen = (a, b) => {
+//   return (a * 10) + b;
+// }
 
 
 //
 function render(value) {
-  if(tempValue.a != 'z') {
-    tempValue.b = value;
-    isNumber(tempValue.a, tempValue.b);
-    value = tempValue.a;
-  } else if (tempValue.c != 'z'){
-    tempValue.b = value;
-    calculWays(tempValue.a,tempValue.b,tempValue.c);
-    render(tempValue.d);
-  } else {
-    tempValue.a = value;
+  // if(tempValue.a != 'z') {
+  //   console.log(`a의 초기값: ${tempValue.a}`);
+  //   tempValue.b = value;
+  //   isNumber(tempValue.a, tempValue.b);
+  //   value = tempValue.a;
+  // } else if (tempValue.c != 'z' && tempValue.a != 'z' && tempValue.b != 'z'){
+  //   console.log('연산자가 생겼을때');
+  //   tempValue.b = value;
+  //   calculWays(tempValue.a,tempValue.b,tempValue.c);
+  //   render(tempValue.d);
+  // } else {
+  //   tempValue.a = value;
+  //   console.log(`a: ${tempValue.a}`);
+  // }
+  if(isNaN(value) != true && isNaN(tempValue.secondValue) != true){
+      tempValue.firstValue = (tempValue.firstValue*10)+value;
+      value = tempValue.firstValue;
+  }else if(isNaN(value) != true && isNaN(tempValue.secondValue) == true){
+    console.log("Hello");
+      tempValue.thirdValue = (tempValue.thirdValue*10)+value;
+      value = tempValue.thirdValue;
+  
+  }else if(value == '=') {
+      value = calculResult(tempValue.firstValue,tempValue.thirdValue,tempValue.secondValue);
+  }else{
+    //연산자가 입력됐을때
+      tempValue.secondValue = value;
+      value = tempValue.firstValue;
   }
   
+console.log(`value:${tempValue.firstValue}`);
+console.log(`tempValue.secondValue:${tempValue.secondValue}`);
+console.log(isNaN(tempValue.secondValue));
 
 
   const element = (
