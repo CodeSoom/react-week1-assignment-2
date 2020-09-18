250bd9f913b210fe1b4c0e6fff94019e86517af8
Dong A Kim <dongakim90@gmail.com>
2020-09-18 10:49:59 +0900

    numbers 순서 변경

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function createElement(tagName, props, ...children) {
 }
 
 function render(display, calculator) {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const operators = [
     { label: '+', func: (number1) => (number2) => number1 + number2 },
     { label: '-', func: (number1) => (number2) => number1 - number2 },
