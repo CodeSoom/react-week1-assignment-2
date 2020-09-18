01e626867c6c24158fc271e2d346072106d5b904
comejtome508 <youngnoh.goh@gmail.com>
2020-09-18 17:28:47 +0900

    Make functions to sort values and calculate the values
    1. object 값을 a,b,c,d로 두고 각각에 임시 값을 할당하도록 하였습니다.
    2. 할당 된 값이 초기값이 있는가/ 두번째 값은 숫자인가,연산자인가로 나누었습니다.
    3. 숫자일 경우 10을 곱해서 이전 입력값과 합치도록 하였습니다.
    4. 연산자인 경우 선택한 연산자에 따라 일단 임시 할당 후 세번째 값을 받으면 선택한 연산자의 함수로 이동하도록 하였습니다.
    5. d에다가 연산한 값을 할당하고 '='를 누를 경우 render함수에 넣어서 값을 출력하도록 했습니다.
    6. 현재는 숫자가 연달아 왔을 때 값을 추가하는 것까지 출력되고 나머지는 디버깅을 해봐야 합니다.

--- src/index.js
+++ src/index.js
@@ -20,53 +20,91 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-const preNumber = {value: 0};
+const tempValue = {a: 'z', b: 'z', c:'z',d:'z'};
 
-const makeNumber = (value) => {
-  console.log("숫자 만드는 곳");
-  // 들어온 숫자에 무조건 10을 곱한 뒤 지금 들어온 숫자와 더하면 됨
-  //이전의 값을 할당해놓을 곳이 필요함
-  //value = value * 10 + value
-  preNumber.value = preNumber.value + value;
-  return value;
+// const makeNumber = (value) => {
+//   console.log("숫자 만드는 곳");
+//   // 들어온 숫자에 무조건 10을 곱한 뒤 지금 들어온 숫자와 더하면 됨
+//   //이전의 값을 할당해놓을 곳이 필요함
+//   //value = value * 10 + value
+//   preNumber.value = preNumber.value + value;
+//   return value;
   
-}
+// }
 
-const calNumber = (str) => {
+const calculWays = (value1,value2,str) => {
   console.log("연산해야할곳");
   switch (str) 
   {
+    //연산자 switch의 default는 숫자까리 더하는 값이 되어야함
+    //switch로 연산자를 입력하고 그 다음에 숫자를 다시 입력 받아야 연산을 할 수 있음
+    //연산자를 스트링으로 받으면 할당받고 그다음에 연산진행함수 안에서 계산을 해야 할듯하다.
     case '+':
-      value = preNumber.value + value;
+      preValue.d  = sum(value1, value2);
       break;
     case '-':
-      value = preNumber.value - value;
+      preValue.d = substract(value1, value2);
       break;
     case '*':
-      value = preNumber.value * value;
+      preValue.d = multiply(value1, value2);
       break;
     case '/':
-      value = preNumber.value / value;
+      preValue.d = divide(value1, value2);
       break;
     case '=':
-      break;
-    default:
-      
+      return preValue.d;
   }
 } 
 
-
-function render(value) {
-  //value가 연산자이냐 숫자이냐에 따라 1차 분기
-  if(isNaN(value) != true){
+const isNumber = (preValue1, preValue2) => {
+  if(isNaN(preValue2) != true){
     //숫자
-    console.log('숫자');
-    makeNumber(value);
+    tempValue.a = (preValue1*10)+preValue2;
+    
   }else {
     //연산자
-    console.log('연산자');
-    calNumber(value);
+    tempValue.c = preValue2;
+    
+  };
+}
+
+//사칙연산 함수 
+const sum = (a, b) => {
+  return a + b;
+}
+
+const substract = (a, b) => {
+  return a - b;
+}
+
+const multiply = (a, b) => {
+  return a * b;
+}
+
+const divide = (a, b) => {
+  return a / b;
+}
+//숫자 다음 숫자를 누른 경우
+const multiplyTen = (a, b) => {
+  return (a * 10) + b;
+}
+
+
+//
+function render(value) {
+  if(tempValue.a != 'z') {
+    tempValue.b = value;
+    isNumber(tempValue.a, tempValue.b);
+    value = tempValue.a;
+  } else if (tempValue.c != 'z'){
+    tempValue.b = value;
+    calculWays(tempValue.a,tempValue.b,tempValue.c);
+    render(tempValue.d);
+  } else {
+    tempValue.a = value;
   }
+  
+
 
   const element = (
     <div>
