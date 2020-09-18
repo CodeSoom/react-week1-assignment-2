c89fa35359fc2f77bf0b4ebd6f5abb6ef9a42f02
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-09-18 23:50:16 +0900

    Assn2. Make Simple Calculator
    Create project 'Simple calculator'.
    First, develop a number indicator!

--- src/index.js
+++ src/index.js
@@ -20,10 +20,45 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+const compute = {
+  '+': (num1, num2) => num1 + num2,
+  '-': (num1, num2) => num1 - num2,
+  '*': (num1, num2) => num1 * num2,
+  '/': (num1, num2) => num1 / num2,
+};
+
+function calculator(operator = '', num1 = 0, num2 = 0) {
+  const result = (operator) ? compute[operator](num1, num2) : num1;
+  // eslint-disable-next-line no-use-before-define
+  render(0, result);
+}
+
+function render(calculator = { displayNumber: 0 }) {
   const element = (
-    <div>
-      <p>간단 계산기</p>
+    <div id="calculator" className="calcurating">
+      <p>간단 계산기 by EHOTO</p>
+      <p>
+        { calculator.displayNumber }
+      </p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => render({ displayNumber: calculator.displayNumber * 10 + i })}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {/* 사칙연산자 등록. 만약, [숫자, 연산자, 숫자]가 존재하면 사칙연산 결과값 출력 후 사칙연산자 등록. */}
+        {['+', '-', '*', '/'].map((i) => (
+          <button type="button" onClick={() => render()}>
+            {i}
+          </button>
+        ))}
+        {/* 첫 번째 및 두 번째 숫자에 대한 사칙연산 결과값 출력 */}
+        <button type="button" onClick={() => render()}>
+          =
+        </button>
+      </p>
     </div>
   );
 
