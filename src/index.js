cd51a0b8e2af76ee9d03d01397e7e56bd9c8eaf3
Dong A Kim <dongakim90@gmail.com>
2020-09-19 19:21:12 +0900

    계산 로직 분리
    숫자 버튼이 클릭될 때 계산되는 로직과
    연산자 버튼이 클리될 때의 계산되는 로직을 render 함수 외로 분리함.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
 
+import { calculateWithNumber, calculateWithOperator } from './calculate';
+
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -24,6 +26,8 @@ const initialState = {
   precedingValue: 0,
   succeedingValue: null,
   operator: null,
+  addedNumber: 0,
+  addedOperator: null,
   isReset: false,
 };
 
@@ -31,8 +35,6 @@ function render(state) {
   const {
     precedingValue,
     succeedingValue,
-    operatorType,
-    isReset,
   } = state;
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const operators = [
@@ -42,72 +44,13 @@ function render(state) {
     { operator: '/', type: 'multiplication' },
     { operator: '=', type: 'equal' },
   ];
-  const calculator = {
-    addition: (number1, number2) => number1 + number2,
-    subtraction: (number1, number2) => number1 - number2,
-    division: (number1, number2) => number1 * number2,
-    multiplication: (number1, number2) => number1 / number2,
-  };
-
-  const calculatePrecedingValue = (number) => {
-    if (precedingValue === 0 || isReset) {
-      return {
-        ...state,
-        precedingValue: number,
-      };
-    }
-
-    return {
-      ...state,
-      precedingValue: +`${precedingValue}${number}`,
-    };
-  };
-
-  const calculateSucceedingValue = (number) => {
-    if (succeedingValue) {
-      return {
-        ...state,
-        succeedingValue: +`${succeedingValue}${number}`,
-      };
-    }
-
-    return {
-      ...state,
-      succeedingValue: number,
-    };
-  };
-
-  const calculateNumber = (number) => {
-    if (operatorType) {
-      return calculateSucceedingValue(number);
-    }
-    return calculatePrecedingValue(number);
-  };
-
-  const calculateOperator = (type) => {
-    if (succeedingValue) {
-      const operatorFunc = calculator[operatorType];
-      return {
-        ...state,
-        precedingValue: operatorFunc(precedingValue, succeedingValue),
-        succeedingValue: null,
-        operatorType: type === 'equal' ? null : type,
-        isReset: type === 'equal',
-      };
-    }
-
-    return {
-      ...state,
-      operatorType: type,
-    };
-  };
 
   const handleNumberClick = (number) => {
-    render(calculateNumber(number));
+    render(calculateWithNumber({ ...state, addedNumber: number }));
   };
 
   const handleOperatorClick = (operator) => {
-    render(calculateOperator(operator));
+    render(calculateWithOperator({ ...state, addedOperator: operator }));
   };
 
   const element = (
