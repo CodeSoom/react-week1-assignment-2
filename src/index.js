95e9eb76e5647e9c0358237523f5617a1d1432bd
comejtome508 <youngnoh.goh@gmail.com>
2020-09-20 11:59:08 +0900

    Make functions for input number
    1. 연속된 숫자처리 함수생성
    2. 연산자 입력 전후에 대한 분기처리

--- src/index.js
+++ src/index.js
@@ -20,79 +20,72 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-const tempValue = {firstValue: 0, secondValue: 0, thirdValue: 0};
-
-// const calculResult = (value1,value2,str) => {
-//   console.log("연산해야할곳");
-//   switch (str) 
-//   {
-//     case '+':
-//       return(value1 + value2)
-//     case '-':
-//       return(value1 - value2)
-//     case '*':
-//       return(value1 * value2)
-//     case '/':
-//       return(value1 / value2)
-//   }
-// } 
-
-const calculResult = (value1,value2,str) => {
-  const opValue = new Map([
-    ['+', value1 + value2],
-    ['-', value1 - value2],
-    ['*', value1 * value2],
-    ['/', value1 / value2],
-  ]);
-
-  return opValue[str] || 
-
-}
-
-
-
-const calculResult = ['+','-','*','/'].filter((value1,value2,str) => {
-  if(str == '+'){return(value1 + value2)}
-  if(str == '-'){return(value1 - value2)}
-  if(str == '*'){return(value1 * value2)}
-  if(str == '/'){return(value1 / value2)}
-});
-
-
-const judgeNumber = (value) => {
+const tempValue = {
+  firstValue: 0, 
+  secondValue: 0, 
+  operator: 0,
+};
  
-  if(isNaN(value) != true && isNaN(tempValue.secondValue) != true){
+
+
+// const calculResult = ['+','-','*','/'].filter((value1,value2,str) => {
+//   if(str === '+'){return(value1 + value2)}
+//   if(str === '-'){return(value1 - value2)}
+//   if(str === '*'){return(value1 * value2)}
+//   if(str === '/'){return(value1 / value2)}
+// });
+
+
+// const judgeNumber = (value) => {
+ 
+//   if(isNaN(value) != true && isNaN(tempValue.secondValue) != true){
    
-    //처음 숫자가 입력될 경우 (지금 들어온 값 : 숫자, 연산자 할당 값이 없는 경우)
-      tempValue.firstValue = (tempValue.firstValue*10)+value;
-      value = tempValue.firstValue;
-      console.log(`value: ${value}`);
-      return value;
+//     //처음 숫자가 입력될 경우 (지금 들어온 값 : 숫자, 연산자 할당 값이 없는 경우)
+//       tempValue.firstValue = (tempValue.firstValue*10)+value;
+//       value = tempValue.firstValue;
+//       console.log(`value: ${value}`);
+//       return value;
+//   }
+//   if(isNaN(value) != true && isNaN(tempValue.secondValue) == true){
+//     //연산자가 입력되고 나서 숫자를 입력하는 경우 (지금 들어온 값 : 숫자, 연산자 할당 값이 있는 경우)
+//     tempValue.thirdValue = (tempValue.thirdValue*10)+value;
+//     value = tempValue.thirdValue;
+//     return value;
+//   }
+//   if(value == '=') {
+//     // 연산자의 처리를 원하는 경우
+//       value = calculResult(tempValue.firstValue,tempValue.thirdValue,tempValue.secondValue);
+//       return value;
+//   }
+//   if(isNaN(value) == true && isNaN(tempValue.secondValue) != true){
+//     //연산자를 입력해야 하는 경우(현재 들어온 값: 문자, 연산자 할당 값이 없는 경우)
+//       tempValue.secondValue = value;
+//       value = tempValue.firstValue;
+//       return value;
+//   }
+// }
+
+const seriatedValue = (preValue, value) => {
+  return (preValue*10)+value;
+} 
+
+
+const whichNumber = (recentNumber) => {
+  if (tempValue.operator === 0) {
+    tempValue.firstValue = seriatedValue(tempValue.firstValue, recentNumber);
+    return tempValue.firstValue;
   }
-  if(isNaN(value) != true && isNaN(tempValue.secondValue) == true){
-    //연산자가 입력되고 나서 숫자를 입력하는 경우 (지금 들어온 값 : 숫자, 연산자 할당 값이 있는 경우)
-    tempValue.thirdValue = (tempValue.thirdValue*10)+value;
-    value = tempValue.thirdValue;
-    return value;
-  }
-  if(value == '=') {
-    // 연산자의 처리를 원하는 경우
-      value = calculResult(tempValue.firstValue,tempValue.thirdValue,tempValue.secondValue);
-      return value;
-  }
-  if(isNaN(value) == true && isNaN(tempValue.secondValue) != true){
-    //연산자를 입력해야 하는 경우(현재 들어온 값: 문자, 연산자 할당 값이 없는 경우)
-      tempValue.secondValue = value;
-      value = tempValue.firstValue;
-      return value;
-  }
+  if (tempValue.operator != 0) {
+    tempValue.secondValue  = seriatedValue(tempValue.secondValue, recentNumber);
+    operateResult(tempValue.firstValue, tempValue.secondValue, tempValue.operator);
+    return tempValue.secondValue;
+  }  
 }
 
 
 //
 function render(value) {
-  const received = judgeNumber(value);
-  console.log(`value2: ${received}`);
+  const received = whichNumber(value);
 
   const element = (
     <div>
@@ -107,7 +100,7 @@ function render(value) {
       </p>
       <p>
         {['+','-','*','/','='].map((j) => (
-          <button type="button" onClick={() => render(j)}>
+          <button type="button" onClick={() => operateWork(j)}>
             {j}
           </button>
         ))}
