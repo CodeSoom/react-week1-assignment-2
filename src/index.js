bfebb45a0c9d55122090f41c60842da25964ae17
dodoongtak <themighty.tak@gmail.com>
2021-01-20 04:36:04 +0900

    Implement Presenting Number Output Feature
    Able to show numbers users enter and renew after user click signs

--- src/index.js
+++ src/index.js
@@ -22,7 +22,21 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render(presentNumber = "0") {
+function handleDigit(presentNumber, previousNumber = "0") {
+  if (presentNumber === "0") {
+    return true;
+  }
+  if (presentNumber === previousNumber) {
+    return true;
+  }
+  return false;
+}
+
+function handleJoin(presentNumber, digit) {
+  return [...presentNumber, ...digit].reduce((prev, cur) => prev + cur);
+}
+
+function render(presentNumber = "0", previousNumber = "0", presentSign = 0) {
   const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const signs = ["+", "-", "*", "/", "="];
 
@@ -32,12 +46,46 @@ function render(presentNumber = "0") {
       <p>{presentNumber}</p>
       <p>
         {digits.map((digit) => (
-          <button type="button">{digit}</button>
+          <button
+            type="button"
+            onClick={() => {
+              if (previousNumber === "0") {
+                if (handleDigit(presentNumber)) {
+                  render(digit, previousNumber, presentSign);
+                } else {
+                  render(
+                    handleJoin(presentNumber, digit),
+                    previousNumber,
+                    presentSign
+                  );
+                }
+              } else {
+                if (handleDigit(presentNumber, previousNumber)) {
+                  render(digit, previousNumber, presentSign);
+                } else {
+                  render(
+                    handleJoin(presentNumber, digit),
+                    previousNumber,
+                    presentSign
+                  );
+                }
+              }
+            }}
+          >
+            {digit}
+          </button>
         ))}
       </p>
       <p>
         {signs.map((sign) => (
-          <button type="button">{sign}</button>
+          <button
+            type="button"
+            onClick={() => {
+              render(presentNumber, presentNumber, sign);
+            }}
+          >
+            {sign}
+          </button>
         ))}
       </p>
     </div>
