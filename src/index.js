265d6dab0e9e278811598285420063d0e8664bc6
dodoongtak <themighty.tak@gmail.com>
2021-01-20 03:59:40 +0900

    Add UI structure
    UI without any functions

--- src/index.js
+++ src/index.js
@@ -22,15 +22,29 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render(presentNumber = "0") {
+  const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const signs = ["+", "-", "*", "/", "="];
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{presentNumber}</p>
+      <p>
+        {digits.map((digit) => (
+          <button type="button">{digit}</button>
+        ))}
+      </p>
+      <p>
+        {signs.map((sign) => (
+          <button type="button">{sign}</button>
+        ))}
+      </p>
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  document.getElementById("app").textContent = "";
+  document.getElementById("app").appendChild(element);
 }
 
 render();
