f14e136283aaeae53ebf73300a725dd526c38aa3
datalater <the7mincheol@gmail.com>
2021-01-19 15:07:23 +0900

    Build UI
    Make nubmer buttons and operator buttons

--- src/index.js
+++ src/index.js
@@ -24,6 +24,26 @@ function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>display</p>
+      <div>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button">0</button>
+      </div>
+      <div>
+        <button type="button">+</button>
+        <button type="button">-</button>
+        <button type="button">*</button>
+        <button type="button">/</button>
+        <button type="button">=</button>
+      </div>
     </div>
   );
 
