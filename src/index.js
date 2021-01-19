0879a38f1a4c324d508f9bf28f5eabb6e619a1d2
EL <ickyicky@gmail.com>
2021-01-19 21:50:18 +0900

    FEAT: Add a simple calculator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -20,15 +19,72 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
-  const element = (
-    <div>
-      <p>간단 계산기</p>
-    </div>
-  );
+const numbers = [];
+let calculaedNumber = '';
+let oprator = '';
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+let render;
+
+function handleClickNumber(number) {
+  calculaedNumber = calculaedNumber.concat(number);
+  render(Number(calculaedNumber));
+}
+function handleClickOperator(count, clickedOperator) {
+  numbers.push(count);
+  calculaedNumber = '';
+
+  let result = 0;
+
+  if (numbers.length < 2) {
+    oprator = clickedOperator;
+    return;
+  }
+
+  switch (oprator) {
+  case '+':
+    result = numbers[0] + numbers[1];
+    break;
+  case '-':
+    result = numbers[0] - numbers[1];
+    break;
+  case '*':
+    result = numbers[0] * numbers[1];
+    break;
+  case '/':
+    result = numbers[0] ? numbers[0] / numbers[1] : 0;
+    break;
+  default:
+    break;
+  }
+  oprator = clickedOperator;
+  numbers.length = 0;
+  numbers.push(result);
+  render(result);
 }
 
-render();
+render = (count) => {
+  const element = (
+    <div>
+      <div id="displayArea" className="display">
+        {count}
+      </div>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => (
+          <button type="button" onClick={() => handleClickOperator(count, i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+};
+render(0);
