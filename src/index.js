2b31d3cf0756f949c409d1a72c372fe421bda291
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-01-20 21:57:49 +0900

    Calculate with object
    if나 switch를 대신하기 위해서 object를 이용하는것을 시도했습니다.

--- src/index.js
+++ src/index.js
@@ -22,25 +22,53 @@ function createElement(tagName, props, ...children) {
 
 const makeDecimal = (accumulator, currentValue) => accumulator * 10 + currentValue;
 
+function arithmetiOperations(operand1, operand2, operator) {
+  function add() {
+    return operand1 + operand2;
+  }
+  function abstract() {
+    return operand1 - operand2;
+  }
+  function multiply() {
+    return operand1 * operand2;
+  }
+  function divide() {
+    return operand1 / operand2;
+  }
+  function equal() {
+    return 0;
+  }
+  return {
+    '+': add, '-': abstract, '*': multiply, '/': divide, '=': equal,
+  }[operator]();
+}
+function calculate(operand1, operand2, operator) {
+  return arithmetiOperations(operand1, operand2, operator);
+}
+
 function render(input, num) {
   const element = (
     <div id="hello" className="greeting">
       <p>간단 계산기</p>
+
       <p>
         {num}
       </p>
+
       <p>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
           <button
             type="button"
             onClick={() => {
-              render(input.concat(i), input.concat(i).reduce(makeDecimal));
+              render(input.concat(i),
+                calculate(input.concat(i).reduce(makeDecimal), num));
             }}
           >
             {i}
           </button>
         ))}
       </p>
+
       <p>
         {['+', '-', '*', '/', '='].map((i) => (
           <button
@@ -55,6 +83,7 @@ function render(input, num) {
       </p>
     </div>
   );
+
   document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(element);
 }
