a99ecdb2441607d2e0d5f9e071c291bf1dfe28a5
shinhyogeun <tls1gy2rms3@naver.com>
2021-01-20 21:08:21 +0900

    feat : 계산기능 추가(refactoring 필요)

--- src/index.js
+++ src/index.js
@@ -20,13 +20,50 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+const calculator = {
+  process: [],
+  resetNumber: true,
+};
+
 function render(count = 0) {
+  function hardrender(number) {
+    calculator.resetNumber = true;
+    render(number);
+  }
+  function calculate(operator, number) {
+    calculator.process.push(number);
+    calculator.process.push(operator);
+    calculator.resetNumber = false;
+    if (calculator.process.length === 4) {
+      const cases = {
+        '+': calculator.process[0] + calculator.process[2],
+        '-': calculator.process[0] - calculator.process[2],
+        '*': calculator.process[0] * calculator.process[2],
+        '/': calculator.process[0] / calculator.process[2],
+        '=': calculator.process[2],
+      };
+      calculator.process = [cases[calculator.process[1]], calculator.process[3]];
+      render(calculator.process[0]);
+    }
+  }
   const element = (
     <div>
       <p>간단 계산기</p>
       <p>{count}</p>
-      <p>{[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => <button type="button" onClick={() => render(count * 10 + number)}>{number}</button>)}</p>
-      <p>{['+', '-', '*', '/', '='].map((operator) => <button type="button" onClick={() => render(operator)}>{operator}</button>)}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+          <button
+            type="button"
+            onClick={() => {
+              if (calculator.resetNumber === false) return hardrender(number);
+              return render(count * 10 + number);
+            }}
+          >
+            {number}
+          </button>
+        ))}
+      </p>
+      <p>{['+', '-', '*', '/', '='].map((operator) => <button type="button" onClick={() => calculate(operator, count)}>{operator}</button>)}</p>
     </div>
   );
 
