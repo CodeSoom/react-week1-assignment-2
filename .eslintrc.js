41d5bfada78b8541ae618fefe43a2861aff3b21d
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-01-20 17:24:14 +0900

    Fix CRLF error and undefined error
    CRLF를 LF로 바꾸기 위해 .eslintrc.js에 rule을 추가하였고
    makeDecimal과 calculator가 undefined되었는데 사용하는 error를 제거하기
    위해 render함수내의 익명함수로 변경하였습니다.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,14 +10,12 @@ module.exports = {
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    Feature: 'readonly',
-    Scenario: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 11,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: [
@@ -25,29 +23,5 @@ module.exports = {
   ],
   rules: {
     'linebreak-style': 0,
-    indent: ['error', 2],
-    'no-trailing-spaces': 'error',
-    curly: 'error',
-    'brace-style': 'error',
-    'no-multi-spaces': 'error',
-    'space-infix-ops': 'error',
-    'space-unary-ops': 'error',
-    'no-whitespace-before-property': 'error',
-    'func-call-spacing': 'error',
-    'space-before-blocks': 'error',
-    'keyword-spacing': ['error', { before: true, after: true }],
-    'comma-spacing': ['error', { before: false, after: true }],
-    'comma-style': ['error', 'last'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'space-in-parens': ['error', 'never'],
-    'block-spacing': 'error',
-    'array-bracket-spacing': ['error', 'never'],
-    'object-curly-spacing': ['error', 'always'],
-    'key-spacing': ['error', { mode: 'strict' }],
-    'arrow-spacing': ['error', { before: true, after: true }],
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: ['codecept.conf.js'] },
-    ],
   },
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
+
   Object.entries(props || {}).forEach(([key, value]) => {
     element[key.toLowerCase()] = value;
   });
@@ -21,52 +22,13 @@ const clicked = {
   count: 0,
 };
 
-let operators = [];
+const operators = [];
 let digit = [];
 let res = 0;
 let operand1 = 0;
 let operand2 = 0;
 
-const reducer = (accumulator, currentValue) => accumulator*10 + currentValue;
-
-function makeDecimal(num) {
-
-  digit.push(num);
-  operand1 = digit.reduce(reducer);
-  console.log('operand1: ' + operand1);
-  render();
-}
-
-function calculator(operator) {
-  if (operator=='=') {
-      console.log('operand1 : '+operand1);
-      console.log('operand2 : '+operand2);
-      console.log('result : '+ operand1 + operand2);
-      while(!operators.length==0) {
-          switch(operators.shift) {
-              case '+':
-                  res = operand1 + operand2;
-                  break;
-              case '-':
-                  res = operand1 - operand2;
-                  break;
-              case '*':
-                  res = operand1 * operand2;
-                  break;
-              case '/':
-                  res = operand1 / operand2;
-                  break;
-          }
-      }
-
-  } else {
-      operators.push(operator);
-      operand2 = operand1;
-  }
-  operand1 = 0;
-  digit = [];
-  render();
-}
+const reducer = (accumulator, currentValue) => accumulator * 10 + currentValue;
 
 function render() {
   const element = (
@@ -74,25 +36,64 @@ function render() {
       <p>간단 계산기</p>
       <p>
         {res}
-    </p>
-    <p>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
-        <button type="button" onClick={() => makeDecimal(i)}>
-             {i}
-        </button>
-      ))}
-    </p>
-    <p>
-      {['+', '-', '*', '/', '='].map((i) => (
-        <button type="button" onClick={() => calculator(i)}>
-             {i}
-        </button>
-      ))}
-    </p>
-  </div>
-);
-document.getElementById('app').textContent = '';
-document.getElementById('app').appendChild(element);
+      </p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button
+            type="button"
+            onClick={(num) => {
+              digit.push(num);
+              operand1 = digit.reduce(reducer);
+              console.log(`operand1: ${operand1}`);
+              render();
+            }}
+          >
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => (
+          <button
+            type="button"
+            onClick={(operator) => {
+              if (operator === '=') {
+                console.log(`operand1 : ${operand1}`);
+                console.log(`operand2 : ${operand2}`);
+                console.log(`result : ${operand1}${operand2}`);
+                while (!operators.length === 0) {
+                  switch (operators.shift) {
+                    case '+':
+                      res = operand1 + operand2;
+                      break;
+                    case '-':
+                      res = operand1 - operand2;
+                      break;
+                    case '*':
+                      res = operand1 * operand2;
+                      break;
+                    case '/':
+                      res = operand1 / operand2;
+                      break;
+                  }
+                }
+              } else {
+                operators.push(operator);
+                operand2 = operand1;
+              }
+              operand1 = 0;
+              digit = [];
+              render();
+            }}
+          >
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
 render();
