0d1e5d3d7d2087ea82481eb5f1e345073e092fb5
daniel <daniel@innosonian.com>
2021-01-22 00:56:52 +0900

    연산 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
+/* eslint-disable no-use-before-define */
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -20,48 +20,90 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-const outputData = {
-  number: '0',
-  number2: '0',
+const temp = {
+  num1: 0.0,
+  num2: 0.0,
   기호: '',
 };
 
-function handleClickNumber(value) {
-  outputData.number = (outputData.number * 10) + (value);
-  render();
-}
-
-function handleClick기호(value) {
-  console.log(outputData.number);
-  if (value === '=') {
-    outputData.number = eval(outputData.number2 + outputData.기호 + outputData.number);
-    render();
-    return;
+function 숫자입력(value) {
+  if (temp.기호 === '') {
+    temp.num1 = (temp.num1 * 10) + value;
+    render(temp.num1);
+  } else {
+    temp.num2 = (temp.num2 * 10) + value;
+    render(temp.num2);
   }
-  outputData.기호 = value;
-  outputData.number2 = outputData.number;
-
-  render();
 }
 
-function render() {
+function 연산기호입력(value) {
+  if (temp.num2 !== 0) {
+    연산결과();
+    temp.기호 = value;
+  } else {
+    temp.기호 = value;
+  }
+}
+
+function 덧셈() {
+  temp.num1 += temp.num2;
+}
+
+function 뺄셈() {
+  temp.num1 -= temp.num2;
+}
+
+function 곱셈() {
+  temp.num1 *= temp.num2;
+}
+
+function 나눗셈() {
+  temp.num1 /= temp.num2;
+}
+
+function 연산결과() {
+  switch (temp.기호) {
+    case '+':
+      덧셈(temp);
+      break;
+    case '-':
+      뺄셈(temp);
+      break;
+    case '*':
+      곱셈(temp);
+      break;
+    case '/':
+      나눗셈(temp);
+      break;
+    default:
+      return;
+  }
+  temp.num2 = 0;
+  render(temp.num1);
+}
+
+
+function render(displayNumber = 0) {
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{outputData.number}</p>
+      <p>{displayNumber}</p>
       <p>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
-          <button type="button" onClick={() => handleClickNumber(i)}>
+          <button type="button" onClick={() => 숫자입력(i)}>
             {i}
           </button>
         ))}
       </p>
       <p>
-        {['+', '-', '*', '/', '='].map((i) => (
-          <button type="button" onClick={() => handleClick기호(i)}>
+        {['+', '-', '*', '/'].map((i) => (
+          <button type="button" onClick={() => 연산기호입력(i)}>
             {i}
           </button>
         ))}
+        <button type="button" onClick={() => 연산결과()}>
+          =
+        </button>
       </p>
     </div>
   );
