c43451a6c4b5ad3b1b99cd0fa4a8bd6d966ac9af
Najeong Kim <atpeoe1023@gmail.com>
2021-01-22 20:10:01 +0900

    계산기 구성하기

--- src/index.js
+++ src/index.js
@@ -20,10 +20,29 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function handleClick(count) {
+  render(count);
+}
+
+function render(value = 0) {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{value}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(i => (
+          <button type="button" onClick={() => handleClick(i)}>
+            {i}
+          </button>
+        ))}  
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map(i => (
+          <button type="button" onClick={() => handleClick(i)}>
+            {i}
+          </button>
+        ))}  
+      </p>
     </div>
   );
 
