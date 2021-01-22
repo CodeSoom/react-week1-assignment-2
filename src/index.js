c12dc41a463968b7bc52387f89c1ccf1af960553
hxunz <guswl2843@gmail.com>
2021-01-23 02:38:49 +0900

    - 웹페이지에서 계산기가 보이게 셋팅
     - 숫자와  계산 기능 버튼 모양 생성

--- src/index.js
+++ src/index.js
@@ -20,15 +20,29 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+const render = () => {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => (
+          <button type="button" onClick={() => handleClick(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
   document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(element);
-}
+};
 
 render();
