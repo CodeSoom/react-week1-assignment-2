adcf1075ae20000a87422a493e060a57276793ad
noah-innosonian <noah@innosonian.com>
2021-01-22 21:43:26 +0900

    미완성 1차

--- src/index.js
+++ src/index.js
@@ -20,10 +20,47 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+let result = '';
+let number = 0;
+function calculator(i) {
+  console.log(i);
+  if (i === '=') {
+    render(eval(result));
+    result = '';
+    number = 0;
+  } else if (typeof (i) === 'string') {
+    render(number);
+    result += i;
+    number = i;
+  } else if (typeof (i) === 'number') {
+    render(i);
+    result += i;
+    number = i;
+  }
+  console.log(result);
+}
+
+function render(count = 0) {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const symbols = ['+', '-', '*', '-', '='];
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{count}</p>
+      <p>
+        {numbers.map((i) => (
+          <button type="button" onClick={() => calculator(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {symbols.map((i) => (
+          <button type="button" onClick={() => calculator(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
