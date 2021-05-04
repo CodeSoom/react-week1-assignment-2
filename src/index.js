4b66f11b3fcb553215c9635d8f289ffc74f3595d
최한울 <hanul.choi95@nts-corp.com>
2021-05-05 00:15:10 +0900

    [1주차] 간단한 사칙 연산 계산기 구현
    - 숫자를 누르면 누른 숫자 출력 (완)
    - 숫자를 연속해서 누르면 숫자가 더해져서 출력 (완)
    - 숫자와 연산자를 입력 후 = 를 클릭하면 계산 결과 출력 (완)
    - 연속해서 숫자와 연산자를 입력하면 중간에 계산 결과 출력 (미완)

--- src/index.js
+++ src/index.js
@@ -20,10 +20,73 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+const elements = {result: 0, number1: '', sign: '', number2: ''};
+
 function render() {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const calcSigns = ['+', '-', '*', '/', '='];
+
+  const handleClickItem = (item) => {
+    if (item === '=') {
+      const num1 = Number(elements.number1);
+      const num2 = Number(elements.number2);
+
+      switch (elements.sign) {
+        case '+':
+          const sum = num1 + num2;
+          elements.result = sum;
+          break;
+        case '-':
+          const minus = num1 - num2;
+          elements.result = minus;
+          break;
+        case '*':
+          const multi = num1 * num2;
+          elements.result = multi;
+          break;
+        case '/':
+          const div = num1 / num2;
+          elements.result = div;
+          break;
+      }
+
+      elements.number1 = '';
+      elements.sign = '';
+      elements.number2 = '';
+      render();
+      return;
+    }
+
+    if (typeof item === 'string') {
+      elements.sign = item;
+      return;
+    }
+
+    elements.sign === '' ? elements.number1 += item : elements.number2 += item;
+
+    render();
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>
+        {elements.number1 === ''
+          ? elements.result
+          : elements.sign === ''
+            ? elements.number1
+            : elements.number2}
+      </p>
+      <p>
+        {numbers.map((num) => (
+          <button type="button" onClick={() => handleClickItem(num)}>{num}</button>
+        ))}
+      </p>
+      <p>
+        {calcSigns.map((sign) => (
+          <button type="button" onClick={() => handleClickItem(sign)}>{sign}</button>
+        ))}
+      </p>
     </div>
   );
 
