7e1c0ccc5376e402fdf886b4c1bd18a9b900d895
yujonglee <yujonglee@g.skku.edu>
2021-05-04 13:56:29 +0900

    eslint오류없이 모든 인수테스트를 통과하라

--- src/index.js
+++ src/index.js
@@ -2,33 +2,104 @@
 
 /* @jsx createElement */
 
-function createElement(tagName, props, ...children) {
-  const element = document.createElement(tagName);
+const createElement = (tagname, props, ...children) => {
+  const element = document.createElement(tagname);
 
   Object.entries(props || {}).forEach(([key, value]) => {
     element[key.toLowerCase()] = value;
   });
 
   children.flat().forEach((child) => {
-    if (child instanceof Node) {
-      element.appendChild(child);
-      return;
-    }
-    element.appendChild(document.createTextNode(child));
+    const nodeToAppend = (child instanceof Node)
+      ? child
+      : document.createTextNode(child);
+
+    element.appendChild(nodeToAppend);
   });
 
   return element;
-}
+};
 
-function render() {
+const isOperator = (input) => (['+', '-', '*', '/', '='].includes(input));
+const calculate = (left, operator, right) => {
+  const l = Number(left, 10);
+  const r = Number(right, 10);
+
+  if (operator === '+') {
+    return `${l + r}`;
+  }
+  if (operator === '-') {
+    return `${l - r}`;
+  }
+  if (operator === '*') {
+    return `${l * r}`;
+  }
+  if (operator === '/') {
+    return `${l / r}`;
+  }
+  if (operator === '=') {
+    return r;
+  }
+  return null;
+};
+const render = (holdingValue, holdingOperator, display, previous) => {
+  const reRender = (e, hv, ho, d, p) => {
+    const current = e.target.textContent;
+    if (isOperator(current)) {
+      if (!isOperator(p)) {
+        const calculated = calculate(hv, ho, d);
+        render(calculated, current, calculated, current);
+      } else {
+        render(hv, current, d, current);
+      }
+    } else if (d !== '0' && !isOperator(p)) {
+      render(hv, ho, d + current, current);
+    } else {
+      render(hv, ho, current, current);
+    }
+  };
+
   const element = (
-    <div>
-      <p>간단 계산기</p>
+    <div id="simpleCalculator">
+      <h1>간단 계산기</h1>
+      <h2>{display}</h2>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+          <button
+            type="button"
+            onClick={(e) => reRender(
+              e,
+              holdingValue,
+              holdingOperator,
+              display, previous,
+            )}
+          >
+            {number}
+          </button>
+        ))}
+      </p>
+
+      <p>
+        {['+', '-', '*', '/', '='].map((operator) => (
+          <button
+            type="button"
+            onClick={(e) => reRender(
+              e,
+              holdingValue,
+              holdingOperator,
+              display, previous,
+            )}
+          >
+            {operator}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
-}
+  const container = document.getElementById('app');
+  container.textContent = '';
+  container.appendChild(element);
+};
 
-render();
+render('0', '=', '0', '0');
