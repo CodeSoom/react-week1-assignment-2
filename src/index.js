14a1624580b66400de616484643ef3a2aea7f57e
yujonglee <yujonglee@g.skku.edu>
2021-05-06 13:11:50 +0900

    getNewState를 리팩토링하라

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import _ from 'lodash';
 
 const operatorButtons = ['+', '-', '*', '/', '='];
 const numberButtons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-const initialState = {
-  holdingValue: '0',
-  holdingOperator: '=',
-  display: '0',
-  previousInput: '0',
-};
 
 const createElement = (tagname, props, ...children) => {
   const element = document.createElement(tagname);
@@ -35,7 +29,6 @@ const minus = (left, right) => left - right;
 const multiplication = (left, right) => left * right;
 const division = (left, right) => left / right;
 const equal = (left, right) => right;
-
 const calculate = (left, operator, right) => {
   const [leftInNumber, rightInNumber] = [left, right].map(toNumber);
   const operationSet = {
@@ -48,51 +41,65 @@ const calculate = (left, operator, right) => {
 
   return operationSet[operator](leftInNumber, rightInNumber).toString();
 };
+const initialState = {
+  holdingValue: '0',
+  holdingOperator: '=',
+  display: '0',
+  previousInput: '0',
+
+  updateHoldingOperator(currentInput) {
+    return (
+      (isOperator(currentInput))
+        ? { ...this, holdingOperator: currentInput }
+        : this
+    );
+  },
+
+  updatePreviousInput(currentInput) {
+    return { ...this, previousInput: currentInput };
+  },
+
+  updateHoldingValue(currentInput, calculated) {
+    const { previousInput } = this;
+
+    return (
+      (isOperator(currentInput) && !isOperator(previousInput))
+        ? { ...this, holdingValue: calculated }
+        : this
+    );
+  },
+
+  updateDisplay(currentInput, calculated) {
+    const { display, previousInput } = this;
+
+    if (isOperator(currentInput) && !isOperator(previousInput)) {
+      return { ...this, display: calculated };
+    }
+    if (isOperator(currentInput) && isOperator(previousInput)) {
+      return this;
+    }
+    if (!isOperator(currentInput) && (display !== '0' && !isOperator(previousInput))) {
+      return { ...this, display: display + currentInput };
+    }
+    if (!isOperator(currentInput) && !(display !== '0' && !isOperator(previousInput))) {
+      return { ...this, display: currentInput };
+    }
+    return null;
+  },
+};
 
 const getNewState = (e, oldState) => {
-  const {
-    holdingValue,
-    holdingOperator,
-    display,
-    previousInput,
-  } = oldState;
+  const { holdingValue, holdingOperator, display } = oldState;
   const currentInput = e.target.textContent;
+  const calculated = calculate(holdingValue, holdingOperator, display);
 
-  if (isOperator(currentInput) && !isOperator(previousInput)) {
-    const calculated = calculate(holdingValue, holdingOperator, display);
-    return ({
-      holdingValue: calculated,
-      holdingOperator: currentInput,
-      display: calculated,
-      previousInput: currentInput,
-    });
-  }
+  const newState = oldState
+    .updateHoldingValue(currentInput, calculated)
+    .updateHoldingOperator(currentInput)
+    .updateDisplay(currentInput, calculated)
+    .updatePreviousInput(currentInput);
 
-  if (isOperator(currentInput) && isOperator(previousInput)) {
-    return ({
-      ...oldState,
-      holdingOperator: currentInput,
-      previousInput: currentInput,
-    });
-  }
-
-  if (!isOperator(currentInput) && (display !== '0' && !isOperator(previousInput))) {
-    return ({
-      ...oldState,
-      display: display + currentInput,
-      previousInput: currentInput,
-    });
-  }
-
-  if (!isOperator(currentInput) && !(display !== '0' && !isOperator(previousInput))) {
-    return ({
-      ...oldState,
-      display: currentInput,
-      previousInput: currentInput,
-    });
-  }
-
-  return null;
+  return newState;
 };
 
 const render = (currentState) => {
