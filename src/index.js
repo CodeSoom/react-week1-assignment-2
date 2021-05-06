e416bbfc7754b44a88dd747460fe3edba99b9360
yujonglee <yujonglee@g.skku.edu>
2021-05-06 09:53:23 +0900

    switch-case문을 제거하고 가독성을 개선하라

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const initialState = {
   display: '0',
   previous: '0',
 };
-const toNumber = (_) => Number(_);
 
 const createElement = (tagname, props, ...children) => {
   const element = document.createElement(tagname);
@@ -28,25 +27,25 @@ const createElement = (tagname, props, ...children) => {
 
   return element;
 };
-
+const toNumber = (_) => Number(_);
 const isOperator = (input) => (operatorButtons.includes(input));
-const calculate = (left, operator, right) => {
-  const [l, r] = [left, right].map(toNumber);
+const plus = (left, right) => left + right;
+const minus = (left, right) => left - right;
+const multiplication = (left, right) => left * right;
+const division = (left, right) => left / right;
+const equal = (left, right) => right;
 
-  switch (operator) {
-  case ('+'):
-    return `${l + r}`;
-  case ('-'):
-    return `${l - r}`;
-  case ('*'):
-    return `${l * r}`;
-  case ('/'):
-    return `${l / r}`;
-  case ('='):
-    return `${r}`;
-  default:
-    return null;
-  }
+const calculate = (left, operator, right) => {
+  const [leftInNumber, rightInNumber] = [left, right].map(toNumber);
+  const operationSet = {
+    '+': plus,
+    '-': minus,
+    '*': multiplication,
+    '/': division,
+    '=': equal,
+  };
+
+  return operationSet[operator](leftInNumber, rightInNumber).toString();
 };
 const render = (sstate) => {
   const reRender = (e, state) => {
