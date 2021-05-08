7a5583fca199b1b6b40baa5cd41f5b954d0dc81b
studioLuda <studio.luda.work@gmail.com>
2021-05-09 00:16:39 +0900

    과제 1-2 계산기 : 레이아웃 완료 (연산 로직 진행중)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 /* @jsx createElement */
 
+// 첫번째 커밋 : 계산기 외형 틀 완료 (연산 처리는 진행중)
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
@@ -20,10 +21,37 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render({ result, operation }) {
+  console.log(result, operation);
+  const numberKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const operatorKeys = ['+', '-', '*', '/', '='];
+
+  function handleClickNumber(value) {
+    if (operation != null) {
+      render({ result: result * value });
+      return;
+    }
+    render({ result: value });
+  }
+
+  function handleClickOperator(value) {
+    render({ result, operation: value });
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{result || 0}</p>
+      <div>
+        {numberKeys.map((key) => (
+          <button id={`num${key}`} onClick={() => { handleClickNumber(key); }}>{key}</button>
+        ))}
+      </div>
+      <div>
+        {operatorKeys.map((key) => (
+          <button id={`num${key}`} onClick={() => { handleClickOperator(key); }}>{key}</button>
+        ))}
+      </div>
     </div>
   );
 
@@ -31,4 +59,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render({ result: 0 });
