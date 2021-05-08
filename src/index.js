3f5acaf1534039f190bdf83bd104737123507b2f
Parksejun <fgfg1202@gmail.com>
2021-05-08 22:09:35 +0900

    numberLeft 와 numberRight를 배열로 만들면 더 낫을까요?

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -16,18 +15,71 @@ function createElement(tagName, props, ...children) {
     }
     element.appendChild(document.createTextNode(child));
   });
-
   return element;
+}
+
+let numberLeft = '';
+let numberRight = '';
+let savedSign = '';
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const signList = ['+', '-', '*', '/', '='];
+
+function numberClick(clickedNumber) {
+  if (savedSign === '') {
+    numberLeft += clickedNumber;
+    document.getElementById('result').innerText = numberLeft;
+    return;
+  }
+  numberRight += clickedNumber;
+  document.getElementById('result').innerText = numberRight;
+}
+
+function strClick(clickedSign) {
+  if (savedSign === '') {
+    savedSign = clickedSign;
+    return;
+  }
+  numberLeft = Number(numberLeft);
+  numberRight = Number(numberRight);
+  if (savedSign === '+') {
+    numberLeft += numberRight;
+  } else if (savedSign === '-') {
+    numberLeft -= numberRight;
+  } else if (savedSign === '*') {
+    numberLeft *= numberRight;
+  } else if (savedSign === '/') {
+    numberLeft /= numberRight;
+  }
+  document.getElementById('result').innerText = numberLeft;
+  numberRight = '';
+  savedSign = clickedSign;
+  if (clickedSign === '=') {
+    numberLeft = '';
+    savedSign = '';
+  }
 }
 
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p id="result" />
+      <p>
+        {numberList.map((clickedNumber) => (
+          <button type="button" onClick={() => numberClick(clickedNumber)}>
+            {clickedNumber}
+          </button>
+        ))}
+      </p>
+      <p>
+        {signList.map((clickedSign) => (
+          <button type="button" onClick={() => strClick(clickedSign)}>
+            {clickedSign}
+          </button>
+        ))}
+      </p>
     </div>
   );
-
-  document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(element);
 }
 
