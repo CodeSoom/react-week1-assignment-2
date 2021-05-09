35ff30abeadb4517453bb66f577b51cbc1663b1f
Parksejun <fgfg1202@gmail.com>
2021-05-09 17:34:20 +0900

    render에서 처리하는 방식으로 수정, 아직 계산은 못만들었습니다.

--- src/index.js
+++ src/index.js
@@ -18,69 +18,106 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-let numberLeft = '';
-let numberRight = '';
 let savedSign = '';
-const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const numberList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const signList = ['+', '-', '*', '/', '='];
 
-function numberClick(clickedNumber) {
-  if (savedSign === '') {
-    numberLeft += clickedNumber;
-    document.getElementById('result').innerText = numberLeft;
-    return;
-  }
-  numberRight += clickedNumber;
-  document.getElementById('result').innerText = numberRight;
-}
+function render(leftNumber, rightNumber, paraSign) {
+  console.log(leftNumber, rightNumber, paraSign, savedSign);
+  const results = {
+    '+': Number(leftNumber) + Number(rightNumber),
+    '-': Number(leftNumber - rightNumber),
+    '*': Number(leftNumber * rightNumber),
+    '/': Number(leftNumber / rightNumber),
+    '=': Number(leftNumber),
+  };
 
-function strClick(clickedSign) {
-  if (savedSign === '') {
-    savedSign = clickedSign;
-    return;
-  }
-  numberLeft = Number(numberLeft);
-  numberRight = Number(numberRight);
-  if (savedSign === '+') {
-    numberLeft += numberRight;
-  } else if (savedSign === '-') {
-    numberLeft -= numberRight;
-  } else if (savedSign === '*') {
-    numberLeft *= numberRight;
-  } else if (savedSign === '/') {
-    numberLeft /= numberRight;
-  }
-  document.getElementById('result').innerText = numberLeft;
-  numberRight = '';
-  savedSign = clickedSign;
-  if (clickedSign === '=') {
-    numberLeft = '';
-    savedSign = '';
-  }
-}
-
-function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p id="result" />
+      <p id="result">
+        {
+          savedSign === ''
+            ? leftNumber
+            : rightNumber
+        }
+      </p>
       <p>
-        {numberList.map((clickedNumber) => (
-          <button type="button" onClick={() => numberClick(clickedNumber)}>
-            {clickedNumber}
-          </button>
-        ))}
+        {
+          (function a() {
+            if (paraSign !== '') savedSign = paraSign;
+            return '';
+          }())
+        }
+        {
+          savedSign === ''
+            ? numberList.map((clickedNumber) => (
+              <button type="button" onClick={() => render(leftNumber + clickedNumber, rightNumber, '')}>
+                {clickedNumber}
+              </button>
+            ))
+            : numberList.map((clickedNumber) => (
+              <button type="button" onClick={() => render(leftNumber, rightNumber + clickedNumber, '')}>
+                {clickedNumber}
+              </button>
+            ))
+        }
       </p>
       <p>
         {signList.map((clickedSign) => (
-          <button type="button" onClick={() => strClick(clickedSign)}>
+          <button type="button" onClick={() => render(leftNumber, rightNumber, clickedSign)}>
             {clickedSign}
           </button>
         ))}
       </p>
     </div>
   );
+
+  document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render('', '', '');
+
+// function mathNums(savedLeftNumber, savedRightNumber, clickedSign) {
+//   let leftnum = Number(savedLeftNumber);
+//   const rightnum = Number(savedRightNumber);
+
+//   leftnum += rightnum;
+// }
+
+// function numberClick(clickedNumber) {
+//   console.log(clickedNumber);
+//   if (savedSign === '') {
+//     numberLeft += clickedNumber;
+//     document.getElementById('result').innerText = numberLeft;
+//     return;
+//   }
+//   numberRight += clickedNumber;
+//   document.getElementById('result').innerText = numberRight;
+// }
+
+// function strClick(clickedSign) {
+//   if (savedSign === '') {
+//     savedSign = clickedSign;
+//     return;
+//   }
+//   numberLeft = Number(numberLeft);
+//   numberRight = Number(numberRight);
+//   if (savedSign === '+') {
+//     numberLeft += numberRight;
+//   } else if (savedSign === '-') {
+//     numberLeft -= numberRight;
+//   } else if (savedSign === '*') {
+//     numberLeft *= numberRight;
+//   } else if (savedSign === '/') {
+//     numberLeft /= numberRight;
+//   }
+//   document.getElementById('result').innerText = numberLeft;
+//   numberRight = '';
+//   savedSign = clickedSign;
+//   if (clickedSign === '=') {
+//     numberLeft = '';
+//     savedSign = '';
+//   }
+// }
