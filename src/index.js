e0a8c153c441031c4a9b30e59fcb7b65e8329e00
studioLuda <studio.luda.work@gmail.com>
2021-05-09 13:27:09 +0900

    1-2 과제 진행중 50% 완료 (목표 4개중 2개 완료)  * 중간 과정 커밋입니다!  * 완료한느대로 바로 커밋하겠습니다.  감사합니다.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
+/* eslint no-console: "off" */
 /* @jsx createElement */
 
-// 첫번째 커밋 : 계산기 외형 틀 완료 (연산 처리는 진행중)
+// 과제 제출 1차
+/** 목표
+ * 숫자를 누르면 누른 숫자가 출력되어야 합니다. (완료)
+ * 숫자를 연속해서 누르면 숫자가 더해져서 출력되어야 합니다. <- 여기서 부터 시작해야함 ! 
+ * 숫자와 연산자를 입력한 후 =를 클릭하면 계산 결과가 출력되어야 합니다. (완료)
+ * 연속해서 숫자와 연산자를 입력하면 중간에 계산 결과가 출력되어야 합니다.
+ */
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
@@ -20,36 +26,63 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
+const numberKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const operatorKeys = ['+', '-', '*', '/', '='];
 
-function render({ result, operation }) {
-  console.log(result, operation);
-  const numberKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  const operatorKeys = ['+', '-', '*', '/', '='];
+function render({ displayNumber, operation, result }) {
+  console.log({ displayNumber, operation, result });
+
+  function calcuation(value) {
+    const data = { displayNumber: value, operation, result };
+    switch (operation) {
+    case '+':
+      render({ ...data, result: displayNumber + value });
+      break;
+    case '-':
+      render({ ...data, result: displayNumber - value });
+      break;
+    case '*':
+      render({ ...data, result: displayNumber * value });
+      break;
+    case '/':
+      render({ ...data, result: displayNumber / value });
+      break;
+    default:
+      break;
+    }
+  }
 
   function handleClickNumber(value) {
-    if (operation != null) {
-      render({ result: result * value });
+    /** 현재 구현할 기능
+     * 숫자를 연속해서 누르면 숫자가 더해져서 출력되어야 합니다.
+    */
+    if (operation == null) {
+      render({ displayNumber: value });
       return;
     }
-    render({ result: value });
+    calcuation(value);
   }
 
   function handleClickOperator(value) {
-    render({ result, operation: value });
+    if (value === '=') {
+      render({ displayNumber: result, result: 0 });
+      return;
+    }
+    render({ displayNumber, operation: value });
   }
 
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{result || 0}</p>
+      <p>{displayNumber || 0}</p>
       <div>
         {numberKeys.map((key) => (
-          <button id={`num${key}`} onClick={() => { handleClickNumber(key); }}>{key}</button>
+          <button type="button" id={`num${key}`} onClick={() => { handleClickNumber(key); }}>{key}</button>
         ))}
       </div>
       <div>
         {operatorKeys.map((key) => (
-          <button id={`num${key}`} onClick={() => { handleClickOperator(key); }}>{key}</button>
+          <button type="button" id={`num${key}`} onClick={() => { handleClickOperator(key); }}>{key}</button>
         ))}
       </div>
     </div>
@@ -59,4 +92,4 @@ function render({ result, operation }) {
   document.getElementById('app').appendChild(element);
 }
 
-render({ result: 0 });
+render({ displayNumber: 0, result: 0 });
