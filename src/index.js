e86682e70ef3632e458527a28009467960b0724c
DESKTOP-00QSB4T\ko <kbp4154@naver.com>
2021-08-10 15:58:48 +0900

    Assignment-2 finish

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 /* @jsx createElement */
 
+// porps
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
@@ -19,11 +20,63 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
+let number = '';
+let operator = '';
+let answer = '';
+
+function handleClickNumber(n) {
+  if (number === '') {
+    document.getElementById('answer').textContent = `${n}`;
+    number += `${n}`;
+  } else {
+    number += n;
+    document.getElementById('answer').textContent = `${number}`;
+  }
+}
+
+function handleClickOperator(o) {
+  if (o === '=') {
+    answer *= 1; number *= 1;
+    if (operator === '+') {
+      answer += number;
+      document.getElementById('answer').textContent = `${answer}`;
+    } else if (operator === '-') {
+      answer -= number;
+      document.getElementById('answer').textContent = `${answer}`;
+    } else if (operator === '*') {
+      answer *= number;
+      document.getElementById('answer').textContent = `${answer}`;
+    } else if (operator === '/') {
+      answer /= number;
+      document.getElementById('answer').textContent = `${answer}`;
+    }
+    answer = ''; operator = ''; number = '';
+  } else {
+    operator += o;
+    answer = number;
+    number = '';
+  }
+}
 
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p id="answer">0</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => (
+          <button type="button" onClick={() => handleClickOperator(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
