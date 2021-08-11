64c1a843a8de9374b9a857147742b2f3ac932af9
Aiden <rheech22@gmail.com>
2021-08-11 10:05:16 +0900

    Make calculator
    1. 테스트는 완료했지만, let을 사용했습니다.
    2. 더 좋은 방법은 없는지 고민해봐야겠습니다.

--- src/index.js
+++ src/index.js
@@ -20,10 +20,73 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+let oldNum = 0;
+let newNum = 0;
+let operator = '';
+let isOperating = false;
+
+function render(num = 0) {
+  function makeResult(oper) {
+    let result;
+    if (oper === '+') {
+      result = oldNum + num;
+    } else if (oper === '-') {
+      result = oldNum - num;
+    } else if (oper === '/') {
+      result = oldNum / num;
+    } else if (oper === '*') {
+      result = oldNum * num;
+    }
+    oldNum = result;
+    newNum = 0;
+    render(result);
+  }
+
+  function handleClickNumber(value) {
+    const display = document.getElementById('display');
+    if (isOperating) {
+      display.innerText = '';
+      isOperating = false;
+    }
+    display.innerText += value;
+    newNum = Number(display.innerText);
+    render(newNum);
+  }
+
+  function handleClickOperator() {
+    const {
+      event: {
+        target: { innerText },
+      },
+    } = this;
+    if (operator === '') {
+      operator = innerText;
+      oldNum = newNum;
+    } else {
+      makeResult(operator);
+      operator = innerText;
+    }
+    isOperating = true;
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p id="display">{num}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => (
+          <button type="button" onClick={() => handleClickOperator()}>
+            {i}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
