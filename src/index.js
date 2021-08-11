0248ca29561cfaaf5f85f5689beb83bfdd7e9ad9
hanabyhana <yaong1002@gmail.com>
2021-08-11 14:03:23 +0900

    let 또는 var를 사용하지 않고 계산기 코드 작성

--- src/index.js
+++ src/index.js
@@ -20,10 +20,76 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+//
+
+const initNum = 0;
+const digits = [];
+const inputs = [];
+
+function calculate() {
+  switch (inputs[1]) {
+  case '+':
+    return inputs[0] + inputs[2];
+  case '-':
+    return inputs[0] - inputs[2];
+  case '*':
+    return inputs[0] * inputs[2];
+  case '/':
+    return inputs[0] / inputs[2];
+  default:
+    break;
+  }
+  return 0;
+}
+
+// 객체로 만들기?
+function render(numForScreen) {
+  function handleClickNumber(num) {
+    digits.push(num);
+    render(parseInt(digits.join(''), 10));
+  }
+
+  function handleClickOperator(opr) {
+    if (digits.length === 0) {
+      inputs.splice(-1, 1, opr);
+      return;
+    }
+    inputs.push(parseInt(digits.join(''), 10));
+    inputs.push(opr);
+    digits.splice(0);
+
+    if (opr === '=' && inputs.length === 2) {
+      render(inputs[0]);
+      inputs.splice(0);
+    } else if (opr === '=' && inputs.length > 3) {
+      const result = calculate();
+      render(result);
+      inputs.splice(0);
+    } else if (inputs.length > 3) {
+      const result = calculate();
+      inputs.splice(0, 3, result);
+      render(result);
+    }
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <span id="screen">{numForScreen}</span>
+      <div id="numButtons">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+          <button type="button" onClick={() => handleClickNumber(num)}>
+            {num}
+          </button>
+        ))}
+      </div>
+      <div id="oprButtons">
+        {['+', '-', '*', '/', '='].map((opr) => (
+          <button type="button" onClick={() => handleClickOperator(opr)}>
+            {opr}
+          </button>
+        ))}
+      </div>
     </div>
   );
 
@@ -31,4 +97,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render(initNum);
