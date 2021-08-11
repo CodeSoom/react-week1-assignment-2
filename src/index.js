d100edeb936588b0b7e453696b263df47dbb9cb0
hanabyhana <yaong1002@gmail.com>
2021-08-11 18:38:13 +0900

    리팩토링
    변수명 변경, 주석 추가, 조건문 가독성 개선

--- src/index.js
+++ src/index.js
@@ -20,54 +20,68 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-//
-
 const initNum = 0;
-const digits = [];
-const inputs = [];
+const digits = []; // 사용자가 입력하는 숫자를 자릿수별로 배열에 담는다. 예. 123 입력 -> [1, 2, 3]
+const userInputs = []; // 사용자가 입력한 숫자, 연산자 모음
 
 function calculate() {
-  switch (inputs[1]) {
-  case '+':
-    return inputs[0] + inputs[2];
-  case '-':
-    return inputs[0] - inputs[2];
-  case '*':
-    return inputs[0] * inputs[2];
-  case '/':
-    return inputs[0] / inputs[2];
-  default:
-    break;
+  const opr = userInputs[1];
+  if (opr === '+') {
+    return userInputs[0] + userInputs[2];
+  }
+  if (opr === '-') {
+    return userInputs[0] - userInputs[2];
+  }
+  if (opr === '*') {
+    return userInputs[0] * userInputs[2];
+  }
+  if (opr === '/') {
+    return userInputs[0] / userInputs[2];
   }
   return 0;
 }
 
-// 객체로 만들기?
-function render(numForScreen) {
+function render(visibleNum) {
   function handleClickNumber(num) {
     digits.push(num);
     render(parseInt(digits.join(''), 10));
   }
 
   function handleClickOperator(opr) {
-    if (digits.length === 0) {
-      inputs.splice(-1, 1, opr);
+    // * 잘못된 입력 처리 *
+    // 1. 입력한 숫자가 전혀 없는 상태에서 연산자만 입력했을 경우
+    if (digits.length === 0 && userInputs.length === 0) {
       return;
     }
-    inputs.push(parseInt(digits.join(''), 10));
-    inputs.push(opr);
+    // 2. '숫자 -> 연산자 -> 연산자' 처럼 연산자를 연속해서 입력했을 경우
+    if (digits.length === 0) {
+      userInputs.splice(-1, 1, opr);
+      return;
+    }
+
+    // 이전에 입력된 숫자와 새로 입력된 연산자를 userInputs에 담는다.
+    userInputs.push(parseInt(digits.join(''), 10));
+    userInputs.push(opr);
     digits.splice(0);
 
-    if (opr === '=' && inputs.length === 2) {
-      render(inputs[0]);
-      inputs.splice(0);
-    } else if (opr === '=' && inputs.length > 3) {
+    // * 계산하기 *
+    // 숫자 -> '=' 순서로 입력된 경우
+    if (opr === '=' && userInputs.length === 2) {
+      render(userInputs[0]);
+      userInputs.splice(0);
+      return;
+    }
+    // 숫자 -> 연산자 -> 숫자 -> '=' 순서로 입력된 경우
+    if (opr === '=' && userInputs.length > 3) {
       const result = calculate();
       render(result);
-      inputs.splice(0);
-    } else if (inputs.length > 3) {
+      userInputs.splice(0);
+      return;
+    }
+    // 연속해서 숫자와 연산자를 입력하는 경우 중간 계산 결과 구하기
+    if (userInputs.length > 3) {
       const result = calculate();
-      inputs.splice(0, 3, result);
+      userInputs.splice(0, 3, result);
       render(result);
     }
   }
@@ -75,21 +89,22 @@ function render(numForScreen) {
   const element = (
     <div>
       <p>간단 계산기</p>
-      <span id="screen">{numForScreen}</span>
-      <div id="numButtons">
+      <span id="screen">{visibleNum}</span>
+      <br />
+      <p id="numButtons">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
           <button type="button" onClick={() => handleClickNumber(num)}>
             {num}
           </button>
         ))}
-      </div>
-      <div id="oprButtons">
+      </p>
+      <p id="oprButtons">
         {['+', '-', '*', '/', '='].map((opr) => (
           <button type="button" onClick={() => handleClickOperator(opr)}>
             {opr}
           </button>
         ))}
-      </div>
+      </p>
     </div>
   );
 
