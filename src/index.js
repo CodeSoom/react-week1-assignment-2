3c83b510e1e2c5146b50813c17d7746252c1a7da
seilylook <seilylook@naver.com>
2021-08-15 12:49:39 +0900

    feature: code refactoring

--- src/index.js
+++ src/index.js
@@ -20,76 +20,104 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-let before = 0;
-let after = 0;
-let operator = "";
-let opercheck = false;
+createElement();
 
-function render(num = 0) {
-  function calculate(oper) {
-    let result;
-    if (oper === "+") result = before + num;
-    if (oper === "-") result = before - num;
-    if (oper === "/") result = before / num;
-    if (oper === "*") result = before * num;
+// let 변수를 하나도 안만들고도 가능한거였구나.
 
-    before = result;
-    after = 0;
-    render(result);
+const initialState = {
+  accumulator: 0,
+  number: 0,
+  operator: ""
+};
+
+// 와 연산자 계산을 조건문을 이용하는게 아니라 객체로 만들어서 사용할 줄은 몰랐다.
+// 확실히 복잡한 조건문들보다 직관적이다.
+const operatorFunctions = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "*": (x, y) => x * y,
+  "/": (x, y) => x / y
+};
+
+// 이런식으로 연산들을 함수로 분리하는 것도 고려하자.
+// function plus(x, y) {
+//   return x + y;
+// }
+
+// function minus(x, y) {
+//   return x - y;
+// }
+
+// function multiple(x, y) {
+//   return x * y;
+// }
+
+// function division(x,y) {
+//   return x/y;
+// }
+
+function defaultFunction(x, y) {
+  return x || y;
+}
+
+function calculate(operator, accumulator, number) {
+  // 연산자 입력이 아닐때는 기존에 있던 숫자만을 보여준다.
+  return (operatorFunctions[operator] || defaultFunction)(accumulator, number);
+}
+
+function render({ accumulator, number, operator }) {
+  function handleClickReset() {
+    render(initialState);
   }
 
-  function handleClickNumber(num) {
-    const display = document.getElementById("display");
-    if (opercheck) {
-      display.innerText = "";
-      opercheck = false;
-    }
-    display.innerText += num;
-    after = Number(display.innerText);
-    render(after);
+  function handleClickNumber(value) {
+    // 숫자를 누를 때마다 자릿수를 변경해주면서 입력값을 반영해준다.
+
+    render({
+      accumulator,
+      number: number * 10 + value,
+      operator
+    });
   }
 
-  function handleClickOperator() {
-    const {
-      event: {
-        after: { innerText }
-      }
-    } = this;
-    if (operator === "") {
-      operator = innerText;
-      before = after;
-    } else {
-      calculate(operator);
-      operator = innerText;
-    }
-    opercheck = true;
+  function handleClickOperator(value) {
+    render({
+      accumulator: calculate(operator, accumulator, number),
+      number: 0,
+      operator: value
+    });
   }
 
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p id="display">{num}</p>
+      <p>{number || accumulator}</p>
+
       <p>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(i => (
-          <button type="button" onClick={() => handleClickNumber(i)}>
-            {i}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(num => (
+          <button type="button" onClick={() => handleClickNumber(num)}>
+            {num}
           </button>
         ))}
       </p>
+
       <p>
-        {["+", "-", "*", "/", "="].map(i => (
-          <button type="button" onClick={() => handleClickOperator()}>
-            {i}
+        {["+", "-", "*", "/", "="].map(oper => (
+          <button type="button" onClick={() => handleClickOperator(oper)}>
+            {oper}
           </button>
         ))}
+
+        <button type="button" onClick={() => handleClickReset()}>
+          Reset
+        </button>
       </p>
     </div>
   );
 
   const app = document.getElementById("app");
-
   app.textContent = "";
   app.appendChild(element);
 }
 
-render();
+render(initialState);
