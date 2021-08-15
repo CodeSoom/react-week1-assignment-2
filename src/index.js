fc8ca6b08c6a86c16a1ac3bc0cdb66c9863a9c41
psy082 <zhappy114@gmail.com>
2021-08-15 22:15:01 +0900

    feat: 계산기 구현
    - 계산기 layout 구성
    - 계산기 입력 타입을 3개로 분류
    - 이전 입력 타입에 따라 현재 입력처리가 달라져야 하므로
      이전 입력 타입과 현재 입력 타입으로 이벤트 처리 타입이 결정되도록 구현

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -20,15 +19,138 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+const TYPE = {
+  OPERATOR: 'o',
+  NUMBER: 'n',
+  RESULT: 'r',
+};
+
+const initialState = {
+  number: '0', preInputType: TYPE.NUMBER, stack: ['0'],
+};
+
+const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const operators = {
+  '+': (num1, num2) => +num1 + +num2,
+  '-': (num1, num2) => +num1 - +num2,
+  '*': (num1, num2) => +num1 * +num2,
+  '/': (num1, num2) => +num1 / +num2,
+};
+
+const getType = (input) => {
+  if (input === '=') return TYPE.RESULT;
+  return operators[input] ? TYPE.OPERATOR : TYPE.NUMBER;
+};
+
+const inputHandlers = {
+  calculate(stack) {
+    while (stack.length > 1) {
+      const num1 = stack.shift();
+      const op = stack.shift();
+      const num2 = stack.shift();
+      stack.unshift(operators[op](num1, num2));
+    }
+    return stack[0];
+  },
+  [TYPE.NUMBER + TYPE.NUMBER](input, stack) {
+    const lastToken = stack.pop();
+    const number = `${+(lastToken + input)}`;
+    stack.push(number);
+    return {
+      number,
+      preInputType: TYPE.NUMBER,
+      stack,
+    };
+  },
+  [TYPE.OPERATOR + TYPE.NUMBER](input, stack) {
+    const number = input;
+    stack.push(number);
+    return {
+      number,
+      preInputType: TYPE.NUMBER,
+      stack,
+    };
+  },
+  [TYPE.NUMBER + TYPE.OPERATOR](input, stack) {
+    let newStack = [...stack];
+    if (newStack.length > 2) {
+      newStack = [this.calculate(newStack)];
+    }
+    const number = newStack[newStack.length - 1];
+    newStack.push(input);
+    return {
+      number,
+      preInputType: TYPE.OPERATOR,
+      stack: newStack,
+    };
+  },
+  [TYPE.OPERATOR + TYPE.OPERATOR](input, stack) {
+    stack.pop();
+    return this[TYPE.NUMBER + TYPE.OPERATOR](input, stack);
+  },
+  [TYPE.NUMBER + TYPE.RESULT](input, stack) {
+    return {
+      number: `${this.calculate(stack)}`,
+      preInputType: TYPE.RESULT,
+      stack,
+    };
+  },
+  [TYPE.OPERATOR + TYPE.RESULT](input, stack) {
+    stack.pop();
+    return this[TYPE.NUMBER + TYPE.RESULT](input, stack);
+  },
+  [TYPE.RESULT + TYPE.NUMBER](input, stack) {
+    const number = input;
+    return {
+      number,
+      preInputType: TYPE.NUMBER,
+      stack: [number],
+    };
+  },
+  [TYPE.RESULT + TYPE.OPERATOR](input, stack) {
+    const number = stack[stack.lenth - 1];
+    return {
+      number,
+      preInputType: TYPE.NUMBER,
+      stack: [number, input],
+    };
+  },
+};
+
+function render({
+  number, preInputType, stack,
+}) {
+  const handleInput = (input, _preInputType, _stack) => {
+    const handlerType = _preInputType + getType(input);
+    render(inputHandlers[handlerType](input, _stack));
+  };
+
+  const rootNode = document.getElementById('app');
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{number}</p>
+      <p>
+        {digits.map((digit) => <button type="button" onClick={() => { handleInput(digit, preInputType, stack); }}>{digit}</button>)}
+      </p>
+      <p>
+        {Object.keys(operators).map((operator) => <button type="button" onClick={() => { handleInput(operator, preInputType, stack); }}>{operator}</button>)}
+        <button type="button" onClick={() => { handleInput('=', preInputType, stack); }}>=</button>
+      </p>
+      <p>
+        <button
+          type="button"
+          onClick={() => { render({ number: '0', preInputType: TYPE.NUMBER, stack: ['0'] }); }}
+        >
+          clear
+        </button>
+      </p>
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  rootNode.textContent = '';
+  rootNode.appendChild(element);
 }
 
-render();
+render(initialState);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  devServer: {
+    port: 4000,
+  },
   module: {
     rules: [
       {
