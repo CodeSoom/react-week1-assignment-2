54559678309212d7fcbf653812b8abb79a7997d8
padawanR0k <padawanR0k@gmail.com>
2021-12-03 00:15:39 +0900

    up: add calculate function

--- src/index.js
+++ src/index.js
@@ -26,6 +26,11 @@ const OPERATION_ENUM = {
   '*': 'multiply',
   '/': 'divide',
   '=': 'equal',
+  plus: '+',
+  minus: '-',
+  multiply: '*',
+  divide: '/',
+  equal: '=',
 };
 
 const Button = (content, onClick) => (
@@ -38,7 +43,7 @@ const ButtonGroup = (contentList, onClick) => (
   contentList.map((item) => Button(item, () => onClick(item)))
 );
 
-const Result = (result = 0, stack = []) => (
+const Result = (result = 0) => (
   <p id="result">{result}</p>
 );
 
@@ -47,25 +52,63 @@ function render(child) {
   document.getElementById('app').appendChild(child);
 }
 
-const component = (stack = []) => {
-  const inputNumber = (i) => {
-    render(component());
+const operate = (leftValue, type, rightValue) => {
+  switch (type) {
+  case OPERATION_ENUM.plus:
+    return leftValue + rightValue;
+  case OPERATION_ENUM.minus:
+    return leftValue - rightValue;
+  case OPERATION_ENUM.multiply:
+    return leftValue * rightValue;
+  case OPERATION_ENUM.divide:
+    return leftValue / rightValue;
+  default:
+    return 0;
+  }
+};
+
+const component = (state = {
+  left: null, operator: null, right: null, accumulator: 0,
+}) => {
+  const setState = (newState) => render(component(newState));
+
+  const inputNumber = (n) => {
+    if (state.left === null) {
+      setState({ ...state, left: n, accumulator: n });
+    } else if (!state.operator && !state.right) {
+      const newLeft = Number(`${state.left}${n}`);
+      setState({ ...state, left: newLeft, accumulator: newLeft });
+    } else if (state.operator) {
+      if (state.right === null) {
+        setState({ ...state, right: n, accumulator: n });
+      } else {
+        const newRight = Number(`${state.right}${n}`);
+        setState({ ...state, right: newRight, accumulator: newRight });
+      }
+    }
   };
 
   const setOperation = (oper) => {
-    render(component());
+    if (oper === OPERATION_ENUM.equal) {
+      const result = operate(state.left, state.operator, state.right);
+      setState({
+        left: result, operator: null, right: null, accumulator: result,
+      });
+      return;
+    }
+    setState({ ...state, operator: oper });
   };
   return (
     <div>
       <p>간단 계산기</p>
-      {Result()}
+      {Result(state.accumulator)}
       <div>
         <section>
           {ButtonGroup([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], inputNumber)}
         </section>
         <br />
         <section>
-          {ButtonGroup(Object.keys(OPERATION_ENUM), setOperation)}
+          {ButtonGroup(['+', '-', '*', '/', '='], setOperation)}
         </section>
       </div>
     </div>
