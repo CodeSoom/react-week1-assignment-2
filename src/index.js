52a8667a3ad4d404587bc64af0fb3bfc6f05e2ec
jeongseop.lee <sjl0614@naver.com>
2021-12-02 21:14:51 +0900

    :sparkles: 계산기 UI 추가

--- src/index.js
+++ src/index.js
@@ -20,10 +20,36 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+const calculator = {
+  value: 0,
+  sign: '',
+  result: 0,
+};
+
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p id="screen">{calculator.value}</p>
+      <div className="number-btn-con">
+        <button type="button" onClick={() => {}}>1</button>
+        <button type="button" onClick={() => {}}>2</button>
+        <button type="button" onClick={() => {}}>3</button>
+        <button type="button" onClick={() => {}}>4</button>
+        <button type="button" onClick={() => {}}>5</button>
+        <button type="button" onClick={() => {}}>6</button>
+        <button type="button" onClick={() => {}}>7</button>
+        <button type="button" onClick={() => {}}>8</button>
+        <button type="button" onClick={() => {}}>9</button>
+        <button type="button" onClick={() => {}}>0</button>
+      </div>
+      <div className="sign-btn-con">
+        <button type="button" onClick={() => {}}>+</button>
+        <button type="button" onClick={() => {}}>-</button>
+        <button type="button" onClick={() => {}}>*</button>
+        <button type="button" onClick={() => {}}>/</button>
+        <button type="button" onClick={() => {}}>=</button>
+      </div>
     </div>
   );
 
