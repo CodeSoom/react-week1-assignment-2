114d48a7d4bb7982c56d261fd167d220cde560c6
ocipap <papico@inflearn.com>
2021-12-03 00:55:00 +0900

    new: 계산기 퍼블리싱 및 초반 뼈대 코드 작업

--- src/index.js
+++ src/index.js
@@ -20,10 +20,31 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+const OPERATIONS = ['+', '-', '*', '/', '='];
+
+const { log } = console;
+
+const result = 0;
+
+function handleNumberClick(i) {
+  log(i);
+}
+
+function handleOperationClick(operation) {
+  log(operation);
+}
+
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{0}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (<button type="button" onClick={() => handleNumberClick(i)}>{i}</button>))}
+      </p>
+      <p>
+        {OPERATIONS.map((operation) => (<button type="button" onClick={() => handleOperationClick(operation)}>{operation}</button>))}
+      </p>
     </div>
   );
 
