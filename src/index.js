f8b917d193a92ac16824a787c78e3e8dd19d44d8
CheolWoong Choi <bluesky4381@naver.com>
2021-12-03 00:31:40 +0900

    Complete hw2 and waiting PR review

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
+/*
+  eslint-disable
+  react/react-in-jsx-scope,
+  react/jsx-filename-extension,
+  no-unused-vars,
+  quotes,
+  indent,
+*/
 
 /* @jsx createElement */
+
+const state = {
+  result: 0,
+  acc: 0,
+  symbolCnt: 0,
+  prev: null,
+};
 
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
@@ -21,14 +35,102 @@ function createElement(tagName, props, ...children) {
 }
 
 function render() {
+  const calculateAcc = (symbol, num) => {
+    switch (symbol) {
+      case "+":
+        state.acc += num;
+        state.prev = num;
+        break;
+      case "-":
+        state.acc -= num;
+        state.prev = num;
+        break;
+      case "*":
+        state.acc *= num;
+        state.prev = num;
+        break;
+      case "/":
+        state.acc /= num;
+        state.prev = num;
+        break;
+      case "=":
+        // ?
+        break;
+      default:
+    }
+  };
+
+  const handleNumber = (num) => {
+    switch (typeof state.prev) {
+      case "number": {
+        const accNum = Number(`${state.prev}${num}`);
+
+        state.result = accNum;
+        state.acc = accNum;
+        state.prev = accNum;
+        break;
+      }
+      case "string":
+        calculateAcc(state.prev, num);
+        break;
+      case "object": {
+        state.result = num;
+        state.acc = num;
+        state.prev = num;
+        break;
+      }
+      default:
+    }
+
+    render();
+  };
+
+  const handleSymbol = (symbol) => {
+    switch (symbol) {
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        state.symbolCnt += 1;
+        state.prev = symbol;
+
+        if (state.symbolCnt > 0) {
+          state.result = state.acc;
+        }
+        break;
+      case "=":
+        state.result = state.acc;
+        state.prev = null;
+        break;
+      default:
+    }
+
+    render();
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <span>{state.result}</span>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+          <button type="button" onClick={() => handleNumber(num)}>
+            {num}
+          </button>
+        ))}
+      </p>
+      <p>
+        {["+", "-", "*", "/", "="].map((symbol) => (
+          <button type="button" onClick={() => handleSymbol(symbol)}>
+            {symbol}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  document.getElementById("app").textContent = "";
+  document.getElementById("app").appendChild(element);
 }
 
 render();
