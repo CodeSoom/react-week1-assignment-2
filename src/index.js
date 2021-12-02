f0fd7582700314bce0c595d01aff004cf5f6a6f5
Ravi <ravi@inflearn.com>
2021-12-02 23:59:52 +0900

    우선은 더하기 기능만 구현

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function getLastIndex(inputs = []) {
 
 /**
  * @param {Array<string | number>} [inputs=[]]
- * @return {number}
+ * @return {number | string}
  */
 function getLastInput(inputs = []) {
   return inputs[getLastIndex(inputs)];
@@ -49,13 +49,29 @@ function getLastInput(inputs = []) {
  * @return {string}
  */
 function getDisplayContent(inputs = []) {
+  // 처음에는 0
+  // 마지막 누른 버튼이 숫자라면 마지막 입력한 숫자를 보여줌
+  // 마지막 누른 버튼이 연산자라면 지금까지의 연산 결과를 보여줌
   if (inputs.length === 0) {
-    return '';
+    return '0';
   }
-  if (inputs.length === 1 && typeof inputs[0] !== 'number') {
-    return '';
+  const lastInput = getLastInput(inputs);
+  if (typeof lastInput === 'number') {
+    return lastInput;
   }
-  return `${inputs[getLastIndex(inputs)]}`;
+
+  return `${inputs.reduce((acc, input, index) => {
+    if (index === 0) {
+      return input;
+    }
+    if (typeof input === 'number') {
+      return acc + input;
+    }
+    if (input === '=') {
+      return acc;
+    }
+    return acc;
+  })}`;
 }
 
 /**
@@ -75,6 +91,34 @@ function render({ inputs = [] } = { inputs: [] }) {
       return;
     }
     setState({ inputs: [...inputs, number] });
+  }
+
+  function handleClickOperatorExceptEquals(event) {
+    const operator = event.target.closest('[data-key]').dataset.key;
+    if (inputs.length === 0) {
+      return;
+    }
+    const lastInput = getLastInput(inputs);
+    if (typeof lastInput !== 'number' && lastInput !== '=') {
+      const newInputs = [...inputs];
+      newInputs[getLastIndex(inputs)] = operator;
+      setState({ inputs: newInputs });
+      return;
+    }
+    setState({ inputs: [...inputs, operator] });
+  }
+
+  function handleClickEquals() {
+    if (inputs.length === 0) {
+      return;
+    }
+    if (typeof getLastInput(inputs) !== 'number') {
+      const newInputs = [...inputs];
+      newInputs[getLastIndex(inputs)] = '=';
+      setState({ inputs: newInputs });
+      return;
+    }
+    setState({ inputs: [...inputs, '='] });
   }
 
   const element = (
@@ -99,29 +143,45 @@ function render({ inputs = [] } = { inputs: [] }) {
           );
         })}
       </ul>
-      <ul className="calculator__numbers">
+      <ul className="calculator__operators">
         <li data-key="+">
-          <button type="button" aria-label="plus">
+          <button
+            type="button"
+            aria-label="plus"
+            onClick={handleClickOperatorExceptEquals}
+          >
             +
           </button>
         </li>
         <li data-key="-">
-          <button type="button" aria-label="minus">
+          <button
+            type="button"
+            aria-label="minus"
+            onClick={handleClickOperatorExceptEquals}
+          >
             -
           </button>
         </li>
         <li data-key="*">
-          <button type="button" aria-label="multiply">
+          <button
+            type="button"
+            aria-label="multiply"
+            onClick={handleClickOperatorExceptEquals}
+          >
             *
           </button>
         </li>
         <li data-key="/">
-          <button type="button" aria-label="divide">
+          <button
+            type="button"
+            aria-label="divide"
+            onClick={handleClickOperatorExceptEquals}
+          >
             /
           </button>
         </li>
         <li data-key="=">
-          <button type="button" aria-label="equals">
+          <button type="button" aria-label="equals" onClick={handleClickEquals}>
             =
           </button>
         </li>
