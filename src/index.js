a50f0049c61d1f0b51c812faced5a113e71685d8
jeongseop.lee <sjl0614@naver.com>
2021-12-02 21:16:47 +0900

    :sparkles: 숫자 버튼 함수 추가

--- src/index.js
+++ src/index.js
@@ -26,22 +26,26 @@ const calculator = {
   result: 0,
 };
 
+const handleClickNumber = (number) => {
+  calculator.value = number;
+};
+
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
       <p id="screen">{calculator.value}</p>
       <div className="number-btn-con">
-        <button type="button" onClick={() => {}}>1</button>
-        <button type="button" onClick={() => {}}>2</button>
-        <button type="button" onClick={() => {}}>3</button>
-        <button type="button" onClick={() => {}}>4</button>
-        <button type="button" onClick={() => {}}>5</button>
-        <button type="button" onClick={() => {}}>6</button>
-        <button type="button" onClick={() => {}}>7</button>
-        <button type="button" onClick={() => {}}>8</button>
-        <button type="button" onClick={() => {}}>9</button>
-        <button type="button" onClick={() => {}}>0</button>
+        <button type="button" onClick={() => { handleClickNumber(1); render(); }}>1</button>
+        <button type="button" onClick={() => { handleClickNumber(2); render(); }}>2</button>
+        <button type="button" onClick={() => { handleClickNumber(3); render(); }}>3</button>
+        <button type="button" onClick={() => { handleClickNumber(4); render(); }}>4</button>
+        <button type="button" onClick={() => { handleClickNumber(5); render(); }}>5</button>
+        <button type="button" onClick={() => { handleClickNumber(6); render(); }}>6</button>
+        <button type="button" onClick={() => { handleClickNumber(7); render(); }}>7</button>
+        <button type="button" onClick={() => { handleClickNumber(8); render(); }}>8</button>
+        <button type="button" onClick={() => { handleClickNumber(9); render(); }}>9</button>
+        <button type="button" onClick={() => { handleClickNumber(0); render(); }}>0</button>
       </div>
       <div className="sign-btn-con">
         <button type="button" onClick={() => {}}>+</button>
