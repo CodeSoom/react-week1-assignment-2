b13650bafc6f98f323295db8aeee7c28e8d0b930
Ravi <ravi@inflearn.com>
2021-12-02 23:21:58 +0900

    handleClickNumber 구현
    바로 직전에 숫자를 눌렀으면 계속 번호 입력 아니면 그냥 숫자 새로 입력

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
   Object.entries(props || {}).forEach(([key, value]) => {
+    if (key === 'className') {
+      element.className = value;
+      return;
+    }
     if (key.includes('-')) {
       element.setAttribute(key.toLowerCase(), value);
       return;
@@ -24,17 +28,71 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+/**
+ * @param {Array<string | number>} [inputs=[]]
+ * @return {number}
+ */
+function getLastIndex(inputs = []) {
+  return inputs.length - 1;
+}
+
+/**
+ * @param {Array<string | number>} [inputs=[]]
+ * @return {number}
+ */
+function getLastInput(inputs = []) {
+  return inputs[getLastIndex(inputs)];
+}
+
+/**
+ * @param {Array<string | number>} [inputs=[]]
+ * @return {string}
+ */
+function getDisplayContent(inputs = []) {
+  if (inputs.length === 0) {
+    return '';
+  }
+  if (inputs.length === 1 && typeof inputs[0] !== 'number') {
+    return '';
+  }
+  return `${inputs[getLastIndex(inputs)]}`;
+}
+
+/**
+ * @param {{ inputs: Array<string | number> }} props
+ */
+function render({ inputs = [] } = { inputs: [] }) {
+  function setState(newState) {
+    render(newState);
+  }
+
+  function handleClickNumber(event) {
+    const number = Number(event.target.closest('[data-key]').dataset.key);
+    if (typeof getLastInput(inputs) === 'number') {
+      const newInputs = [...inputs];
+      newInputs[getLastIndex(inputs)] = getLastInput(inputs) * 10 + number;
+      setState({ inputs: newInputs });
+      return;
+    }
+    setState({ inputs: [...inputs, number] });
+  }
+
   const element = (
     <section className="calculator">
       <h1>간단 계산기</h1>
-      <div className="calculator__display" />
+      <div className="calculator__display">
+        <p>{getDisplayContent(inputs)}</p>
+      </div>
       <ul className="calculator__numbers">
         {[...Array(9).keys()].map((_, index) => {
           const number = (index + 1) % 10;
           return (
             <li data-key={number}>
-              <button type="button" aria-label={number}>
+              <button
+                type="button"
+                aria-label={number}
+                onClick={handleClickNumber}
+              >
                 {number}
               </button>
             </li>
