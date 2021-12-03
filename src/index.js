f0863b112f8c32e179908da63b3ba82239182dfd
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-03 23:16:36 +0900

    [feat] 숫자 클릭 기능 구현

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 /* @jsx createElement */
 
 const $app = document.getElementById('app');
-const NUMBER_PAD_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const NUMBER_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const OPERATOR_LIST = ['+', '-', '*', '/', '='];
+const expressionStack = [];
 
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
@@ -23,19 +24,63 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render(count = 0) {
+function covertExpressionToResult() {
+  if (expressionStack.length === 0) {
+    return 0;
+  }
+
+  return expressionStack.join('');
+}
+
+function render() {
+  function handleClickNumber({ target }) {
+    const number = target.textContent;
+
+    if (expressionStack.length <= 0) {
+      if (number === '0') {
+        return;
+      }
+
+      expressionStack.push(number);
+      return;
+    }
+
+    const value = expressionStack.pop();
+    if (NUMBER_LIST.includes(value)) {
+      expressionStack.push(value + number);
+      render();
+      return;
+    }
+
+    render();
+  }
+
+  function handleClickOperator({ target }) {
+    if (expressionStack.length < 1) {
+      return;
+    }
+
+    const expression = target.textContent;
+
+    expressionStack.push(expression);
+  }
+
   const $element = (
     <div>
       <p>간단 계산기</p>
-      <p>{count}</p>
+      <p>{covertExpressionToResult()}</p>
       <div>
-        {NUMBER_PAD_LIST.map((number) => (
-          <button type="button">{number}</button>
+        {NUMBER_LIST.map((number) => (
+          <button type="button" onClick={handleClickNumber}>
+            {number}
+          </button>
         ))}
       </div>
       <div>
         {OPERATOR_LIST.map((operation) => (
-          <button type="button">{operation}</button>
+          <button type="button" onClick={handleClickOperator}>
+            {operation}
+          </button>
         ))}
       </div>
     </div>
