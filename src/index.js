75ddfabb050213ee6ea79df9307e20cf7b9bb9ba
yeji <lerry597@gmail.com>
2021-12-03 22:21:05 +0900

    Setting skeleton code

--- src/index.js
+++ src/index.js
@@ -20,10 +20,29 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function handleClickNumber(clickedNumber) {
+  render(clickedNumber);
+}
+
+function render(calcResult) {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{calcResult}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(i => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map(i => (
+          <button>
+            {i}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
@@ -31,4 +50,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render(0);
