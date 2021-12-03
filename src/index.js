5b88dc245fe9023122959e1d718377ca20246aa8
jeongseop.lee <sjl0614@naver.com>
2021-12-03 19:03:40 +0900

    :bug: 숫자버튼 클릭 이벤트 수정
      숫자버튼 연속 클릭 시 자릿수 추가로 수정함.

--- src/index.js
+++ src/index.js
@@ -22,22 +22,29 @@ function createElement(tagName, props, ...children) {
 
 const app = document.getElementById('app');
 
+const handleClickNumber = (prev, value) => {
+  if (prev !== 0) {
+    return prev * 10 + value;
+  }
+  return value;
+};
+
 function render(value = 0) {
   const element = (
     <div>
       <p>간단 계산기</p>
       <p>{value}</p>
       <div className="number-btn-con">
-        <button type="button" onClick={() => { render(1); }}>1</button>
-        <button type="button" onClick={() => { render(2); }}>2</button>
-        <button type="button" onClick={() => { render(3); }}>3</button>
-        <button type="button" onClick={() => { render(4); }}>4</button>
-        <button type="button" onClick={() => { render(5); }}>5</button>
-        <button type="button" onClick={() => { render(6); }}>6</button>
-        <button type="button" onClick={() => { render(7); }}>7</button>
-        <button type="button" onClick={() => { render(8); }}>8</button>
-        <button type="button" onClick={() => { render(9); }}>9</button>
-        <button type="button" onClick={() => { render(0); }}>0</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 1)); }}>1</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 2)); }}>2</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 3)); }}>3</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 4)); }}>4</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 5)); }}>5</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 6)); }}>6</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 7)); }}>7</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 8)); }}>8</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 9)); }}>9</button>
+        <button type="button" onClick={() => { render(handleClickNumber(value, 0)); }}>0</button>
       </div>
       <div className="sign-btn-con">
         <button type="button" onClick={() => {}}>+</button>
