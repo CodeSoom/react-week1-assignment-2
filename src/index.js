6f6ce0fdede3afd3cf90e12bac84982aba889d25
장희유 <autowinifront@naver.com>
2021-12-05 01:41:15 +0900

    연속해서 숫자와 연산자를 입력하면 중간에 계산 결과가 출력

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
   Object.entries(props || {}).forEach(([key, value]) => {
-    element[key.toLowerCase()] = value;
+    element[key.toLowerCase() === 'classname' ? 'className' : key.toLowerCase()] = value;
   });
 
   children.flat().forEach((child) => {
@@ -22,45 +22,54 @@ function createElement(tagName, props, ...children) {
 
 function render(props) {
   const {
-    previousOperand, currentOperand, operator,
+    previousOperand, currentOperand, operator, result,
   } = props;
-  function compute() {
+
+  function getCalculatedCount() {
     switch (operator) {
     case '+':
-      render({
-        ...props, currentOperand: previousOperand + currentOperand, operator: '',
-      });
-      break;
+      return previousOperand + currentOperand;
     case '-':
-      render({ ...props, currentOperand: previousOperand - currentOperand, operator: '' });
-      break;
+      return previousOperand - currentOperand;
     case '*':
-      render({ ...props, currentOperand: previousOperand * currentOperand, operator: '' });
-      break;
+      return previousOperand * currentOperand;
     case '/':
-      render({ ...props, currentOperand: previousOperand / currentOperand, operator: '' });
-      break;
+      return previousOperand / currentOperand;
     default:
-      break;
+      throw new Error('잘못된 operator');
     }
   }
 
+  function compute(operatorValue) {
+    const calculatedCount = getCalculatedCount();
+    render({
+      ...props,
+      currentOperand: 0,
+      previousOperand: calculatedCount,
+      result: calculatedCount,
+      operator: operatorValue,
+    });
+  }
+
   function appendNumber(event) {
-    const clickedNumber = Number(event.target.value);
+    const button = event.target;
+    const clickedNumber = Number(button.value);
     const appendedNumber = Number(`${currentOperand}${clickedNumber}`);
 
-    if (operator) {
-      render({ ...props, previousOperand: currentOperand, currentOperand: clickedNumber });
-      return;
-    }
-
-    render({ ...props, currentOperand: appendedNumber });
+    render({ ...props, currentOperand: appendedNumber, result: appendedNumber });
   }
 
   function chooseOperator(event) {
     const clickedOperator = event.target.value;
 
-    render({ ...props, operator: clickedOperator });
+    if (operator) {
+      compute(clickedOperator);
+      return;
+    }
+
+    render({
+      ...props, operator: clickedOperator, previousOperand: currentOperand, currentOperand: 0,
+    });
   }
 
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
@@ -68,7 +77,7 @@ function render(props) {
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{currentOperand}</p>
+      <p className="currentCount">{result}</p>
       <p>
         {numbers.map((number) => (
           <button type="button" value={number} onClick={appendNumber}>
@@ -81,7 +90,7 @@ function render(props) {
           <button
             type="button"
             value={operatorItem}
-            onClick={operatorItem === '=' ? () => compute() : chooseOperator}
+            onClick={operatorItem === '=' ? () => compute('') : chooseOperator}
           >
             {operatorItem}
           </button>
@@ -96,5 +105,5 @@ function render(props) {
 }
 
 render({
-  previousOperand: 0, currentOperand: 0, operator: '',
+  previousOperand: 0, currentOperand: 0, operator: '', result: 0,
 });
