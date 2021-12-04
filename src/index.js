b21154b4e5fd3f9d79e9d09324dd1cf084e24f01
Beomyeon <beomyeon.kim@adriel.ai>
2021-12-04 17:16:19 +0900

    add basic calculator function

--- src/index.js
+++ src/index.js
@@ -20,15 +20,58 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function curry(f) {
+  return function first(a) {
+    return function second(b) {
+      return f(a, b);
+    };
+  };
+}
+
+const rootElement = document.getElementById('app');
+
+const OPERATORS = [
+  { key: '+', func: curry((x, y) => x + y) },
+  { key: '-', func: curry((x, y) => x - y) },
+  { key: '*', func: curry((x, y) => x * y) },
+  { key: '/', func: curry((x, y) => x / y) },
+  { key: '=' },
+];
+
+function render({ displayedNum, operator, currentValue }) {
+  function onClickNum(number) {
+    const isOperating = typeof currentValue === 'function';
+    const operated = isOperating ? currentValue(number) : number;
+    return render({ displayedNum: number, operator, currentValue: operated });
+  }
+
+  function onClickOperator(func) {
+    return render({ displayedNum, operator, currentValue: func(displayedNum) });
+  }
+
+  function onClickFinal() {
+    return render({ displayedNum: currentValue, operator: '=', currentValue: 0 });
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{displayedNum}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+          <button type="button" onClick={() => onClickNum(number)}>{number}</button>
+        ))}
+      </p>
+      <p>
+        {OPERATORS.map(({ key, func }) => (
+          <button type="button" onClick={key === '=' ? onClickFinal : () => onClickOperator(func)}>{key}</button>
+        ))}
+      </p>
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  rootElement.textContent = '';
+  rootElement.appendChild(element);
 }
 
-render();
+render({ displayedNum: 0, operator: '', currentValue: 0 });
