97e58a6fce29ec47289a3a27a09858be636d9aec
syaring <sarah.k.serim@gmail.com>
2021-12-05 15:40:26 +0900

    add comments explain how to resolve problem

--- src/index.js
+++ src/index.js
@@ -20,20 +20,38 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render(result = 0) {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const operators = ['+', '-', '*', '/', '='];
-  const result = 0;
+
+  function handleClickNumber(inputNumber) {
+    const currentResult = document.getElementById('result').innerText;
+    const inputResult = '';
+
+    // currentResult가 0일 경우 입력한 값
+    // currentResult가 0이 아닌 경우 현재 값에 뒤에 이어붙이기
+
+    render(inputResult);
+  }
+
+  function handleClickOperator(inputNumber) {
+    // switch
+    // +, -, *, / 일때는 렌더를 새로 할 필요가 없음
+    // = 일때는 render를 새로 해야 함
+  }
+
+  // render 가 필요할 때: 숫자를 눌렀을 때, = 연산자를 눌렀을 때
 
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{result}</p>
+      <p id="result">{result}</p>
       <div>
         {numbers.map((number) => (
           <button
             type="button"
             value={number}
+            onClick={() => handleClickNumber(number)}
           >
             {number}
           </button>
@@ -45,6 +63,7 @@ function render() {
           <button
             type="button"
             value={operator}
+            onClick={() => handleClickOperator(operator)}
           >
             {operator}
           </button>
