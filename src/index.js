14ad6563a300a660bb5fe157994f7567f1aa946e
jnny1017 <jnny1017@gmail.com>
2021-12-05 19:56:20 +0900

    Feat: 간단산 사칙연산 계산기 구현
    사칙연산 계산기를 구현했습니다.

--- src/index.js
+++ src/index.js
@@ -20,10 +20,52 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+// TODO calculation 변수 리팩터링
+// TODO 2개 이상 연속한 숫자의 연산
+let calculation;
+
+function render(count = 0) {
+  const handleClickNumber = (value) => {
+    render(value);
+  };
+
+  const handleClickValue = (event) => {
+    calculation = event.target.value;
+  };
+
+  const handleClickSum = () => {
+    if (calculation === 'plus') {
+      render(count + count);
+    }
+    if (calculation === 'minus') {
+      render(count - count);
+    }
+    if (calculation === 'multiplication') {
+      render(count * count);
+    }
+    if (calculation === 'division') {
+      render(count / count);
+    }
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
+       <p>{count}</p>
+      <div>
+        <button type="button" value="plus" onClick={(event) => handleClickValue(event)}>+</button>
+        <button type="button" value="minus" onClick={(event) => handleClickValue(event)}>-</button>
+        <button type="button" value="multiplication" onClick={(event) => handleClickValue(event)}>*</button>
+        <button type="button" value="division" onClick={(event) => handleClickValue(event)}>/</button>
+        <button type="button" onClick={() => handleClickSum()}>=</button>
+      </div>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
@@ -32,3 +74,4 @@ function render() {
 }
 
 render();
+
