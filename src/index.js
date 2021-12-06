25030134e77a895796e65055dd856aa87aa7eb3e
Dyel0216 ParkDyel <bluetibe@gmail.com>
2021-12-06 18:23:32 +0900

    feature: 계산기 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
 
 /* @jsx createElement */
+const INIT_STATE = Object.freeze({
+  acc: 0,
+  operator: '',
+  operand: null,
+});
+const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const OPERATORS = ['+', '-', '*', '/', '='];
+const app = document.getElementById('app');
 
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
@@ -20,15 +28,61 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function calculate({ acc, operator, operand }) {
+  return {
+    '': acc || operand,
+    '=': acc || operand,
+    '+': acc + operand,
+    '-': acc - operand,
+    '*': acc * operand,
+    '/': acc / operand,
+  }[operator];
+}
+
+function render({ acc, operator, operand }) {
+  function handleClickNumber(value) {
+    return render({
+      acc,
+      operator,
+      operand: (operand || 0) * 10 + value,
+    });
+  }
+
+  function handleClickOperator(value) {
+    return render({
+      acc: calculate({ acc, operator, operand }),
+      operator: value,
+      operand: null,
+    });
+  }
+
+  function getDisplay() {
+    return operand || acc || '';
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{getDisplay({ acc, operand })}</p>
+      <p>
+        {NUMBERS.map((_number) => (
+          <button type="button" onClick={() => handleClickNumber(_number)}>
+            {_number}
+          </button>
+        ))}
+      </p>
+      <p>
+        {OPERATORS.map((_operator) => (
+          <button type="button" onClick={() => handleClickOperator(_operator)}>
+            {_operator}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  app.textContent = '';
+  app.appendChild(element);
 }
 
-render();
+render(INIT_STATE);
