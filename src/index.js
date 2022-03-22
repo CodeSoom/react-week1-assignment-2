aa4f864100fdeb214e76e2c0f44e0fe083b4d4d8
free-ko <duddnr654@naver.com>
2022-03-22 18:39:49 +0900

    [1-2] Add: Calculator Class

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 /* @jsx createElement */
 
+const { log } = console;
+
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
@@ -20,15 +22,84 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
-  const element = (
-    <div>
-      <p>간단 계산기</p>
-    </div>
-  );
+class Calculator {
+  constructor(count) {
+    this.count = count || 0;
+  }
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  handleClickNumber(number) {
+    if (this.count === 0) {
+      this.count = number;
+      this.render();
+    } else {
+      this.count = this.count * 10 + number;
+      this.render();
+    }
+  }
+
+  handleClickOperator(operator) {
+    if (operator === '=') {
+      switch (this.operator) {
+        case '+':
+          this.count = this.leftCount + this.count;
+          this.render();
+          break;
+        case '-':
+          this.count = this.leftCount - this.count;
+          this.render();
+          break;
+        case '*':
+          this.count = this.leftCount * this.count;
+          this.render();
+          break;
+        case '/':
+          this.count = this.leftCount / this.count;
+          this.render();
+          break;
+        default:
+          log('연산');
+      }
+    } else {
+      this.operator = operator;
+      this.leftCount = this.count;
+      this.count = 0;
+    }
+  }
+
+  countElement() {
+    return (
+      <div>
+        <div>
+          <p>간단 계산기</p>
+        </div>
+
+        <p>{this.count}</p>
+
+        <p>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+            <button type="button" onClick={() => this.handleClickNumber(i)}>
+              {i}
+            </button>
+          ))}
+        </p>
+
+        <p>
+          {['+', '-', '*', '/', '='].map((i) => (
+            <button type="button" onClick={() => this.handleClickOperator(i)}>
+              {i}
+            </button>
+          ))}
+        </p>
+      </div>
+    );
+  }
+
+  render() {
+    const app = document.getElementById('app');
+    app.textContent = '';
+    app.appendChild(this.countElement());
+  }
 }
 
-render();
+const calculator = new Calculator();
+calculator.render();
