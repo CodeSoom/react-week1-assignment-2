933e4a29be0416d5d658747bb7dc1a562e9c13bb
HJUN <guswns823@gmail.com>
2022-03-25 00:16:56 +0900

    [리액트 7기_신현준] 1강 과제 1 - 간단 계산기 만들기

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
   Object.entries(props || {}).forEach(([key, value]) => {
+    if (key === 'className') {
+      element.className = value;
+      return;
+    }
     element[key.toLowerCase()] = value;
   });
 
@@ -20,15 +24,103 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+const renderValue = {
+  displayNumber: 0,
+  storeNumber: 0,
+  sign: '',
+  isNumber: true,
+};
+
+function render({
+  storeNumber,
+  displayNumber,
+  sign,
+  isNumber,
+}) {
+  const app = document.getElementById('app');
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const operators = ['+', '-', '*', '/', '='];
+
+  const calculate = (value) => {
+    const operator = {
+      '+': storeNumber + displayNumber,
+      '-': storeNumber - displayNumber,
+      '*': storeNumber * displayNumber,
+      '/': storeNumber / displayNumber,
+    };
+    const clacNumber = operator[sign] || 0;
+    if (value === '=') {
+      render({
+        displayNumber: clacNumber,
+        isNumber: false,
+      });
+    } else {
+      render({
+        storeNumber: clacNumber,
+        displayNumber: clacNumber,
+        sign: value,
+        isNumber: false,
+      });
+    }
+  };
+
+  const handleClickNumber = (value) => {
+    const resultNumber = (isNumber ? displayNumber : 0) * 10 + value;
+    render({
+      displayNumber: resultNumber,
+      storeNumber,
+      sign,
+      isNumber: true,
+    });
+  };
+
+  const handleClickOperator = (value) => {
+    if (isNumber && sign !== '') {
+      calculate(value);
+    } else {
+      render({
+        displayNumber,
+        storeNumber: displayNumber,
+        sign: value,
+        isNumber: false,
+      });
+    }
+  };
+
   const element = (
-    <div>
-      <p>간단 계산기</p>
+    <div className="calculator">
+      <p className="calculator-title">간단 계산기</p>
+      <p className="calculator-result">{displayNumber}</p>
+      <div className="calculator-key">
+        <ul className="calculator-number">
+          {numbers.map((i) => {
+            let list = '';
+            list = (
+              <li>
+                <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+              </li>
+            );
+            return list;
+          })}
+        </ul>
+
+        <ul className="calculator-operator">
+          {operators.map((i) => {
+            let list = '';
+            list = (
+              <li>
+                <button type="button" onClick={() => handleClickOperator(i)}>{i}</button>
+              </li>
+            );
+            return list;
+          })}
+        </ul>
+      </div>
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  app.textContent = '';
+  app.appendChild(element);
 }
 
-render();
+render(renderValue);
