0d8db974773f0dd5f68189ff5326ad87094b42a2
kjoobin <kjoobin>
2022-03-26 14:23:55 +0900

    Add calculrator
    해설 강의를 보기전 let 을 사용하지 않고 계산기를 만듬

--- src/index.js
+++ src/index.js
@@ -19,11 +19,71 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
+/**
+ * 작성이 완료된 값,
+ * 보통 operator 를 입력하는 순간 currnetNum 이 prevNum 으로 변한다.
+ * @param prevNum
+ *
+ * @param currentNum - 현재 작성중인 값
+ *
+ * @param renderNum - 화면에 표시되는 값
+ *
+ * @param operator - 연산자 '+', '-', '*', '/', '=' 를 포함
+ *
+ */
 
-function render() {
+function calc({ prevNum, currentNum, operator }) {
+  if (!operator) {
+    return currentNum;
+  }
+
+  const operatorLookup = {
+    '+': prevNum + currentNum,
+    '-': prevNum - currentNum,
+    '*': prevNum * currentNum,
+    '/': prevNum / currentNum,
+  };
+  return operatorLookup[operator];
+}
+
+function render({
+  prevNum = 0, currentNum = 0, renderNum = 0, operator,
+}) {
+  function handleClickNumber(value) {
+    const current = +`${currentNum}${value}`;
+    return render({
+      prevNum, currentNum: current, renderNum: current, operator,
+    });
+  }
+  function handleClickOperator(value) {
+    if (value === '=') {
+      return render({ renderNum: calc({ prevNum, currentNum, operator }) });
+    }
+
+    const calcNum = calc({ prevNum, currentNum, operator });
+    return render({
+      prevNum: calcNum, renderNum: calcNum, operator: value,
+    });
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{renderNum}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+          <button type="button" onClick={() => handleClickNumber(num)}>
+            {num}
+          </button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((oper) => (
+          <button type="button" onClick={() => handleClickOperator(oper)}>
+            {oper}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
@@ -31,4 +91,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render({});
