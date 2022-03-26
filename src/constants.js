d7d4db22597e68e68a1218481a02404781ec0cf3
yujo <yujo.dev@gmail.com>
2022-03-26 09:12:47 +0900

    feat: 상수 선언

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const PLUS = '+';
+export const MINUS = '-';
+export const MULTIPLICATION = '*';
+export const DIVISION = '/';
+export const EQUAL = '=';
+
+export const OPERATORS = [PLUS, MINUS, MULTIPLICATION, DIVISION];
