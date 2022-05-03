6ba00406d5d62b0a81bd4e5256d4399ffd287502
양탕구리 <rukeni@naver.com>
2022-05-03 09:02:09 +0900

    Wip : 계산기 구현중(몇개 테스트실패)

--- src/index.js
+++ src/index.js
@@ -20,10 +20,104 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+const initialState = {
+  previousValue: '0',
+  currentValue: '0',
+  currentOperator: '',
+  status: 'operand',
+};
+
+const sum = (a, b) => Number(a) + Number(b);
+const subtract = (a, b) => Number(a) - Number(b);
+const multiply = (a, b) => Number(a) * Number(b);
+const divide = (a, b) => Number(a) / Number(b);
+
+const calculate = (operator, a, b) => {
+  switch (operator) {
+  case '+':
+    return sum(a, b);
+  case '-':
+    return subtract(a, b);
+  case '*':
+    return multiply(a, b);
+  case '/':
+    return divide(a, b);
+  default:
+    return 0;
+  }
+};
+
+// COMPLETE: 기능1 : 숫자를 입력하면 입력된 숫자가 표시되고, 연산자를 입력하면 연산자는 표시되지 않습니다.
+// COMPLETE: 기능2 : 숫자를 연속으로 누르면 +=연산이 실행됩니다.
+// COMPLETE: 기능3 : =은 연산을 실행하고 결과를 표시 그리고 연산자를 초기화합니다.
+// COMPLETE: 기능4 : =이 아닌 연산자일지라도 연산을 실행하면 결과를 표시합니다.
+
+function render(state) {
+  const {
+    previousValue, currentValue, currentOperator, status,
+  } = state;
+
+  function handleOperand(value) {
+    if (status === 'operator') {
+      return render({
+        ...state,
+        currentValue: value,
+        status: 'operand',
+      });
+    }
+    if (currentValue[0] === '0') {
+      return render({
+        ...state,
+        currentValue: value,
+        status: 'operand',
+      });
+    }
+    return render({
+      ...state,
+      currentValue: currentValue + value,
+      status: 'operand',
+    });
+  }
+
+  function handleOperator(value) {
+    if (value === '=') {
+      const result = calculate(currentOperator, previousValue, currentValue);
+      return render({
+        ...state,
+        previousValue: '0',
+        currentValue: result.toString(),
+        currentOperator: '',
+        status: 'operator',
+      });
+    }
+    if (currentOperator) {
+      const result = calculate(currentOperator, currentValue, previousValue);
+      return render({
+        ...state,
+        previousValue: result.toString(),
+        currentValue: result.toString(),
+        currentOperator: value,
+        status: 'operator',
+      });
+    }
+    return render({
+      ...state,
+      previousValue: currentValue,
+      currentOperator: value,
+      status: 'operator',
+    });
+  }
+
   const element = (
-    <div>
+    <div id="hello" className="greeting">
       <p>간단 계산기</p>
+      <p>{currentValue}</p>
+      <p>
+        {['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].map((i) => <button type="button" onClick={() => handleOperand(i)}>{i}</button>)}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => <button type="button" onClick={() => handleOperator(i)}>{i}</button>)}
+      </p>
     </div>
   );
 
@@ -31,4 +125,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render(initialState);
