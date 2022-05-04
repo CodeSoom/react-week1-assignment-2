1a084d2f901dbf7ef83180615046585ddba45974
jodheeee <soso99700@naver.com>
2022-05-05 03:31:22 +0900

    최초 과제 제출

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -20,10 +19,81 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const operators = ['+', '-', '*', '/', '='];
+
+let picks = {
+  number: 0,
+  operator: '',
+  lastPick: '',
+  sequence: [],
+};
+
+function render(value = 0) {
+  const calculation = () => {
+    switch (picks.operator) {
+    case '+':
+      return (picks.number + value);
+    case '-':
+      return (picks.number - value);
+    case '*':
+      return (picks.number * value);
+    case '/':
+      return (picks.number / value);
+    default:
+      return null;
+    }
+  };
+
+  const numberClick = (num) => {
+    let changeNumber = num;
+
+    picks = { ...picks, sequence: picks.sequence.concat(changeNumber), lastPick: 'number' };
+
+    if (picks.lastPick === 'number') {
+      changeNumber = '';
+      picks.sequence?.forEach((n) => {
+        changeNumber += n.toString();
+      });
+      changeNumber = Number(changeNumber);
+    }
+
+    render(changeNumber);
+  };
+
+  const operatorClick = (opera) => {
+    picks = { ...picks, lastPick: 'operator', sequence: [] };
+    if (opera === '=' && picks.operator !== opera) {
+      const result = calculation();
+      picks = { ...picks, number: 0, operator: opera };
+      render(result);
+    } else if (picks.operator !== '') {
+      const result = calculation();
+      picks = { ...picks, operator: opera, number: result };
+      render(result);
+    } else {
+      picks = { ...picks, operator: opera, number: value };
+    }
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{value}</p>
+      <p>
+        {numbers?.map((num) => (
+          <button type="button" onClick={() => numberClick(num)}>
+            {num}
+          </button>
+        ))}
+      </p>
+      <p>
+        {operators?.map((opera) => (
+          <button type="button" onClick={() => operatorClick(opera)}>
+            {opera}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
