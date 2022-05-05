6fb443bf237d8d2ed34236a77fa63fcc4a34f269
jodheeee <soso99700@naver.com>
2022-05-05 14:42:17 +0900

    전역 let 변수 활용 삭제 수정

--- src/index.js
+++ src/index.js
@@ -22,64 +22,42 @@ function createElement(tagName, props, ...children) {
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const operators = ['+', '-', '*', '/', '='];
 
-let picks = {
+const init = {
   number: 0,
+  temporary: 0,
   operator: '',
-  lastPick: '',
-  sequence: [],
 };
 
-function render(value = 0) {
-  const calculation = () => {
-    switch (picks.operator) {
-    case '+':
-      return (picks.number + value);
-    case '-':
-      return (picks.number - value);
-    case '*':
-      return (picks.number * value);
-    case '/':
-      return (picks.number / value);
-    default:
-      return null;
-    }
-  };
+const calculation = (operator, firstNumber, secondNumber) => {
+  switch (operator) {
+  case '+':
+    return (firstNumber + secondNumber);
+  case '-':
+    return (firstNumber - secondNumber);
+  case '*':
+    return (firstNumber * secondNumber);
+  case '/':
+    return (firstNumber / secondNumber);
+  default:
+    return (firstNumber || secondNumber);
+  }
+};
+
+function render(value = init) {
+  const { number, temporary, operator } = value;
 
   const numberClick = (num) => {
-    let changeNumber = num;
-
-    picks = { ...picks, sequence: picks.sequence.concat(changeNumber), lastPick: 'number' };
-
-    if (picks.lastPick === 'number') {
-      changeNumber = '';
-      picks.sequence?.forEach((n) => {
-        changeNumber += n.toString();
-      });
-      changeNumber = Number(changeNumber);
-    }
-
-    render(changeNumber);
+    render({ ...value, number: number * 10 + num });
   };
 
   const operatorClick = (opera) => {
-    picks = { ...picks, lastPick: 'operator', sequence: [] };
-    if (opera === '=' && picks.operator !== opera) {
-      const result = calculation();
-      picks = { ...picks, number: 0, operator: opera };
-      render(result);
-    } else if (picks.operator !== '') {
-      const result = calculation();
-      picks = { ...picks, operator: opera, number: result };
-      render(result);
-    } else {
-      picks = { ...picks, operator: opera, number: value };
-    }
+    render({ number: 0, temporary: calculation(operator, temporary, number), operator: opera });
   };
 
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{value}</p>
+      <p>{ number || temporary }</p>
       <p>
         {numbers?.map((num) => (
           <button type="button" onClick={() => numberClick(num)}>
