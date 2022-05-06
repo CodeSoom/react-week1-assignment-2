11ece776d06d92ee1548641313333ba91eccab9d
DaengDo <ring_yoonsuk@naver.com>
2022-05-06 21:36:16 +0900

    Change eval to compute method
    다른분들의 코드를 참고해 연산 방법을 바꿨습니다.
    eval을 갖다 치우고 compute객체의 메서드로 연산을 하도록했습니다.
    
    또한 입력 받는 데이터를 다시 숫자형으로 바꿨습니다.
    원 데이터는 그대로 두고 String() 이나 Number() 래퍼 객체로 감싸주는게
    더 의도를 보여주는 것 같아서 그랬습니다.
    
    어떻게 해야 더 깔끔한 코드가 될까요?

--- src/index.js
+++ src/index.js
@@ -26,19 +26,29 @@ function render(
   isFin = false,
 ) {
   // ui 요소 데이터
-  const numsPad = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  const numsPad = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const operators = ['+', '-', '*', '/', '='];
+
+  // 연산 메서드 모음 객체
+  const compute = {
+    '+': (x, y) => Number(x) + Number(y),
+    '-': (x, y) => x - y,
+    '*': (x, y) => x * y,
+    '/': (x, y) => x / y,
+  };
 
   // 숫자 클릭 이벤트
   const handleNum = (clickedNum) => {
-    const assignOperand = operand ? operand + clickedNum : clickedNum;
+    const assignOperand = operand ? operand + String(clickedNum) : clickedNum;
 
     if (isFin) {
       return render(clickedNum);
     }
 
     if (!currentOperator) {
-      return currentNum ? render(currentNum + clickedNum) : render(clickedNum);
+      return currentNum
+        ? render(currentNum + String(clickedNum))
+        : render(clickedNum);
     }
 
     return render(currentNum, currentOperator, assignOperand);
@@ -46,16 +56,23 @@ function render(
 
   // 연산자 클릭 이벤트
   const handleOperator = (clickedOperator) => {
-    const value = `${currentNum}${currentOperator}${operand}`;
-
     if (clickedOperator === '=') {
       return operand
-        ? render(eval(value), null, null, true)
+        ? render(
+          compute[currentOperator](currentNum, operand),
+          null,
+          null,
+          true,
+        )
         : render(currentNum, '=', null, true);
     }
 
     if (operand) {
-      return render(eval(value), clickedOperator, null);
+      return render(
+        compute[currentOperator](currentNum, operand),
+        clickedOperator,
+        null,
+      );
     }
 
     return render(currentNum, clickedOperator);
