87bd7e5b32f85d369ab0cd279f8fe031b6fd224c
yerin512 <sosu0512@naver.com>
2022-06-07 18:29:49 +0900

    미션2. 숫자 연속 누르면 숫자가 더해져서 출력

--- src/index.js
+++ src/index.js
@@ -19,49 +19,33 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-const calculateNumber = (sign) => {
-  switch (sign) {
-  case '+':
-    // 더한다
-    break;
-  case '-':
-    // 뺀다
-    break;
-  case '*':
-    // 나눈다
-    break;
-  case '/':
-    // 더한다
-    break;
-  case '=':
-    // 더한다
-    break;
-  default:
-    // 아무것도 안 한다.
-    break;
-  }
-};
-
 // 1. 숫자를 누르면 누른 숫자가 출력되어야 합니다. (완료!)
-// 2. 숫자를 연속해서 누르면 숫자가 더해져서 출력되어야 합니다. 힌트 한 번만 부탁드립니다...
-// 3. 숫자와 연산자를 입력한 후 =를 클릭하면 계산 결과가 출력되어야 합니다.
+// 2. 숫자를 연속해서 누르면 숫자가 더해져서 출력되어야 합니다. (완료!)
+// 3. 숫자와 연산자를 입력한 후 = 를 클릭하면 계산 결과가 출력되어야 합니다.
 // 4. 연속해서 숫자와 연산자를 입력하면 중간에 계산 결과가 출력되어야 합니다.
 
 function render(result = 0) {
-  const resultNumber = (a) => {
-    render(a);
+  const selectNumber = (number) => {
+    const newResult = result * 10 + number;
+    render(newResult);
+  };
+
+  // 계산은 미완성
+  const calculateNumber = (sign) => {
+    const newResult = result + 10;
+    render(newResult);
   };
 
   const element = (
     <div>
       <p>간단 계산기</p>
-      {result}
+      <p>{result}</p>
       <div>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
           <button
             type="button"
             onClick={() => {
-              resultNumber(i);
+              selectNumber(i);
             }}
           >
             {i}
