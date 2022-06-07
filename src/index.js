c1e3b5ea97f1b1504c01af45dd65043d853d120d
LeeHyangGi <52326913+2scent@users.noreply.github.com>
2022-06-08 01:00:25 +0900

    Pass plus test

--- src/index.js
+++ src/index.js
@@ -20,22 +20,78 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render(result = 0) {
+function render(result = 0, operand, operator) {
+  function handleClickOperand(value) {
+    if (operand) {
+      render(result, Number(`${operand}${value}`).toString(), operator);
+      return;
+    }
+
+    render(result, value, operator);
+  }
+
+  function handleClickPlus() {
+    let newOperand = Number(operand);
+
+    let newResult;
+
+    if (operator === '+') {
+      newResult = result + newOperand;
+    } else {
+      newResult = newOperand;
+    }
+
+    newOperand = '';
+
+    render(newResult, newOperand, '+');
+  }
+
+  function handleClickEqual() {
+    let newOperand = Number(operand);
+
+    let newResult;
+
+    if (operator === '+') {
+      newResult = result + newOperand;
+    } else {
+      newResult = newOperand;
+    }
+
+    newOperand = '';
+
+    render(newResult, newOperand, '=');
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{result}</p>
+      <p>{operand || result}</p>
       <p>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
-          <button>{i}</button>
+          <button
+            type="button"
+            onClick={() => handleClickOperand(i)}
+          >
+            {i}
+          </button>
         ))}
       </p>
       <p>
-        <button>+</button>
-        <button>-</button>
-        <button>*</button>
-        <button>/</button>
-        <button>=</button>
+        <button
+          type="button"
+          onClick={handleClickPlus}
+        >
+          +
+        </button>
+        <button type="button">-</button>
+        <button type="button">*</button>
+        <button type="button">/</button>
+        <button
+          type="button"
+          onClick={handleClickEqual}
+        >
+          =
+        </button>
       </p>
     </div>
   );
