2721f15d3d9b6dcb643d56ed69f547f3cf54dd54
Woo Dy <kjop7419@naver.com>
2022-06-10 19:20:00 +0900

    fix(caculator) 리팩토링 변경 내용 1. 오탈자를 수정하였습니다. 2. 문법에 어긋나는 단어를 수정하였습니다. 3. 재활용이 가능한 함수를 render함수 밖에 선언하여 전역적으로 사용이 가능하도록 수정하였습니다. 4. 의미가 중복되는 변수 및 함수명을 수정하였습니다. 5. 배열 변수명을 복수형으로 변경하였습니다.

--- src/index.js
+++ src/index.js
@@ -20,58 +20,59 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-const operatiorAction = {
-  '+': (answer, choicedNumber) => answer + choicedNumber,
-  '-': (answer, choicedNumber) => answer - choicedNumber,
-  '*': (answer, choicedNumber) => answer * choicedNumber,
-  '/': (answer, choicedNumber) => answer / choicedNumber,
+// 계산을 하는 로직이 들어있기에 operator(연산자) 보단 caculate(계산하다)가 잘 어울릴 것 같아 변경했습니다!
+const calculate = {
+  '+': (answer, chooseNumber) => answer + chooseNumber,
+  '-': (answer, chooseNumber) => answer - chooseNumber,
+  '*': (answer, chooseNumber) => answer * chooseNumber,
+  '/': (answer, chooseNumber) => answer / chooseNumber,
   '=': (answer) => answer,
 };
 
-function render({ answer = 0, choicedNumber, operator } = {}) {
-  // 숫자 배열화(매개변수로 값을 받아 재활용할 수 있는 함수로 만들었습니다.)
-  const createNumArr = (length) => {
-    // numArr의 초기값이 undefined로 채워지기에 사용하지 않는 매개변수는 _ 로 처리했습니다.
-    const numArr = new Array(length).fill().map((_, i) => i + 1);
-    numArr.splice(-1, 10, 0);
-    return numArr;
-  };
+// 숫자 배열화(매개변수로 값을 받아 재활용할 수 있는 함수로 만들었습니다.)
+const createNumArrs = (length) => {
+  // numArr의 초기값이 undefined로 채워지기에 사용하지 않는 매개변수는 _ 로 처리했습니다.
+  const numArrs = new Array(length).fill().map((_, i) => i + 1);
+  numArrs.splice(-1, 10, 0);
+  return numArrs;
+};
 
+function render({ answer = 0, selectedNumber, selectedOperator } = {}) {
   // 연산자 배열화
-  const operatorArr = ['+', '-', '*', '/', '='];
+  const operatorArrs = ['+', '-', '*', '/', '='];
 
   // 결과값을 return 하는 함수
   const getAnswer = () => {
-    if (operator) return operatiorAction[operator](answer, choicedNumber);
-    return choicedNumber;
+    if (selectedOperator) return calculate[selectedOperator](answer, selectedNumber);
+    return selectedNumber;
   };
 
   // 숫자를 선택하는 함수
-  const handleClickChoiceNumber = (value) => {
-    if (choicedNumber) {
-      render({ answer, choicedNumber: Number(`${choicedNumber}${value}`), operator });
+  const handleSelectNumber = (value) => {
+    if (selectedNumber) {
+      render({ answer, selectedNumber: Number(`${selectedNumber}${value}`), selectedOperator });
       return;
     }
-    render({ answer, choicedNumber: value, operator });
+    render({ answer, selectedNumber: value, selectedOperator });
   };
 
   // 계산을 하는 함수
-  const handleClickOperator = (value) => {
-    render({ answer: getAnswer(), operator: value });
+  const handleSelectOperator = (value) => {
+    render({ answer: getAnswer(), selectedOperator: value });
   };
 
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{choicedNumber || answer}</p>
+      <p>{selectedNumber || answer}</p>
       <p>
-        {createNumArr(10).map((i) => (
-          <button type="button" onClick={() => { handleClickChoiceNumber(i); }}>{i}</button>
+        {createNumArrs(10).map((i) => (
+          <button type="button" onClick={() => { handleSelectNumber(i); }}>{i}</button>
         ))}
       </p>
       <p>
-        {operatorArr.map((i) => (
-          <button type="button" onClick={() => { handleClickOperator(i); }}>{i}</button>
+        {operatorArrs.map((i) => (
+          <button type="button" onClick={() => { handleSelectOperator(i); }}>{i}</button>
         ))}
       </p>
     </div>
