83c440869b856ccab09dbbc900c2868ed29d9608
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-11 19:30:54 +0900

    refactor: 매개변수의 명확한 의미를 알 수 있도록 수정하였습니다.

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
+const container = document.getElementById('app');
 
 const initStates = {
   prevNumber: 0,
@@ -28,36 +29,41 @@ const initStates = {
 
 function render(propStates) {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
   const operators = ['+', '-', '*', '/'];
+
   const formula = {
     '+': (x, y) => x + y,
     '-': (x, y) => x - y,
     '*': (x, y) => x * y,
     '/': (x, y) => x / y,
-
   };
 
   const onClickNumber = (state, num) => {
     const { currentNumber } = state;
-    const nowNumber = currentNumber === 0 ? num : currentNumber * 10 + num;
-    render({ ...state, currentNumber: nowNumber, showNumber: nowNumber });
+
+    const displayNumber = currentNumber === 0 ? num : currentNumber * 10 + num;
+
+    render({ ...state, currentNumber: displayNumber, showNumber: displayNumber });
   };
 
   const calculate = (state) => (formula[state.operator](state.prevNumber, state.currentNumber));
 
-  const onClickOperator = (state, op) => {
+  const onClickOperator = (state, inputOperator) => {
     const { operator, currentNumber } = state;
 
     const getPrevNumber = () => (operator === '' ? currentNumber : calculate(state));
+
     const result = getPrevNumber();
 
     render({
-      prevNumber: result, currentNumber: 0, operator: op, showNumber: result,
+      prevNumber: result, currentNumber: 0, operator: inputOperator, showNumber: result,
     });
   };
 
   const onClickEquals = (state) => {
     const result = calculate(state);
+
     render({
       prevNumber: 0, currentNumber: 0, operator: '', showNumber: result,
     });
@@ -86,12 +92,11 @@ function render(propStates) {
         ))}
         <button type="button" onClick={() => { onClickEquals(propStates); }}>=</button>
       </div>
-
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  container.textContent = '';
+  container.appendChild(element);
 }
 
 render(initStates);
