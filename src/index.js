5306995aea15ece4559bc4b0a3bc263d16964e15
Minjae Kim <innersme@gmail.com>
2022-07-06 20:19:04 +0900

    create cal function

--- src/index.js
+++ src/index.js
@@ -20,10 +20,46 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+
+const numberArry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const operatorArry = ['+', '-', '*', '/', '='];
+
+function handleCalculator(value, windowNumber, numOrOperator) {
+  // https://stackoverflow.com/questions/20169217/how-to-write-isnumber-in-javascript
+  // _isNumber
+
+  if (numOrOperator === '=') {
+
+  }
+
+  if (numberArry.includes(numOrOperator)) {
+    render(value + numOrOperator, windowNumber.toString() + numOrOperator.toString());
+  }
+
+  if (operatorArry.includes(numOrOperator)) {
+    render(value + numOrOperator, windowNumber);
+  }
+}
+
+// value = 사칙연산이 들어간 문자열
+function render(value = '', windowNumber = 0) {
+  console.log(value);
   const element = (
-    <div>
+    <div id="calculator">
       <p>간단 계산기</p>
+      <p>
+        {windowNumber}
+      </p>
+      <p>
+        {numberArry.map(i => (
+          <button onClick={() => handleCalculator(value, windowNumber, i)}>{i}</button>
+        ))}
+      </p>
+      <p>
+        {operatorArry.map(i => (
+          <button onClick={() => handleCalculator(value, windowNumber, i)}>{i}</button>
+        ))}
+      </p>
     </div>
   );
 
