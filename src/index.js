ffdc781f869dfa3bb6533f6aea0f754db27bd990
Heejae Chang <heejae0704@gmail.com>
2022-07-07 19:57:58 +0900

    Implement a simple calculator
    사칙연산 계산기를 구현하였습니다.
    no-use-before-define 룰을 쓰지 않으려고  화살표 함수로 onClik 동작을
    정의했는데요. render 함수가 너무 길어지는 것이 아닌가 하는 생각이
    듭니다. 서로를 호출하는 함수가 있을 때 지금 코드보다 더 나은 작성
    방법이 있을까요?

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -20,10 +19,91 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const operatorsArray = ['+', '-', '*', '/', '='];
+const state = {
+  numberInput: 0,
+  displayNumber: 0,
+  storedOperator: '',
+  storedNumber: 0,
+};
+
+function calculate() {
+  const { log } = console;
+  switch (state.storedOperator) {
+  case '+':
+    return state.storedNumber + state.numberInput;
+  case '-':
+    return state.storedNumber - state.numberInput;
+  case '*':
+    return state.storedNumber * state.numberInput;
+  case '/':
+    return state.storedNumber / state.numberInput;
+  default:
+    log('There is something wrong!');
+    return null;
+  }
+}
+
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{state.displayNumber}</p>
+      <p>
+        {numbersArray.map((i) => (
+          <button
+            type="button"
+            onClick={() => {
+              if (state.numberInput === 0) {
+                state.numberInput = i;
+              } else {
+                state.numberInput = Number(String(state.numberInput) + String(i));
+              }
+              state.displayNumber = state.numberInput;
+              render();
+            }}
+          >
+            {i}
+          </button>
+        ))}
+      </p>
+      <p>
+        {operatorsArray.map((i) => (
+          <button
+            type="button"
+            onClick={() => {
+              // when the calculation is fully completed with '=' operator
+              if (i === '=') {
+                if (state.storedOperator === '') {
+                  state.displayNumber = state.numberInput;
+                } else {
+                  state.displayNumber = calculate();
+                }
+                state.storedOperator = '';
+                state.numberInput = 0;
+                render();
+                return;
+              }
+              // when the operation is happening for the first time
+              if (state.storedOperator === '') {
+                state.storedOperator = i;
+                state.storedNumber = state.displayNumber;
+              } else {
+                // when there was previous operation that needs to be completed
+                // before this calculation
+                state.displayNumber = calculate();
+                state.storedOperator = i;
+              }
+              state.numberInput = 0;
+              state.storedNumber = state.displayNumber;
+              render();
+            }}
+          >
+            {i}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
