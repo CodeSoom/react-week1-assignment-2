b1bdfd45e8665e801d6cc092d4d5559cf763a721
moon-kun86 <108505117+moon-kun86@users.noreply.github.com>
2022-07-11 11:13:21 +0900

    Update index.js
    1. 소스를 다시 이해하고 천천히 작성해보았습니다.
    2. 줄바꿈을 통해서 가독성을 높이려고 해보았습니다.
    3. 숫자형 재할당하는 이유를 콘솔을 통해 다시 확인해보았습니다. 문자열을 더해주는 방식이다 보니 더하기를 할때 계산이 안되고 문자열을 더해주는 반식으로 진행이 되었습니다.
    4. HTML event 을 제거하고 함수로 만들어서 작성을 다시 하였씁니다.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
+
 /* @jsx createElement */
+
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
+
   Object.entries(props || {}).forEach(([key, value]) => {
     element[key.toLowerCase()] = value;
   });
+
   children.flat().forEach((child) => {
     if (child instanceof Node) {
       element.appendChild(child);
@@ -12,12 +16,16 @@ function createElement(tagName, props, ...children) {
     }
     element.appendChild(document.createTextNode(child));
   });
+
   return element;
 }
+
 // 넘버배열
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
 // 사칙연산
 const operators = ['+', '-', '*', '/', '='];
+
 // 입력 받는 값
 const state = {
   numberInput: 0, // 버튼 클릭 시 입력 받는 숫자
@@ -25,18 +33,77 @@ const state = {
   storedOperator: '', // 클릭된 사칙연산
   storedNumber: 0, // 선택한 이전의 숫자
 };
+
 // 계산기 함수
 function calculate() {
   if(state.storedOperator === "+"){
     return state.storedNumber + state.numberInput;
+
   }else if(state.storedOperator === "-"){
     return state.storedNumber - state.numberInput;
+
   }else if(state.storedOperator === "*"){
     return state.storedNumber * state.numberInput;
+
   }else if(state.storedOperator === "/"){
     return state.storedNumber / state.numberInput;
+
   }
 }
+
+// 숫자 클릭 함수
+function showNumber(i) {
+  if (state.numberInput === 0) {
+    state.numberInput = i; // numberInput 0이면 선택한 수의 값 i가 됨
+
+    console.log(state.numberInput); 
+  } else { // numberInput 0이 아니라면? 
+    state.numberInput = Number(String(state.numberInput) + String(i));  // 문자를 숫자로 변경해주기 아래의 코드로 하면 사칙연산에서 계속 뒤에 붙여줌
+    // state.numberInput = state.numberInput + String(i);
+
+    console.log(state.numberInput);
+  }
+
+  state.displayNumber = state.numberInput;
+
+  render(); // 실행해
+}
+
+// 계산 클릭 함수
+function equalFunction(i) {
+  if (i === '=') { // 클릭한 버튼이 = 이면
+    if (state.storedOperator === '') { // storedOperator에 빈값을 체크하는 이유는? 빈값은 계산을 할 필요가 없으니까
+      state.displayNumber = state.numberInput;
+      
+      alert('숫자를 먼저 입력해주세요.');
+
+      return false;
+    } else {
+      state.displayNumber = calculate(); 
+      console.log(state.displayNumber);
+    }
+    state.storedOperator = '';
+
+    state.numberInput = 0;
+
+    return render(); 
+  }
+  if (state.storedOperator === '') {
+    state.storedOperator = i;
+
+    state.storedNumber = state.displayNumber;
+  } else {
+    state.displayNumber = calculate();
+
+    state.storedNumber = i;
+  }
+  state.numberInput = 0;
+
+  state.storedNumber = state.displayNumber;
+
+  render();
+}
+
 function render() {
   const element = (
     <div>
@@ -44,60 +111,23 @@ function render() {
       <p>{state.displayNumber}</p>
       <p>
         {numbers.map((i) => (
-          <button
-            type="button"
-            onClick={() => {
-              if (state.numberInput === 0) {
-                state.numberInput = i; // numberInput 0이면 선택한 수의 값 i가 됨
-                console.log(state.numberInput);
-              } else { // numberInput 0이 아니라면?
-                state.numberInput = Number(String(state.numberInput) + String(i));  // numberInput에 클릭한 수 i 만큼 붙여준다 근데 굳이 숫자형으로 변환이 필요한가?
-                console.log(state.numberInput);
-              }
-              state.displayNumber = state.numberInput;
-              render(); // 실행해
-            }}
-          >
+          <button type="button" onClick={() => showNumber(i)}>
             {i}
           </button>
         ))}
       </p>
       <p>
         {operators.map((i) => (
-          <button
-            type="button"
-            onClick={() => {
-              if (i === '=') { // 클릭한 버튼이 = 이면
-                if (state.storedOperator === '') { // storedOperator에 빈값을 체크하는 이유는?
-                  state.displayNumber = state.numberInput;
-                  console.log(state.displayNumber + "|||" + state.numberInput);
-                } else {
-                  state.displayNumber = calculate(); // state.displayNumber == calculate(); 함수다? 이부분이 이해가 가지 않습니다.
-                  console.log(state.displayNumber);
-                }
-                state.storedOperator = '';
-                state.numberInput = 0;
-                return render();
-              }
-              if (state.storedOperator === '') {
-                state.storedOperator = i;
-                state.storedNumber = state.displayNumber;
-              } else {
-                state.displayNumber = calculate();
-                state.storedNumber = i;
-              }
-              state.numberInput = 0;
-              state.storedNumber = state.displayNumber;
-              render();
-            }}
-          >
+          <button type="button" onClick={() => equalFunction(i)}>
             {i}
           </button>
        ))}
       </p>
     </div>
   );
+
   document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(element);
 }
+
 render();
