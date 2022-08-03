7dcae4717d1191e69fac4aec122a70b54a654aac
violet <violet4795@naver.com>
2022-08-04 02:02:26 +0900

    리팩토링 - 공통되는 부분 함수로 추출 - 불필요한 연산 제거 - 읽을수 있는 코드로 변환

--- src/index.js
+++ src/index.js
@@ -21,17 +21,17 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-// 연산자가 있을때 클릭 -> 계산결과 유지, 연산자 유지, currentNumber계산
-// 연산자가 없을때 클릭 -> 계산결과 삭제, 연산자 유지, currentNumber계산
 function clickNumber(calcResult, operator, currentNumber, number) {
-  render(operator !== '' ? calcResult : 0, operator, currentNumber === null ? number : Number(`${currentNumber}${number}`));
+  render(calcResult, operator, isExistCurrentNumber(currentNumber) ? Number(`${currentNumber}${number}`) : number);
 }
 
-// 근데 currentNumber가 없다. -> 계산결과 유지, 연산자를 바꾼다, currentNumber null
-// 근데 currentNumber가 있다. -> 계산결과 변경, 연산자를 바꾼다, currentNumber null
+function isExistCurrentNumber(currentNumber) {
+  return currentNumber !== null;
+}
+
 function clickOperator(calcResult, operator, currentNumber, operatorString) {
-  render(currentNumber !== null ? calc(calcResult, operator, currentNumber) : calcResult,
-    operatorString, null);
+  render((isExistCurrentNumber(currentNumber) ? calc(calcResult, operator, currentNumber)
+    : calcResult), operatorString, null);
 }
 
 function calc(calcResult, operator, currentNumber) {
@@ -53,7 +53,7 @@ function render(calcResult, operator, currentNumber) {
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{currentNumber === null ? calcResult : currentNumber}</p>
+      <p>{isExistCurrentNumber(currentNumber) ? currentNumber : calcResult}</p>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => <button type="button" onClick={() => clickNumber(calcResult, operator, currentNumber, number)}>{number}</button>)}
       <br />
       <br />
