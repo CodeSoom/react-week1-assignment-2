12c6134c9a369dd4715ce1d3a667d266059b9be3
violet <violet4795@naver.com>
2022-08-05 00:11:31 +0900

    전역 함수 -> render 내부 함수로 이동

--- src/index.js
+++ src/index.js
@@ -20,43 +20,41 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function calc(calcResult, operator, currentNumber) {
-  switch (operator) {
-  case '+':
-    return calcResult + currentNumber;
-  case '-':
-    return calcResult - currentNumber;
-  case '*':
-    return calcResult * currentNumber;
-  case '/':
-    return calcResult / currentNumber;
-  default: // '=' 3=2 -> 2
-    return currentNumber;
+function render(calculatedValue, operator, currentNumber) {
+  function calculator() {
+    if (operator === '+') return calculatedValue + currentNumber;
+    if (operator === '-') return calculatedValue - currentNumber;
+    if (operator === '*') return calculatedValue * currentNumber;
+    if (operator === '/') return calculatedValue / currentNumber;
+    return currentNumber; // '='
   }
-}
 
-function isExistCurrentNumber(currentNumber) {
-  return currentNumber !== null;
-}
+  function isExistCurrentNumber() {
+    return currentNumber !== null;
+  }
 
-function clickNumber(calcResult, operator, currentNumber, number, callBackFunc) {
-  callBackFunc(calcResult, operator, isExistCurrentNumber(currentNumber) ? Number(`${currentNumber}${number}`) : number);
-}
+  function clickNumber(clickedNumber) {
+    const calculatedCurrentNumber = isExistCurrentNumber() ? Number(`${currentNumber}${clickedNumber}`) : clickedNumber;
+    return render(calculatedValue, operator, calculatedCurrentNumber);
+  }
 
-function clickOperator(calcResult, operator, currentNumber, operatorString, callBackFunc) {
-  callBackFunc((isExistCurrentNumber(currentNumber) ? calc(calcResult, operator, currentNumber)
-    : calcResult), operatorString, null);
-}
+  function clickOperator(operatorString) {
+    const selectOperator = (isExistCurrentNumber() ? calculator() : calculatedValue);
+    return render(selectOperator, operatorString, null);
+  }
 
-function render(calcResult, operator, currentNumber) {
+  function settingShowNumber() {
+    return isExistCurrentNumber(currentNumber) ? currentNumber : calculatedValue;
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{isExistCurrentNumber(currentNumber) ? currentNumber : calcResult}</p>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => <button type="button" onClick={() => clickNumber(calcResult, operator, currentNumber, number, render)}>{number}</button>)}
+      <p>{settingShowNumber()}</p>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => <button type="button" onClick={() => clickNumber(number)}>{number}</button>)}
       <br />
       <br />
-      {['+', '-', '*', '/', '='].map((operatorString) => <button type="button" onClick={() => clickOperator(calcResult, operator, currentNumber, operatorString, render)}>{operatorString}</button>)}
+      {['+', '-', '*', '/', '='].map((operatorString) => <button type="button" onClick={() => clickOperator(operatorString)}>{operatorString}</button>)}
     </div>
   );
 
