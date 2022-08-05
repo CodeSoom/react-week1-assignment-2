4e4b5af4b751de86a227974ab8fa96a749d79926
violet <violet4795@naver.com>
2022-08-06 01:13:48 +0900

    함수 스코프 분리 - 역할이 비슷한 함수 묶어서 클래스 처리 - 전역 유틸성 함수 분리 - 그리는데 필요한 함수들은 render 함수 내부로 이동

--- src/index.js
+++ src/index.js
@@ -20,51 +20,61 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-// calculateParameter [ calculatedNumber, operator, currentNumber ]
-function render(...calculateParameter) {
-  function calculator(calculatedNumber, operator, currentNumber) {
-    if (operator === '+') return calculatedNumber + currentNumber;
-    if (operator === '-') return calculatedNumber - currentNumber;
-    if (operator === '*') return calculatedNumber * currentNumber;
-    if (operator === '/') return calculatedNumber / currentNumber;
-    return currentNumber; // '='
+const Util = (() => {
+  function isNull(parameter) {
+    return parameter === null;
+  }
+  return {
+    isNull,
+  };
+})();
+
+class Calculator {
+  constructor(calculatedNumber, operator, currentNumber) {
+    this.calculatedNumber = calculatedNumber;
+    this.operator = operator;
+    this.currentNumber = currentNumber;
   }
 
-  function isExistCurrentNumber(currentNumber) {
-    return currentNumber !== null;
+  calculateNumber() {
+    if (this.operator === '+') return this.calculatedNumber + this.currentNumber;
+    if (this.operator === '-') return this.calculatedNumber - this.currentNumber;
+    if (this.operator === '*') return this.calculatedNumber * this.currentNumber;
+    if (this.operator === '/') return this.calculatedNumber / this.currentNumber;
+    return this.currentNumber; // '='
   }
 
-  function addNumber(clickedNumber, currentNumber) {
-    return isExistCurrentNumber(currentNumber) ? Number(`${currentNumber}${clickedNumber}`) : clickedNumber;
+  makeNumber(clickedNumber) {
+    return Util.isNull(this.currentNumber) ? clickedNumber : Number(`${this.currentNumber}${clickedNumber}`);
   }
 
-  function clickNumber(clickedNumber, calculatedNumber, operator, currentNumber) {
-    const calculatedCurrentNumber = addNumber(clickedNumber, currentNumber);
-    return render(calculatedNumber, operator, calculatedCurrentNumber);
+  getCalculateNumber() {
+    return Util.isNull(this.currentNumber) ? this.calculatedNumber : this.calculateNumber();
+  }
+}
+
+function render({ calculatedNumber, operator, currentNumber }) {
+  const calculator = new Calculator(calculatedNumber, operator, currentNumber);
+  const showNumber = Util.isNull(currentNumber) ? calculatedNumber : currentNumber;
+
+  function clickNumber(clickedNumber) {
+    const calculatedCurrentNumber = calculator.makeNumber(clickedNumber);
+    render({ calculatedNumber, operator, currentNumber: calculatedCurrentNumber });
   }
 
-  function getCalculateNumber(previousCalculatedNumber, operator, currentNumber) {
-    return (isExistCurrentNumber(currentNumber)
-      ? calculator(previousCalculatedNumber, operator, currentNumber) : previousCalculatedNumber);
-  }
-
-  function clickOperator(operatorString, previousCalculatedNumber, operator, currentNumber) {
-    const calculateNumber = getCalculateNumber(previousCalculatedNumber, operator, currentNumber);
-    return render(calculateNumber, operatorString, null);
-  }
-
-  function settingShowNumber(calculatedNumber, currentNumber) {
-    return isExistCurrentNumber(currentNumber) ? currentNumber : calculatedNumber;
+  function clickOperator(operatorString) {
+    const calculateNumber = calculator.getCalculateNumber();
+    render({ calculatedNumber: calculateNumber, operator: operatorString, currentNumber: null });
   }
 
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{settingShowNumber(calculateParameter[0], calculateParameter[2])}</p>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => <button type="button" onClick={() => clickNumber(number, ...calculateParameter)}>{number}</button>)}
+      <p>{showNumber}</p>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => <button type="button" onClick={() => clickNumber(number)}>{number}</button>)}
       <br />
       <br />
-      {['+', '-', '*', '/', '='].map((operatorString) => <button type="button" onClick={() => clickOperator(operatorString, ...calculateParameter)}>{operatorString}</button>)}
+      {['+', '-', '*', '/', '='].map((operatorString) => <button type="button" onClick={() => clickOperator(operatorString)}>{operatorString}</button>)}
     </div>
   );
 
@@ -72,4 +82,4 @@ function render(...calculateParameter) {
   document.getElementById('app').appendChild(element);
 }
 
-render(0, '=', 0);
+render({ calculatedNumber: 0, operator: '=', currentNumber: 0 });
