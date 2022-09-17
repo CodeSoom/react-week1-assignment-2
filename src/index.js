68039cef26fda588804b81ca70f2b48a6b805db3
wjpark01 <wjpark01@fnfcorp.com>
2022-09-17 21:37:24 +0900

    week1-2 완성

--- src/index.js
+++ src/index.js
@@ -19,55 +19,44 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
-const x = [0, 0];
 
-function render(result = [0, 0]) {
-  function handleClickNumber(data, i) {
-    let temp;
-    if (data[1] === 0) {
-      temp = i;
-    } else {
-      temp = `${data[1]}${i}`;
-    }
-    const aaa = [0, parseInt(temp, 10)];
-    render(aaa);
+const initData = {
+  result: 0,
+  value: 0,
+  operation: '',
+};
+
+const calculator = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y,
+  '=': (x, y) => y,
+  '': (x, y) => y,
+};
+
+function render({ result, value, operation } = initData) {
+  function handleClickNumber(i) {
+    const temp = value * 10 + i;
+    render({ result, value: temp, operation });
   }
-  function handleClickOperation(data, operation) {
-    const aaa = data;
-    // eslint-disable-next-line prefer-destructuring
-    aaa[0] = aaa[1];
-    switch (operation) {
-    case '+':
-      temp = x[0] + x[1];
-      render(temp);dfdf
-      x[0] = temp;
-      x[1] = 0;
-      break;adsfasf
-      // case '-':
-      //   break;
-      // case '*':
-      //   break;
-      // case '/':
-      //    break;
-      //  case '=':
-      //    break;
-    default:
-      break;
-    }
-    // render(temp1);
+
+  function handleClickOperation(oper) {
+    render({ result: calculator[operation](result, value), value: 0, operation: oper });
   }
+
   const element = (
     <div>
       <p>간단 계산기</p>
-      <p>{result[1]}</p>
+      <p>{value === 0 ? result : value}</p>
       <p>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
-          <button type="button" onClick={() => handleClickNumber(result, i)}>{i}</button>
+          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
         ))}
       </p>
       <p>
         {['+', '-', '*', '/', '='].map((oper) => (
-          <button type="button" onClick={() => handleClickOperation(result, oper)}>{oper}</button>
+          <button type="button" onClick={() => handleClickOperation(oper)}>{oper}</button>
         ))}
       </p>
     </div>
