16b68fdbb1ceaf0df3b0fe1523370a57a6b1567d
wjpark01 <wjpark01@fnfcorp.com>
2022-09-17 17:03:25 +0900

    강의듣기전..

--- src/index.js
+++ src/index.js
@@ -19,11 +19,57 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
+const x = [0, 0];
 
-function render() {
+function render(result = [0, 0]) {
+  function handleClickNumber(data, i) {
+    let temp;
+    if (data[1] === 0) {
+      temp = i;
+    } else {
+      temp = `${data[1]}${i}`;
+    }
+    const aaa = [0, parseInt(temp, 10)];
+    render(aaa);
+  }
+  function handleClickOperation(data, operation) {
+    const aaa = data;
+    // eslint-disable-next-line prefer-destructuring
+    aaa[0] = aaa[1];
+    switch (operation) {
+    case '+':
+      temp = x[0] + x[1];
+      render(temp);dfdf
+      x[0] = temp;
+      x[1] = 0;
+      break;adsfasf
+      // case '-':
+      //   break;
+      // case '*':
+      //   break;
+      // case '/':
+      //    break;
+      //  case '=':
+      //    break;
+    default:
+      break;
+    }
+    // render(temp1);
+  }
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{result[1]}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(result, i)}>{i}</button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((oper) => (
+          <button type="button" onClick={() => handleClickOperation(result, oper)}>{oper}</button>
+        ))}
+      </p>
     </div>
   );
 
