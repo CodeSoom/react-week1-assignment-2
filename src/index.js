a7268c8601f7db304aba05829f52a7f935b8fc18
gnpsi <gnpsi90@gmail.com>
2022-09-19 20:43:13 +0900

    계산이 제대로 안 됨 ㅠ.ㅠ
    :wq

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-cond-assign */
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-shadow */
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
-
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
@@ -20,10 +24,61 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render({
+  display, result, opCode, handle,
+}) {
+  function calculate(i, j, opCode) {
+    if (opCode = '+') {
+      return i + j;
+    } if (opCode = '-') {
+      return i - j;
+    } if (opCode = '*') {
+      return i * j;
+    } if (opCode = '/') {
+      return i / j;
+    }
+  }
+  function handleClickNumber(i) {
+    if (handle = 'opCode' && display !== 0) {
+      display = Number(`${display}${i}`);
+    } else {
+      display = i;
+    }
+
+    render({
+      display,
+      result,
+      opCode,
+      handle: 'number',
+    });
+  }
+
+  function handleClickOpCode(i) {
+    render({
+      display: 0,
+      result:
+        (handle = 'number')
+          ? calculate(result, display, opCode)
+          : result,
+      opCode: i,
+      handle: 'opCode',
+    });
+  }
+
   const element = (
-    <div>
+    <div id="hoit" className="hoit">
       <p>간단 계산기</p>
+      <p>{display || result}</p>
+      <p>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+        ))}
+      </p>
+      <p>
+        {['+', '-', '*', '/', '='].map((i) => (
+          <button type="button" onClick={() => handleClickOpCode(i)}>{i}</button>
+        ))}
+      </p>
     </div>
   );
 
@@ -31,4 +86,9 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render({
+  display: 0,
+  result: 0,
+  opCode: '=',
+  handle: 'number',
+});
