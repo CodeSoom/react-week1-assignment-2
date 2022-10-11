85215b253a50c11de8b4cd0f38c06cae8c1092ff
yxxnhx <tjdbsgk514@hanmail.net>
2022-10-11 20:27:16 +0900

    make calculator

--- src/index.js
+++ src/index.js
@@ -20,10 +20,41 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const operators = ['+', '-', '*', '/', '='];
+const calculFnc = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y,
+  '=': (x, y) => x || y,
+};
+
+function handleNum(value) {}
+
+function handleSum() {}
+
+function calculate() {}
+
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{result}</p>
+      <div>
+        {num.map((i) => (
+          <button type="button" onClick={() => handleNum(i)}>
+            {i}
+          </button>
+        ))}
+      </div>
+      <div>
+        {operators.map((i) => (
+          <button type="button" onClick={handleSum}>
+            {i}
+          </button>
+        ))}
+      </div>
     </div>
   );
 
