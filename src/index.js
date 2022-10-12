9d4d6375178d6ca5cf8bfe135fe76d0e65af6c82
sejineeee <sejinee0918@gmail.com>
2022-10-12 19:59:51 +0900

    feat: 계산기 구현
    map 메서드를 이용해서 계산기 형태를 구현
    calculate 객체를 선언하고 연산자를 프로퍼티 키로, 함수를 프로퍼티 값으로 할당

--- src/index.js
+++ src/index.js
@@ -21,9 +21,41 @@ function createElement(tagName, props, ...children) {
 }
 
 function render() {
+  const state = {
+    number1: 0,
+    number2: 0,
+    result: 0,
+  };
+
+  function handleClickNumber(number) {
+
+  }
+
+  const calculate = {
+    '+': (num1, num2) => num1 + num2,
+    '-': (num1, num2) => num1 - num2,
+    '*': (num1, num2) => num1 * num2,
+    '/': (num1, num2) => num1 / num2,
+    '=': (num1, num2, operator) => num1,
+    num2,
+  };
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>숫자</p>
+      <div>
+        <div>
+          {
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => <button type="button" onClick={() => handleClickNumber(number)}>{number}</button>)
+          }
+        </div>
+        <div>
+          {
+            Object.keys(calculate).map((operator) => <button type="button" onClick={() => calculate[operator]()}>{operator}</button>)
+          }
+        </div>
+      </div>
     </div>
   );
 
