4345fd5f800a3abbc20daafd6ec0838a3fe1f794
Sunset-kim <wood2002@naver.com>
2022-10-12 21:14:05 +0900

    feat: calculator

--- src/index.js
+++ src/index.js
@@ -20,15 +20,122 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
-  const element = (
+function calculate(num1, operator, num2) {
+  let result;
+
+  switch (operator) {
+  case '+':
+    result = num1 + num2;
+    break;
+
+  case '-':
+    result = num1 - num2;
+    break;
+
+  case '/':
+    result = num1 / num2;
+    break;
+
+  case '*':
+    result = num1 * num2;
+    break;
+
+  default:
+    break;
+  }
+  return result;
+}
+
+function render(state) {
+  const handleClickNumber = (num) => {
+    if (!state.prev || typeof state.prev === 'string') {
+      render({
+        ...state,
+        total: state.display,
+        display: num,
+        prev: num,
+      });
+    } else {
+      const display = state.display * 10 + num;
+      render({
+        ...state,
+        display,
+        prev: num,
+      });
+    }
+  };
+
+  const handleClickOperator = (operator) => {
+    if (!state.operator) {
+      if (operator === '=') {
+        render({
+          ...state,
+          operator: null,
+          prev: null,
+        });
+      } else {
+        render({
+          ...state,
+          operator,
+          prev: operator,
+        });
+      }
+    } else {
+      const result = calculate(state.total, state.operator, state.display);
+
+      if (operator === '=') {
+        render({
+          ...state,
+          display: result,
+          operator: null,
+          prev: null,
+          total: null,
+        });
+      } else {
+        render({
+          ...state,
+          display: result,
+          operator,
+          total: result,
+          prev: operator,
+        });
+      }
+    }
+  };
+
+  const handleClick = (value) => {
+    if (typeof value === 'number') {
+      handleClickNumber(value);
+    } else {
+      handleClickOperator(value);
+    }
+  };
+
+  const calculatorTemplate = (
     <div>
-      <p>간단 계산기</p>
+      <div>{state.display}</div>
+      <div>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
+          <button type="button" onClick={() => handleClick(num)}>{num}</button>
+        ))}
+      </div>
+      <div>
+        {['+', '-', '*', '/', '='].map((str) => (
+          <button type="button" onClick={() => handleClick(str)}>{str}</button>
+        ))}
+      </div>
     </div>
   );
 
   document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  document.getElementById('app').appendChild(calculatorTemplate);
 }
 
-render();
+const initState = {
+  display: 0,
+  total: null,
+  operator: null,
+  prev: null,
+};
+
+render(initState);
