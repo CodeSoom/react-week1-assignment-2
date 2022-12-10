0f8b5430534f37690aac8344942bedf9b24eff48
Ubermensch0608 <melancholyee28@gmail.com>
2022-12-10 20:26:13 +0900

    Feat: 간단한 사칙 연산 기능을 가진 계산기 구현
    피연산자와 연산자를 구분하기 위해 각각 count와 holder 변수에 할당했습니다.   계산이 이루어질 조건을 나누기 위해 reset변수를 사용했습니다.
    
    각 연산자에 알맞게 계산이 되도록 helper 함수를 사용했습니다.

--- src/index.js
+++ src/index.js
@@ -20,10 +20,109 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function calcNumberHandler(currentOperator, forwardNum, backNum) {
+  let result;
+  switch (currentOperator) {
+  case '+':
+    result = forwardNum + backNum;
+    break;
+  case '-':
+    result = forwardNum - backNum;
+    break;
+
+  case '*':
+    result = forwardNum * backNum;
+    break;
+
+  case '/':
+    result = forwardNum / backNum;
+    break;
+  default:
+  }
+  return result;
+}
+
+function render({
+  count, operator, holder, reset,
+}) {
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  const operators = ['+', '-', '*', '/', '='];
+
+  function checkNumberHandler(clickedNumber) {
+    if (!operator) {
+      const newCount = count === '0' ? '' : count;
+      render({ count: newCount + clickedNumber, holder, reset: true });
+    } else {
+      const newCount = reset ? '' : count;
+      render({
+        count: newCount + clickedNumber,
+        operator,
+        holder,
+        reset: false,
+      });
+    }
+  }
+
+  function calculateNumberHandler(clickedOperator) {
+    if (!operator) {
+      render({
+        count, operator: clickedOperator, holder: count, reset: true,
+      });
+    } else if (clickedOperator === '=') {
+      const didCalcNumber = calcNumberHandler(
+        operator,
+        Number(holder),
+        Number(count),
+      );
+
+      render({
+        count: didCalcNumber,
+        operator: null,
+        holder: null,
+      });
+    } else {
+      const didCalcNumber = calcNumberHandler(
+        operator,
+        Number(holder),
+        Number(count),
+      );
+      render({
+        count: didCalcNumber,
+        operator: clickedOperator,
+        holder: didCalcNumber,
+        reset: true,
+      });
+    }
+  }
+
   const element = (
-    <div>
+    <div id="wrapper">
       <p>간단 계산기</p>
+
+      <h1>{count}</h1>
+
+      <ul id="list">
+        {numbers.map((number) => (
+          <li>
+            <button type="button" onClick={() => checkNumberHandler(number)}>
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <ul id="list">
+        {operators.map((oper) => (
+          <li>
+            <button
+              type="button"
+              onClick={() => calculateNumberHandler(oper)}
+            >
+              {oper}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 
@@ -31,4 +130,6 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render({
+  count: '0', operator: undefined, holder: undefined, reset: false,
+});
--- steps.d.ts
+++ steps.d.ts
@@ -2,8 +2,12 @@
 type steps_file = typeof import('./steps_file.js');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: CodeceptJS.I }
-  interface CallbackOrder { [0]: CodeceptJS.I }
+  interface SupportObject {
+    I: CodeceptJS.I;
+  }
+  interface CallbackOrder {
+    [0]: CodeceptJS.I;
+  }
   interface Methods extends CodeceptJS.Puppeteer {}
   interface I extends ReturnType<steps_file> {}
   namespace Translation {
