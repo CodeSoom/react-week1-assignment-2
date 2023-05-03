3f4029898172f4ee8434d23e1ccbed95859faa7c
ctaaag <qudxor102@gmail.com>
2023-05-04 00:08:17 +0900

    mission2 clear

--- src/index.js
+++ src/index.js
@@ -20,10 +20,74 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+let clickedFirstNum = null;
+let clickedSecondNum = null;
+let clickedOperator = null;
+
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const calculations = ['+', '-', '*', '/', '='];
+
+function numberHandler(num) {
+  if (!clickedOperator) {
+    !clickedFirstNum
+      ? (clickedFirstNum = num)
+      : (clickedFirstNum = clickedFirstNum * 10 + num);
+  } else {
+    !clickedSecondNum
+      ? (clickedSecondNum = num)
+      : (clickedSecondNum = clickedSecondNum * 10 + num);
+  }
+
+  render();
+}
+
+function calculationCases(operator) {
+  switch (operator) {
+    case '+':
+      clickedFirstNum += clickedSecondNum;
+      break;
+    case '-':
+      clickedFirstNum -= clickedSecondNum;
+      break;
+    case '*':
+      clickedFirstNum *= clickedSecondNum;
+      break;
+    case '/':
+      clickedFirstNum /= clickedSecondNum;
+      break;
+    default:
+      break;
+  }
+  clickedSecondNum = null;
+  render();
+}
+
+function operatorHandler(calculation) {
+  if (clickedOperator) {
+    calculationCases(clickedOperator);
+  }
+  clickedOperator = calculation;
+}
+
 function render() {
   const element = (
     <div>
       <p>간단 계산기</p>
+      <div>
+        {!clickedSecondNum ? clickedFirstNum || 0 : clickedSecondNum || 0}
+      </div>
+      <div>
+        {numberList.map((num) => (
+          <button onClick={() => numberHandler(num)}>{num}</button>
+        ))}
+      </div>
+      <div>
+        {calculations.map((calculation) => (
+          <button onClick={() => operatorHandler(calculation)}>
+            {calculation}
+          </button>
+        ))}
+      </div>
     </div>
   );
 
